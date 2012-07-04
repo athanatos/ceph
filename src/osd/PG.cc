athanatos/ceph

commit 3ad54a8225b2b7a421dcac4661bb5d78472da454

pg: report scrub status
Signed-off-by: Mike Ryan <mike.ryan@inktank.com>

src/osd/PG.cc
@@ -4319,6 +4319,20 @@ boost::statechart::result PG::RecoveryState::Active::react(const QueryState& q)
     q.f->close_section();
   }
 
+  q.f->open_object_section("scrub");
+  q.f->dump_stream("scrub_epoch_start") << pg->scrub_epoch_start;
+  q.f->dump_int("scrub_active", pg->scrub_active);
+  q.f->dump_int("scrub_block_writes", pg->scrub_block_writes);
+  q.f->dump_int("finalizing_scrub", pg->finalizing_scrub);
+  q.f->dump_int("scrub_waiting_on", pg->scrub_waiting_on);
+  q.f->open_array_section("scrub_waiting_on_whom");
+  for (set<int>::iterator p = pg->scrub_waiting_on_whom.begin();
+       p != pg->scrub_waiting_on_whom.end();
+       ++p) {
+    q.f->dump_int("osd", *p);
+  }
+  q.f->close_section();
+
   q.f->close_section();
   return forward_event();
 }
@@ -4398,9 +4412,12 @@ boost::statechart::result PG::RecoveryState::ReplicaActive::react(const MQuery& 
 
 boost::statechart::result PG::RecoveryState::ReplicaActive::react(const QueryState& q)
 {
+  PG *pg = context< RecoveryMachine >().pg;
+
   q.f->open_object_section("state");
   q.f->dump_string("name", state_name);
   q.f->dump_stream("enter_time") << enter_time;
+  q.f->dump_int("finalizing_scrub", pg->finalizing_scrub);
   q.f->close_section();
   return forward_event();
 }

commit f623076f32a0f69eb249ee95c7ef79e4f1edac81

osd: add missing formatter close_section() to scrub status
Also add braces to make the open/close matchups easier to see.  Broken
by 3ad54a8225b2b7a421dcac4661bb5d78472da454.

Signed-off-by: Sage Weil <sage@inktank.com>

src/osd/PG.cc
@@ -4342,19 +4342,24 @@ boost::statechart::result PG::RecoveryState::Active::react(const QueryState& q)
     q.f->close_section();
   }
 
-  q.f->open_object_section("scrub");
-  q.f->dump_stream("scrub_epoch_start") << pg->scrub_epoch_start;
-  q.f->dump_int("scrub_active", pg->scrub_active);
-  q.f->dump_int("scrub_block_writes", pg->scrub_block_writes);
-  q.f->dump_int("finalizing_scrub", pg->finalizing_scrub);
-  q.f->dump_int("scrub_waiting_on", pg->scrub_waiting_on);
-  q.f->open_array_section("scrub_waiting_on_whom");
-  for (set<int>::iterator p = pg->scrub_waiting_on_whom.begin();
-       p != pg->scrub_waiting_on_whom.end();
-       ++p) {
-    q.f->dump_int("osd", *p);
+  {
+    q.f->open_object_section("scrub");
+    q.f->dump_stream("scrub_epoch_start") << pg->scrub_epoch_start;
+    q.f->dump_int("scrub_active", pg->scrub_active);
+    q.f->dump_int("scrub_block_writes", pg->scrub_block_writes);
+    q.f->dump_int("finalizing_scrub", pg->finalizing_scrub);
+    q.f->dump_int("scrub_waiting_on", pg->scrub_waiting_on);
+    {
+      q.f->open_array_section("scrub_waiting_on_whom");
+      for (set<int>::iterator p = pg->scrub_waiting_on_whom.begin();
+	   p != pg->scrub_waiting_on_whom.end();
+	   ++p) {
+	q.f->dump_int("osd", *p);
+      }
+      q.f->close_section();
+    }
+    q.f->close_section();
   }
-  q.f->close_section();
 
   q.f->close_section();
   return forward_event();