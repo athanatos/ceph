athanatos/ceph

commit 3f7fae55cda58e6b6e25961d65d50e4be0d4c9da

PG: fix scrub feature check
acting[0] isn't necessarily the primary.  Start at
0 and explicitely skip the primary.

Signed-off-by: Samuel Just <sam.just@inktank.com>

src/osd/PG.cc
@@ -3592,7 +3592,9 @@ void PG::scrub(ThreadPool::TPHandle &handle)
     OSDMapRef curmap = osd->get_osdmap();
     scrubber.is_chunky = true;
     assert(backfill_targets.empty());
-    for (unsigned i=1; i<acting.size(); i++) {
+    for (unsigned i=0; i<acting.size(); i++) {
+      if (acting[i] == pg_whoami.shard)
+	continue;
       if (acting[i] == CRUSH_ITEM_NONE)
 	continue;
       ConnectionRef con = osd->get_con_osd_cluster(acting[i], get_osdmap()->get_epoch());

commit 926a9b081eb5011bd2400086660f0fccc0e97396

PG: skip pg_whoami.osd, not pg_whoami.shard in scrub feature check
Caused by typo in 3f7fae55cda58e6b6e25961d65d50e4be0d4c9da.

Fixes: #7517
Signed-off-by: Samuel Just <sam.just@inktank.com>

src/osd/PG.cc
@@ -3595,7 +3595,7 @@ void PG::scrub(ThreadPool::TPHandle &handle)
     scrubber.is_chunky = true;
     assert(backfill_targets.empty());
     for (unsigned i=0; i<acting.size(); i++) {
-      if (acting[i] == pg_whoami.shard)
+      if (acting[i] == pg_whoami.osd)
 	continue;
       if (acting[i] == CRUSH_ITEM_NONE)
 	continue;