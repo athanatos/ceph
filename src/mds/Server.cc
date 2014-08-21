athanatos/ceph

commit 49a9a49a4b63364d24c56f63a85ef802a69cfb14

mds: use Connection::mark_down()
Signed-off-by: Sage Weil <sage@inktank.com>

src/mds/Server.cc
@@ -351,7 +351,7 @@ void Server::_session_logged(Session *session, uint64_t state_seq, bool open, ve
       session->clear();
     } else if (session->is_killing()) {
       // destroy session, close connection
-      mds->messenger->mark_down(session->connection);
+      session->connection->mark_down();
       mds->sessionmap.remove_session(session);
     } else {
       assert(0);

src/mds/MDS.cc
@@ -2279,13 +2279,13 @@ bool MDS::ms_handle_reset(Connection *con)
     if (session) {
       if (session->is_closed()) {
 	dout(3) << "ms_handle_reset closing connection for session " << session->info.inst << dendl;
-	messenger->mark_down(con);
+	con->mark_down();
 	con->set_priv(NULL);
 	sessionmap.remove_session(session);
       }
       session->put();
     } else {
-      messenger->mark_down(con);
+      con->mark_down();
     }
   }
   return false;
@@ -2308,7 +2308,7 @@ void MDS::ms_handle_remote_reset(Connection *con)
     if (session) {
       if (session->is_closed()) {
 	dout(3) << "ms_handle_remote_reset closing connection for session " << session->info.inst << dendl;
-	messenger->mark_down(con);
+	con->mark_down();
 	con->set_priv(NULL);
 	sessionmap.remove_session(session);
       }

commit f261281830fe4c108d59d1682622c2ec5a3689d2

mds: fix crash killing sessions without conn
Bug was introduced in 49a9a49a4b  (mds: use Connection::mark_down()).
Used to call SimpleMessenger::mark_down, which had a check for
connection==NULL, when calling conn->mark_down() we must
now do our own nullness check.

Fixes: #9173

Signed-off-by: John Spray <john.spray@redhat.com>

src/mds/Server.cc
@@ -310,6 +310,7 @@ void Server::_session_logged(Session *session, uint64_t state_seq, bool open, ve
     assert(session->is_opening());
     mds->sessionmap.set_state(session, Session::STATE_OPEN);
     mds->sessionmap.touch_session(session);
+    assert(session->connection != NULL);
     session->connection->send_message(new MClientSession(CEPH_SESSION_OPEN));
   } else if (session->is_closing() ||
 	     session->is_killing()) {
@@ -343,7 +344,11 @@ void Server::_session_logged(Session *session, uint64_t state_seq, bool open, ve
       // ms_handle_remote_reset() and realize they had in fact closed.
       // do this *before* sending the message to avoid a possible
       // race.
-      session->connection->mark_disposable();
+      if (session->connection != NULL) {;
+        // Conditional because terminate_sessions will indiscrimately
+        // put sessions in CLOSING whether they ever had a conn or not.
+        session->connection->mark_disposable();
+      }
 
       // reset session
       mds->send_message_client(new MClientSession(CEPH_SESSION_CLOSE), session);
@@ -351,7 +356,9 @@ void Server::_session_logged(Session *session, uint64_t state_seq, bool open, ve
       session->clear();
     } else if (session->is_killing()) {
       // destroy session, close connection
-      session->connection->mark_down();
+      if (session->connection != NULL) {
+        session->connection->mark_down();
+      }
       mds->sessionmap.remove_session(session);
     } else {
       assert(0);