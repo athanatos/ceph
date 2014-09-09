athanatos/ceph

commit 469447ff4a850d8595255df572e926e8f82fb9b6

osd/ClassHandler: improve error logging
Fixes: #9365
Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/ClassHandler.cc
@@ -107,8 +107,12 @@ int ClassHandler::_load_class(ClassData *cls)
 
     struct stat st;
     int r = ::stat(fname, &st);
-    if (r < 0)
-      return -errno;
+    if (r < 0) {
+      r = -errno;
+      dout(0) << __func__ << " could not stat class " << fname
+	      << ": " << cpp_strerror(r) << dendl;
+      return r;
+    }
 
     cls->handle = dlopen(fname, RTLD_NOW);
     if (!cls->handle) {

commit a09e0eb7e2f1ce323768280a24a30d89eb355658

osd/ClassHandler: fix build
Broken by 469447ff4a850d8595255df572e926e8f82fb9b6.

Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/ClassHandler.cc
@@ -3,6 +3,7 @@
 #include "msg/Message.h"
 #include "osd/OSD.h"
 #include "ClassHandler.h"
+#include "common/errno.h"
 
 #include <dlfcn.h>
 