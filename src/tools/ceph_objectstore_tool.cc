athanatos/ceph

commit cf1987a78257b23898d1cb83a8b0600c644faeae

ceph_objectstore_tool: Minor improvements
Make all non-error non-debug output to stdout
Fix a message

Signed-off-by: David Zafman <david.zafman@inktank.com>

src/tools/ceph_objectstore_tool.cc
@@ -643,7 +643,7 @@ int export_file(ObjectStore *store, coll_t cid, ghobject_t &obj)
   if (ret < 0)
     return ret;
 
-  cerr << "read " << obj << std::endl;
+  cout << "Read " << obj << std::endl;
 
   total = st.st_size;
   if (debug)
@@ -785,7 +785,7 @@ int do_export(ObjectStore *fs, coll_t coll, spg_t pgid, pg_info_t &info,
   PGLog::IndexedLog log;
   pg_missing_t missing;
 
-  cerr << "Exporting " << pgid << std::endl;
+  cout << "Exporting " << pgid << std::endl;
 
   int ret = get_log(fs, coll, pgid, info, log, missing);
   if (ret > 0)
@@ -1836,11 +1836,11 @@ int main(int argc, char **argv)
       }
     }
 
-    cerr << colls_to_check.size() << " pgs to scan" << std::endl;
+    cout << colls_to_check.size() << " pgs to scan" << std::endl;
     for (vector<coll_t>::iterator i = colls_to_check.begin();
          i != colls_to_check.end();
          ++i, ++scanned) {
-      cerr << "Scanning " << *i << ", " << scanned << "/"
+      cout << "Scanning " << *i << ", " << scanned << "/"
            << colls_to_check.size() << " completed" << std::endl;
       ghobject_t next;
       while (!next.is_max()) {
@@ -1883,7 +1883,7 @@ int main(int argc, char **argv)
               cout << *i << "/" << *obj << " is lost" << std::endl;
             }
             if (op == "fix-lost") {
-              cerr << *i << "/" << *obj << " is lost, fixing" << std::endl;
+              cout << *i << "/" << *obj << " is lost, fixing" << std::endl;
               oi.clear_flag(object_info_t::FLAG_LOST);
               bufferlist bl2;
               ::encode(oi, bl2);
@@ -1901,7 +1901,7 @@ int main(int argc, char **argv)
         }
       }
     }
-    cerr << "Completed" << std::endl;
+    cout << "Completed" << std::endl;
 
    UMOUNT:
     fs->sync_and_flush();
@@ -1936,7 +1936,7 @@ int main(int argc, char **argv)
       continue;
     }
     if (snap != CEPH_NOSNAP && debug) {
-      cerr << "skipping snapped dir " << *it
+      cout << "skipping snapped dir " << *it
 	       << " (pg " << pgid << " snap " << snap << ")" << std::endl;
       continue;
     }

commit c215ca1616ab39d769213dfbefb4b8911731f781

ceph_objectstore_tool: When exporting to stdout, don't cout messages
Fixes: #10128
Caused by cf1987a7

Signed-off-by: David Zafman <dzafman@redhat.com>

src/tools/ceph_objectstore_tool.cc
@@ -648,7 +648,7 @@ int export_file(ObjectStore *store, coll_t cid, ghobject_t &obj)
   if (ret < 0)
     return ret;
 
-  cout << "Read " << obj << std::endl;
+  cerr << "Read " << obj << std::endl;
 
   total = st.st_size;
   if (debug)
@@ -791,7 +791,7 @@ int do_export(ObjectStore *fs, coll_t coll, spg_t pgid, pg_info_t &info,
   PGLog::IndexedLog log;
   pg_missing_t missing;
 
-  cout << "Exporting " << pgid << std::endl;
+  cerr << "Exporting " << pgid << std::endl;
 
   int ret = get_log(fs, coll, pgid, info, log, missing);
   if (ret > 0)
@@ -2490,8 +2490,8 @@ int main(int argc, char **argv)
 
     if (op == "export") {
       ret = do_export(fs, coll, pgid, info, map_epoch, struct_ver, superblock, past_intervals);
-      if (ret == 0 && file_fd != STDOUT_FILENO)
-        cout << "Export successful" << std::endl;
+      if (ret == 0)
+        cerr << "Export successful" << std::endl;
     } else if (op == "info") {
       formatter->open_object_section("info");
       info.dump(formatter);