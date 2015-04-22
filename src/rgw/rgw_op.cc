athanatos/ceph

commit e341d64bedb2e36bbdfc79c4efeff99a6074612f

rgw: make rgw_read_user_buckets() backward compatible.
Fixes: #10683
Backport: hammer
Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

src/rgw/rgw_op.cc
@@ -1002,7 +1002,7 @@ void RGWListBuckets::execute()
     read_count = min(limit - total_count, (uint64_t)max_buckets);
 
     ret = rgw_read_user_buckets(store, s->user.user_id, buckets,
-                                marker, read_count, should_get_stats());
+                                marker, read_count, should_get_stats(), 0);
 
     if (!started) {
       send_response_begin(buckets.count() > 0);

src/rgw/rgw_bucket.cc
@@ -42,8 +42,13 @@ void rgw_get_buckets_obj(const string& user_id, string& buckets_obj_id)
  * Get all the buckets owned by a user and fill up an RGWUserBuckets with them.
  * Returns: 0 on success, -ERR# on failure.
  */
-int rgw_read_user_buckets(RGWRados *store, string user_id, RGWUserBuckets& buckets,
-                          const string& marker, uint64_t max, bool need_stats)
+int rgw_read_user_buckets(RGWRados * store,
+                          string user_id,
+                          RGWUserBuckets& buckets,
+                          const string& marker,
+                          uint64_t max,
+                          bool need_stats,
+                          uint64_t default_amount)
 {
   int ret;
   buckets.clear();
@@ -58,6 +63,10 @@ int rgw_read_user_buckets(RGWRados *store, string user_id, RGWUserBuckets& bucke
   string m = marker;
 
   uint64_t total = 0;
+
+  if (!max) {
+    max = default_amount;
+  }
 
   do {
     ret = store->cls_user_list_buckets(obj, m, max - total, entries, &m, &truncated);

src/rgw/rgw_bucket.h
@@ -104,8 +104,13 @@ extern void rgw_bucket_init(RGWMetadataManager *mm);
  * Get all the buckets owned by a user and fill up an RGWUserBuckets with them.
  * Returns: 0 on success, -ERR# on failure.
  */
-extern int rgw_read_user_buckets(RGWRados *store, string user_id, RGWUserBuckets& buckets,
-                                 const string& marker, uint64_t max, bool need_stats);
+extern int rgw_read_user_buckets(RGWRados *store,
+                                 string user_id,
+                                 RGWUserBuckets& buckets,
+                                 const string& marker,
+                                 uint64_t max,
+                                 bool need_stats,
+                                 uint64_t default_amount = 1000);
 
 extern int rgw_link_bucket(RGWRados *store, string user_id, rgw_bucket& bucket, time_t creation_time, bool update_entrypoint = true);
 extern int rgw_unlink_bucket(RGWRados *store, string user_id, const string& bucket_name, bool update_entrypoint = true);

commit 7e72519e6b29093615a04fc461345d7ad4a00694

rgw: fix s3 list buckets
Fixes: #11451
Backport: hammer
Got broken in commit:e341d64bedb2e36bbdfc79c4efeff99a6074612f.
Resurrect the option to list unlimited number of buckets using the S3
api.

Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/rgw_op.cc
@@ -999,7 +999,11 @@ void RGWListBuckets::execute()
   do {
     RGWUserBuckets buckets;
     uint64_t read_count;
-    read_count = min(limit - total_count, (uint64_t)max_buckets);
+    if (limit >= 0) {
+      read_count = min(limit - total_count, (uint64_t)max_buckets);
+    } else {
+      read_count = max_buckets;
+    }
 
     ret = rgw_read_user_buckets(store, s->user.user_id, buckets,
                                 marker, read_count, should_get_stats(), 0);
@@ -1029,7 +1033,7 @@ void RGWListBuckets::execute()
 
     total_count += m.size();
 
-    done = (m.size() < read_count || total_count >= limit);
+    done = (m.size() < read_count || (limit >= 0 && total_count >= (uint64_t)limit));
 
     if (!m.empty()) {
       send_response_data(buckets);

src/rgw/rgw_op.h
@@ -185,7 +185,7 @@ protected:
   int ret;
   bool sent_data;
   string marker;
-  uint64_t limit;
+  int64_t limit;
   uint64_t limit_max;
   uint32_t buckets_count;
   uint64_t buckets_objcount;

src/rgw/rgw_rest_s3.h
@@ -30,7 +30,7 @@ public:
   ~RGWListBuckets_ObjStore_S3() {}
 
   int get_params() {
-    limit = 0; /* no limit */
+    limit = -1; /* no limit */
     return 0;
   }
   virtual void send_response_begin(bool has_buckets);