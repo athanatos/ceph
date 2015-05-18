athanatos/ceph

commit 25ab8b507be5e0c102b40c197b64da6c97cbeeda

mds: handle missing mydir dirfrag
Previously, if the mydir dirfrag was missing, then
creating stray dirs would assert out when trying
to project_fnode().

Signed-off-by: John Spray <john.spray@redhat.com>

src/mds/MDCache.cc
@@ -626,6 +626,14 @@ void MDCache::populate_mydir()
 
   dout(10) << "populate_mydir " << *mydir << dendl;
 
+  if (mydir->get_version() == 0) {
+    // A fresh dirfrag, we must dirty it before dirtying
+    // any of the strays we create within it.
+    LogSegment *ls = mds->mdlog->get_current_segment();
+    mydir->mark_complete();
+    mydir->mark_dirty(mydir->pre_dirty(), ls);
+  }
+
   if (!mydir->is_complete()) {
     mydir->fetch(new C_MDS_RetryOpenRoot(this));
     return;

commit 972103ae21798d7e28016de5aad63a9a9998268c

mds: fix handling missing mydir dirfrag
This was broken by 25ab8b50 aka "mds: handle missing mydir dirfrag"

The previous code was mistakenly treating a not-yet-loaded
dirfrag as a non-existent dirfrag, resulting in
inconsistent fragstats even when no objects had
actually been lost.

Fixes: #11641
Signed-off-by: John Spray <john.spray@redhat.com>

src/mds/MDCache.cc
@@ -638,18 +638,21 @@ void MDCache::populate_mydir()
 
   dout(10) << "populate_mydir " << *mydir << dendl;
 
-  if (mydir->get_version() == 0) {
-    // A fresh dirfrag, we must dirty it before dirtying
-    // any of the strays we create within it.
-    LogSegment *ls = mds->mdlog->get_current_segment();
-    mydir->mark_complete();
-    mydir->mark_dirty(mydir->pre_dirty(), ls);
-  }
-
   if (!mydir->is_complete()) {
     mydir->fetch(new C_MDS_RetryOpenRoot(this));
     return;
-  }    
+  }
+
+  if (mydir->get_version() == 0 && mydir->state_test(CDir::STATE_BADFRAG)) {
+    // A missing dirfrag, we will recreate it.  Before that, we must dirty
+    // it before dirtying any of the strays we create within it.
+    mds->clog->warn() << "fragment " << mydir->dirfrag() << " was unreadable, "
+      "recreating it now";
+    LogSegment *ls = mds->mdlog->get_current_segment();
+    mydir->state_clear(CDir::STATE_BADFRAG);
+    mydir->mark_complete();
+    mydir->mark_dirty(mydir->pre_dirty(), ls);
+  }
 
   // open or create stray
   for (int i = 0; i < NUM_STRAY; ++i) {