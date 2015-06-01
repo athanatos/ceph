athanatos/ceph

commit f5342626317a89b1f5f479ce63fbd86de6fa05fe

CompatSet, MDSMap: formatted dumps must avoid numeric tags
Tags made of numbers only confuse XML parsers.  Avoid them.

Signed-off-by: Dan Mick <dan.mick@inktank.com>

src/include/CompatSet.h
@@ -96,7 +96,7 @@ struct CompatSet {
 	   p != names.end();
 	   ++p) {
 	char s[10];
-	snprintf(s, sizeof(s), "%lld", (unsigned long long)p->first);
+	snprintf(s, sizeof(s), "feature_%lld", (unsigned long long)p->first);
 	f->dump_string(s, p->second);
       }
     }

src/mds/MDSMap.cc
@@ -100,7 +100,7 @@ void MDSMap::dump(Formatter *f) const
   f->open_object_section("up");
   for (map<int32_t,uint64_t>::const_iterator p = up.begin(); p != up.end(); ++p) {
     char s[10];
-    sprintf(s, "%d", p->first);
+    sprintf(s, "mds_%d", p->first);
     f->dump_int(s, p->second);
   }
   f->close_section();
@@ -115,7 +115,7 @@ void MDSMap::dump(Formatter *f) const
   f->open_object_section("info");
   for (map<uint64_t,mds_info_t>::const_iterator p = mds_info.begin(); p != mds_info.end(); ++p) {
     char s[10];
-    sprintf(s, "%llu", (long long unsigned)p->first);
+    sprintf(s, "gid_%llu", (long long unsigned)p->first);
     f->open_object_section(s);
     p->second.dump(f);
     f->close_section();

commit c437b81502424482b28df13d553e42c5930eee8a

mds, include: Fix dump() numeric char array to include additional alpha chars
Caused by: f5342626317a89b1f5f479ce63fbd86de6fa05fe

Signed-off-by: David Zafman <dzafman@redhat.com>

src/include/CompatSet.h
@@ -111,7 +111,7 @@ struct CompatSet {
       for (map<uint64_t,string>::const_iterator p = names.begin();
 	   p != names.end();
 	   ++p) {
-	char s[10];
+	char s[18];
 	snprintf(s, sizeof(s), "feature_%lld", (unsigned long long)p->first);
 	f->dump_string(s, p->second);
       }

src/mds/MDSMap.cc
@@ -122,7 +122,7 @@ void MDSMap::dump(Formatter *f) const
   f->close_section();
   f->open_object_section("up");
   for (map<mds_rank_t,mds_gid_t>::const_iterator p = up.begin(); p != up.end(); ++p) {
-    char s[10];
+    char s[14];
     sprintf(s, "mds_%d", int(p->first));
     f->dump_int(s, p->second);
   }