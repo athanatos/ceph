athanatos/ceph

commit a190feaf797c2e57306e719580b55a8124419cd0

mds/MDSMap: add features to MDSMap's mds_info_t
Signed-off-by: Sage Weil <sage@redhat.com>

src/mds/MDSMap.cc
@@ -85,6 +85,7 @@ void MDSMap::mds_info_t::dump(Formatter *f) const
        p != export_targets.end(); ++p) {
     f->dump_int("mds", *p);
   }
+  f->dump_unsigned("features", mds_features);
   f->close_section();
 }
 
@@ -398,7 +399,7 @@ void MDSMap::get_health(list<pair<health_status_t,string> >& summary,
 
 void MDSMap::mds_info_t::encode_versioned(bufferlist& bl, uint64_t features) const
 {
-  ENCODE_START(4, 4, bl);
+  ENCODE_START(5, 4, bl);
   ::encode(global_id, bl);
   ::encode(name, bl);
   ::encode(rank, bl);
@@ -410,6 +411,7 @@ void MDSMap::mds_info_t::encode_versioned(bufferlist& bl, uint64_t features) con
   ::encode(standby_for_rank, bl);
   ::encode(standby_for_name, bl);
   ::encode(export_targets, bl);
+  ::encode(mds_features, bl);
   ENCODE_FINISH(bl);
 }
 
@@ -432,7 +434,7 @@ void MDSMap::mds_info_t::encode_unversioned(bufferlist& bl) const
 
 void MDSMap::mds_info_t::decode(bufferlist::iterator& bl)
 {
-  DECODE_START_LEGACY_COMPAT_LEN(4, 4, 4, bl);
+  DECODE_START_LEGACY_COMPAT_LEN(5, 4, 4, bl);
   ::decode(global_id, bl);
   ::decode(name, bl);
   ::decode(rank, bl);
@@ -445,6 +447,8 @@ void MDSMap::mds_info_t::decode(bufferlist::iterator& bl)
   ::decode(standby_for_name, bl);
   if (struct_v >= 2)
     ::decode(export_targets, bl);
+  if (struct_v >= 5)
+    ::decode(mds_features, bl);
   DECODE_FINISH(bl);
 }
 

src/mds/MDSMap.h
@@ -140,6 +140,7 @@ public:
     mds_rank_t standby_for_rank;
     std::string standby_for_name;
     std::set<mds_rank_t> export_targets;
+    uint64_t mds_features;
 
     mds_info_t() : global_id(MDS_GID_NONE), rank(MDS_RANK_NONE), inc(0), state(STATE_STANDBY), state_seq(0),
 		   standby_for_rank(MDS_NO_STANDBY_PREF) { }
@@ -348,6 +349,19 @@ public:
   }
   void get_failed_mds_set(std::set<mds_rank_t>& s) {
     s = failed;
+  }
+
+  // features
+  uint64_t get_up_features() {
+    uint64_t f = 0;
+    for (std::map<mds_rank_t, mds_gid_t>::const_iterator p = up.begin();
+	 p != up.end();
+	 ++p) {
+      std::map<mds_gid_t, mds_info_t>::const_iterator q = mds_info.find(p->second);
+      assert(q != mds_info.end());
+      f |= q->second.mds_features;
+    }
+    return f;
   }
 
   /**

commit 1cc272dd958f473e867b2e1c8ac3b387a235c34e

mds: fix mds_info_t::dump
This broke with a190feaf7 ('features' gets
output as an item in the 'export_targets' list),
which wasn't the fsmap branch, but fix it while
I'm tidying.

Signed-off-by: John Spray <john.spray@redhat.com>

src/mds/MDSMap.cc
@@ -88,8 +88,8 @@ void MDSMap::mds_info_t::dump(Formatter *f) const
        p != export_targets.end(); ++p) {
     f->dump_int("mds", *p);
   }
-  f->dump_unsigned("features", mds_features);
   f->close_section();
+  f->dump_unsigned("features", mds_features);
 }
 
 void MDSMap::mds_info_t::generate_test_instances(list<mds_info_t*>& ls)