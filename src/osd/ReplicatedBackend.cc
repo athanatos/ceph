4d396562ddbe1ac305bfecb3e65b53bb115c1bd4
Samuel Just <sjust@redhat.com>
2016-10-28 14:42:25 -0700

    working

--- src/osd/ReplicatedBackend.cc
+++ src/osd/ReplicatedBackend.cc
@@ -577,7 +577,7 @@ void ReplicatedBackend::submit_transaction(
     tid,
     reqid,
     trim_to,
-    roll_forward_to,
+    at_version,
     added.size() ? *(added.begin()) : hobject_t(),
     removed.size() ? *(removed.begin()) : hobject_t(),
     log_entries,
@@ -592,7 +592,7 @@ void ReplicatedBackend::submit_transaction(
     log_entries,
     hset_history,
     trim_to,
-    roll_forward_to,
+    at_version,
     true,
     op_t);
   
