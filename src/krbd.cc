athanatos/ceph

commit fb69f3adffc2f7bf80a0e6994afbf6232c9f4846

krbd.cc: don't rely on MonMap internal members
Use the public interface instead. That's stable and not as prone to
change.

Signed-off-by: Joao Eduardo Luis <joao@suse.de>

src/krbd.cc
@@ -118,12 +118,14 @@ static int build_map_buf(CephContext *cct, const char *pool, const char *image,
   if (r < 0)
     return r;
 
-  for (map<string, entity_addr_t>::const_iterator it = monmap.mon_addr.begin();
-       it != monmap.mon_addr.end();
-       ++it) {
-    if (it != monmap.mon_addr.begin())
+  list<entity_addr_t> mon_addr;
+  monmap.list_addrs(mon_addr);
+
+  for (const auto &p : mon_addr) {
+    if (oss.tellp() > 0) {
       oss << ",";
-    oss << it->second.get_sockaddr();
+    }
+    oss << p;
   }
 
   oss << " name=" << cct->_conf->name.get_id();

commit 5ec7aaeefa8f33d5a1e521fbc39a3c7ecfae5d3e

krbd: kernel client expects ip[:port], not an entity_addr_t
Commit fb69f3adffc2 ("krbd.cc: don't rely on MonMap internal members")
inadvertently dropped .get_sockaddr() call, breaking rbd map.  Fix it.

Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

src/krbd.cc
@@ -125,7 +125,7 @@ static int build_map_buf(CephContext *cct, const char *pool, const char *image,
     if (oss.tellp() > 0) {
       oss << ",";
     }
-    oss << p;
+    oss << p.get_sockaddr();
   }
 
   oss << " name=" << cct->_conf->name.get_id();