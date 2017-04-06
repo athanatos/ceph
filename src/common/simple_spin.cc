athanatos/ceph

commit cd8c6e6276c64e6d3e5debb7b3b9e16741beb905

common: Make spinlock delay more conventional
The accepted method of implementing a spinlock delay is the "pause"
instruction.

Signed-off-by: Brad Hubbard <bhubbard@redhat.com>

src/common/simple_spin.cc
@@ -18,9 +18,6 @@
 #include <stdint.h>
 #include <pthread.h>
 
-static uint32_t bar = 13;
-static uint32_t *foo = &bar;
- 
 void simple_spin_lock(simple_spinlock_t *lock)
 {
   while(1) {
@@ -31,9 +28,15 @@ void simple_spin_lock(simple_spinlock_t *lock)
 	return;
     }
     // delay
-    for (int i = 0; i < 100000; i++) {
-      *foo = (*foo * 33) + 17;
-    }
+#if defined(__i386__) || defined(__x86_64__)
+    asm volatile("pause");
+#elif defined(__arm__) || defined(__aarch64__)
+    asm volatile("yield");
+#elif defined(__ppc64__)
+    asm volatile("or 27,27,27");
+#else
+#error "Unknown architecture"
+#endif
   }
 }
 

commit 2f3d875124dbd488951f9aa97403ceba25c5b5d9

common: support s390 and unknown architectures in spin-wait loop
This fixes the s390 build which was broken by cd8c6e6276c64e6d3e5debb7b3b9e16741beb905

It also reinstates the previous architecture-independent for-loop
implementation as a catch-all for "unknown" (not-explicitly-supported)
architectures.

Fixes: http://tracker.ceph.com/issues/19492
Signed-off-by: Nathan Cutler <ncutler@suse.com>

src/common/simple_spin.cc
@@ -18,25 +18,42 @@
 #include <stdint.h>
 #include <pthread.h>
 
-void simple_spin_lock(simple_spinlock_t *lock)
+namespace {
+
+#if !defined(__i386__) && !defined(__x86_64__) && !defined(__arm__) && !defined(__aarch64__) && !defined(__powerpc__) && !defined(__ppc__) && !defined(__s390__) && !defined(__s390x__)
+static uint32_t bar = 13;
+static uint32_t *foo = &bar;
+#endif
+
+void cpu_relax()
 {
-  while(1) {
-    __sync_synchronize();
-    uint32_t oldval = *lock;
-    if (oldval == 0) {
-      if (__sync_bool_compare_and_swap(lock, 0, 1))
-	return;
-    }
-    // delay
 #if defined(__i386__) || defined(__x86_64__)
     asm volatile("pause");
 #elif defined(__arm__) || defined(__aarch64__)
     asm volatile("yield");
 #elif defined(__powerpc__) || defined(__ppc__)
     asm volatile("or 27,27,27");
+#elif defined(__s390__) || defined(__s390x__)
+    asm volatile("": : :"memory");
 #else
-#error "Unknown architecture"
+    for (int i = 0; i < 100000; i++) {
+      *foo = (*foo * 33) + 17;
+    }
 #endif
+}
+
+} // namespace
+
+void simple_spin_lock(simple_spinlock_t *lock)
+{
+  while (1) {
+    __sync_synchronize();
+    uint32_t oldval = *lock;
+    if (oldval == 0) {
+      if (__sync_bool_compare_and_swap(lock, 0, 1))
+	return;
+    }
+    cpu_relax();
   }
 }
 