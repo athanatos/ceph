athanatos/ceph

commit 12e8fe3b12b71a4ad468f69175463b2a402d75ed

rdma: Assign instead of compare
Fixed:

** CID 1414508 (#1 of 1): Assign instead of compare (PW.ASSIGN_WHERE_COMPARE_MEANT)
1. assign_where_compare_meant: use of "=" where "==" may have been intended

Signed-off-by: Amit Kumar amitkuma@redhat.com

src/msg/async/rdma/RDMAConnectedSocketImpl.cc
@@ -599,11 +599,8 @@ void RDMAConnectedSocketImpl::cleanup() {
 
 void RDMAConnectedSocketImpl::notify()
 {
-  uint64_t i = 1;
-  int ret;
-
-  ret = write(notify_fd, &i, sizeof(i));
-  assert(ret = sizeof(i));
+  int i = 1;
+  assert(sizeof(i) == write(notify_fd, &i, sizeof(i)));
 }
 
 void RDMAConnectedSocketImpl::shutdown()

commit a387be21893638cb75d1e94bc9d57ee7e2e74231

src/msg/rdma: fixes failure on assert in notify()
The commit fixes incorrect eventfd handling introduced in
12e8fe3b12b71a4ad468f69175463b2a402d75ed

Signed-off-by: Alex Mikheev <alexm@mellanox.com>

src/msg/async/rdma/RDMAConnectedSocketImpl.cc
@@ -610,7 +610,10 @@ void RDMAConnectedSocketImpl::cleanup() {
 
 void RDMAConnectedSocketImpl::notify()
 {
-  int i = 1;
+  // note: notify_fd is an event fd (man eventfd)
+  // write argument must be a 64bit integer
+  uint64_t i = 1;
+
   assert(sizeof(i) == write(notify_fd, &i, sizeof(i)));
 }
 