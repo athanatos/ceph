sunnyku/ceph-ci

osd/PG: ensure an actual transaction gets queued for recovery finish
Otherwise, this context gets leaked and lost.

Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/PG.cc
@@ -7419,6 +7419,7 @@ PG::RecoveryState::Clean::Clean(my_context ctx)
   }
   Context *c = pg->finish_recovery();
   context< RecoveryMachine >().get_cur_transaction()->register_on_commit(c);
+  context< RecoveryMachine >().get_cur_transaction()->noop();
 
   if (pg->is_active()) {
     pg->mark_clean();

athanatos/ceph

src/osd/PG.cc
@@ -7774,6 +7774,7 @@ PG::RecoveryState::Clean::Clean(my_context ctx)
   }
   Context *c = pg->finish_recovery();
   context< RecoveryMachine >().get_cur_transaction()->register_on_commit(c);
+  context< RecoveryMachine >().get_cur_transaction()->nop();
 
   if (pg->is_active()) {
     pg->mark_clean();