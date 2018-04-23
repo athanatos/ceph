athanatos/ceph

commit 213de88e4392e2c6566a02528858c4995e8f6cf7

objclass-sdk: expose __cls_init() to the world
the __cls_init() function should be visible. otherwise we cannot call it
to register the cls methods

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/include/rados/objclass.h
@@ -20,13 +20,15 @@ int __cls_ver_min = min;
 #define CLS_NAME(name) \
 int __cls_name__## name = 0; \
 const char *__cls_name = #name;
+
+#define CEPH_CLS_API [[gnu::visibility("default")]]
 #define CLS_INIT(name) \
-void CEPH_CLS_API __cls_init()
+CEPH_CLS_API void __cls_init()
 #else
 #define CLS_VER(maj,min)
 #define CLS_NAME(name)
 #define CLS_INIT(name) \
-void CEPH_CLS_API name##_cls_init()
+CEPH_CLS_API void name##_cls_init()
 #endif
 
 #define CLS_METHOD_RD       0x1 /// method executes read operations

src/objclass/objclass.h
@@ -14,12 +14,6 @@
 
 struct obj_list_watch_response_t;
 
-#if __GNUC__ >= 4
-  #define CEPH_CLS_API    __attribute__ ((visibility ("default")))
-#else
-  #define CEPH_CLS_API
-#endif
-
 extern "C" {
 #endif
 

src/cls/sdk/cls_sdk.cc
@@ -115,7 +115,7 @@ static int test_coverage_replay(cls_method_context_t hctx, bufferlist *in, buffe
   return 0;
 }
 
-void __cls_init()
+CLS_INIT(sdk)
 {
   CLS_LOG(0, "loading cls_sdk");
 

commit e70c990c58bd28ab9547d38a3c04346a08d831c1

rados/objclass.h: fix build define CEPH_CLS_API in all cases
Fix for bug introduced with 213de88e4392e2c6566a02528858c4995e8f6cf7:

In file included from src/objclass/objclass.h:13:0,
                 from src/cls/hello/cls_hello.cc:37:
src/include/rados/objclass.h:31:1: error: ‘CEPH_CLS_API’ does not name a
type; did you mean ‘CEPH_CAP_ANY’?
 CEPH_CLS_API void name##_cls_init()
 ^
/develop/ceph/source/ceph/src/include/rados/objclass.h:31:1: note: in
definition of macro ‘CLS_INIT’
 CEPH_CLS_API void name##_cls_init()
 ^~~~~~~~~~~~

Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

src/include/rados/objclass.h
@@ -11,6 +11,8 @@
 extern "C" {
 #endif
 
+#define CEPH_CLS_API [[gnu::visibility("default")]]
+
 #ifndef BUILDING_FOR_EMBEDDED
 #define CLS_VER(maj,min) \
 int __cls_ver__## maj ## _ ##min = 0; \
@@ -21,7 +23,6 @@ int __cls_ver_min = min;
 int __cls_name__## name = 0; \
 const char *__cls_name = #name;
 
-#define CEPH_CLS_API [[gnu::visibility("default")]]
 #define CLS_INIT(name) \
 CEPH_CLS_API void __cls_init()
 #else