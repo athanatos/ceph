athanatos/ceph

commit 6a63ab16f9b530a5e5dad96300b50a54c24c372e

mon: require cephx v2 signatures from other mons for >= mimic
Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/Monitor.cc
@@ -2198,7 +2198,8 @@ void Monitor::calc_quorum_requirements()
     required_features |= CEPH_FEATUREMASK_SERVER_LUMINOUS;
   }
   if (features.incompat.contains(CEPH_MON_FEATURE_INCOMPAT_MIMIC)) {
-    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC;
+    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC |
+      CEPH_FEATUREMASK_CEPHX_V2;
   }
 
   // monmap
@@ -2212,7 +2213,8 @@ void Monitor::calc_quorum_requirements()
   }
   if (monmap->get_required_features().contains_all(
 	ceph::features::mon::FEATURE_MIMIC)) {
-    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC;
+    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC |
+      CEPH_FEATUREMASK_CEPHX_V2;
   }
   dout(10) << __func__ << " required_features " << required_features << dendl;
 }

commit 5a81e1639f6b1b32aa45f27a7dc0abc4036ca831

mon: don't require CEPHX_V2 from mons until nautilus
The mimic 13.2.0 didn't have it.

Fixes: 6a63ab16f9b530a5e5dad96300b50a54c24c372e
Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/Monitor.cc
@@ -2206,11 +2206,11 @@ void Monitor::calc_quorum_requirements()
     required_features |= CEPH_FEATUREMASK_SERVER_LUMINOUS;
   }
   if (features.incompat.contains(CEPH_MON_FEATURE_INCOMPAT_MIMIC)) {
-    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC |
-      CEPH_FEATUREMASK_CEPHX_V2;
+    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC;
   }
   if (features.incompat.contains(CEPH_MON_FEATURE_INCOMPAT_NAUTILUS)) {
-    required_features |= CEPH_FEATUREMASK_SERVER_NAUTILUS;
+    required_features |= CEPH_FEATUREMASK_SERVER_NAUTILUS |
+      CEPH_FEATUREMASK_CEPHX_V2;
   }
 
   // monmap
@@ -2224,12 +2224,12 @@ void Monitor::calc_quorum_requirements()
   }
   if (monmap->get_required_features().contains_all(
 	ceph::features::mon::FEATURE_MIMIC)) {
-    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC |
-      CEPH_FEATUREMASK_CEPHX_V2;
+    required_features |= CEPH_FEATUREMASK_SERVER_MIMIC;
   }
   if (monmap->get_required_features().contains_all(
 	ceph::features::mon::FEATURE_NAUTILUS)) {
-    required_features |= CEPH_FEATUREMASK_SERVER_NAUTILUS;
+    required_features |= CEPH_FEATUREMASK_SERVER_NAUTILUS |
+      CEPH_FEATUREMASK_CEPHX_V2;
   }
   dout(10) << __func__ << " required_features " << required_features << dendl;
 }