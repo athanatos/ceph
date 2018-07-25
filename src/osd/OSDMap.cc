athanatos/ceph

commit d5bda9c96d3508ee005af3b43bd996a7e4e11acb

osd/OSDMap: require cephx v2 signatures based on require_min_compat_client, require_osd_release
Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/OSDMap.cc
@@ -1420,6 +1420,17 @@ uint64_t OSDMap::get_features(int entity_type, uint64_t *pmask) const
     mask |= kraken_features;
   }
 
+  if (require_min_compat_client >= CEPH_RELEASE_MIMIC) {
+    // if min_compat_client is >= mimic, require v2 cephx signatures
+    // from everyone
+    features |= CEPH_FEATUREMASK_CEPHX_V2;
+  } else if (require_osd_release >= CEPH_RELEASE_MIMIC &&
+	     entity_type == CEPH_ENTITY_TYPE_OSD) {
+    // if osds are >= mimic, at least require the signatures from them
+    features |= CEPH_FEATUREMASK_CEPHX_V2;
+  }
+  mask |= CEPH_FEATUREMASK_CEPHX_V2;
+
   if (pmask)
     *pmask = mask;
   return features;

commit 9c94661671dd1650e7b722c6231f7675247eaa58

osd/OSDMap: fix CEPHX_V2 osd requirement to nautilus, not mimic
This was delayed and did not appear in 13.2.0.

Fixes: d5bda9c96d3508ee005af3b43bd996a7e4e11acb
Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/OSDMap.cc
@@ -1504,13 +1504,13 @@ uint64_t OSDMap::get_features(int entity_type, uint64_t *pmask) const
     mask |= kraken_features;
   }
 
-  if (require_min_compat_client >= CEPH_RELEASE_MIMIC) {
-    // if min_compat_client is >= mimic, require v2 cephx signatures
+  if (require_min_compat_client >= CEPH_RELEASE_NAUTILUS) {
+    // if min_compat_client is >= nautilus, require v2 cephx signatures
     // from everyone
     features |= CEPH_FEATUREMASK_CEPHX_V2;
-  } else if (require_osd_release >= CEPH_RELEASE_MIMIC &&
+  } else if (require_osd_release >= CEPH_RELEASE_NAUTILUS &&
 	     entity_type == CEPH_ENTITY_TYPE_OSD) {
-    // if osds are >= mimic, at least require the signatures from them
+    // if osds are >= nautilus, at least require the signatures from them
     features |= CEPH_FEATUREMASK_CEPHX_V2;
   }
   mask |= CEPH_FEATUREMASK_CEPHX_V2;