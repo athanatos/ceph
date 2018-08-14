athanatos/ceph

commit c22d92559b29fad4ec4e5884b9b96f929bd37030

mon/OSDMonitor: require --yes-i-really-mean-it for force-create-pg
This command declares data loss permanent.  Be sure the user really means
it.

Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/MonCommands.h
@@ -825,7 +825,8 @@ COMMAND("osd reweightn " \
 	"reweight osds with {<id>: <weight>,...})",
 	"osd", "rw", "cli,rest")
 COMMAND("osd force-create-pg " \
-	"name=pgid,type=CephPgid ",
+	"name=pgid,type=CephPgid,"\
+        "name=sure,type=CephChoices,strings=--yes-i-really-mean-it,req=false", \
 	"force creation of pg <pgid>",
         "osd", "rw", "cli,rest")
 COMMAND("osd pg-temp " \

PendingReleaseNotes
@@ -209,3 +209,9 @@ method. See http://docs.ceph.com/docs/luminous/mgr/restful for details.
   first need to define a username and password. See the documentation for a
   feature overview and installation instructions:
   http://docs.ceph.com/docs/master/mgr/dashboard/
+
+* The 'osd force-create-pg' command now requires a force option to
+  proceed because the command is dangerous: it declares that data loss
+  is permanent and instructs the cluster to proceed with an empty PG
+  in its place, without making any further efforts to find the missing
+  data.

src/mon/OSDMonitor.cc
@@ -11559,6 +11559,13 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
       err = -ENOENT;
       goto reply;
     }
+    string sure;
+    cmd_getval(cct, cmdmap, "sure", sure);
+    if (sure != "--yes-i-really-mean-it") {
+      ss << "This command will recreate a lost (as in data lost) PG with data in it, such that the cluster will give up ever trying to recover the lost data.  Do this only if you are certain that all copies of the PG are in fact lost and you are willing to accept that the data is permanently destroyed.  Pass --yes-i-really-mean-it to proceed.";
+      err = -EPERM;
+      goto reply;
+    }
     bool creating_now;
     {
       std::lock_guard<std::mutex> l(creating_pgs_lock);

commit 7dc6f657f5508835bc7f2d98a09219efaed307a7

mon/MonCommands: fix 'osd force-create-pg'
Fixes: c22d92559b29fad4ec4e5884b9b96f929bd37030
Fixes: http://tracker.ceph.com/issues/26940
Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/MonCommands.h
@@ -834,7 +834,7 @@ COMMAND("osd reweightn " \
 	"reweight osds with {<id>: <weight>,...})",
 	"osd", "rw", "cli,rest")
 COMMAND("osd force-create-pg " \
-	"name=pgid,type=CephPgid,"\
+	"name=pgid,type=CephPgid "\
         "name=sure,type=CephChoices,strings=--yes-i-really-mean-it,req=false", \
 	"force creation of pg <pgid>",
         "osd", "rw", "cli,rest")