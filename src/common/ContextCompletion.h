sunnyku/ceph-ci

common/ContextCompletion: Mutex -> ceph::mutex
Signed-off-by: Sage Weil <sage@redhat.com>

src/common/ContextCompletion.h
@@ -17,7 +17,7 @@ public:
   void finish_op(int r);
 
 private:
-  Mutex m_lock;
+  std::mutex m_lock = ceph::make_mutex("ContextCompletion::m_lock");
   Context *m_ctx;
   bool m_ignore_enoent;
   int m_ret;

athanatos/ceph

src/common/ContextCompletion.h
@@ -17,7 +17,7 @@ public:
   void finish_op(int r);
 
 private:
-  Mutex m_lock;
+  ceph::mutex m_lock = ceph::make_mutex("ContextCompletion::m_lock");
   Context *m_ctx;
   bool m_ignore_enoent;
   int m_ret;