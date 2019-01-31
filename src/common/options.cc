yehudasa/ceph-pr-archive

dmclock: optional enable of rgw dmclock
Adding a configurable rgw_enable_dmclock based on which we enable/disable the
scheduler. A TODO to tackle is enabling the throttler for beast frontend when
dmclock scheduler isn't enabled

Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

src/common/options.cc
@@ -6356,6 +6356,10 @@ std::vector<Option> get_rgw_options() {
     .add_tag("performance")
     .add_see_also("rgw_frontends"),
 
+    Option("rgw_enable_dmclock", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    .set_default(true)
+    .set_description("Enable dmclock scheduler for rgw requests"),
+
     Option("rgw_dmclock_admin_res", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
     .set_default(100.0)
     .set_description("mclock reservation for admin requests")

src/rgw/rgw_dmclock_scheduler.h
@@ -253,6 +253,24 @@ class ClientCounters {
   }
 };
 
+/// a simple wrapper to hold client config. objects needed to construct a
+/// scheduler instance, the primary utility of this being to optionally
+/// construct scheduler only when configured in the frontends.
+class optional_scheduler_ctx {
+  std::optional<ClientConfig> clients;
+  std::optional<ClientCounters> counters;
+public:
+  optional_scheduler_ctx(CephContext *cct) {
+    if(cct->_conf.get_val<bool>("rgw_enable_dmclock")){
+      clients.emplace(ClientConfig(cct));
+      counters.emplace(ClientCounters(cct));
+    }
+  }
+  operator bool() const noexcept { return counters && clients; }
+  // both the get functions below will throw
+  ClientCounters& get_counters() { return counters.value(); }
+  ClientConfig& get_clients() { return clients.value(); }
+};
 } // namespace rgw::dmclock
 
 #endif // RGW_DMCLOCK_SCHEDULER_H

athanatos/ceph

src/common/options.cc
@@ -6721,6 +6721,10 @@ std::vector<Option> get_rgw_options() {
     .add_tag("performance")
     .add_see_also("rgw_frontends"),
 
+    Option("rgw_dmclock_enabled", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    .set_default(true)
+    .set_description("Enable dmclock scheduler for rgw requests"),
+
     Option("rgw_dmclock_admin_res", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
     .set_default(100.0)
     .set_description("mclock reservation for admin requests")

src/rgw/rgw_dmclock_scheduler.h
@@ -253,6 +253,24 @@ class ClientCounters {
   }
 };
 
+/// a simple wrapper to hold client config. objects needed to construct a
+/// scheduler instance, the primary utility of this being to optionally
+/// construct scheduler only when configured in the frontends.
+class optional_scheduler_ctx {
+  std::optional<ClientConfig> clients;
+  std::optional<ClientCounters> counters;
+public:
+  optional_scheduler_ctx(CephContext *cct) {
+    if(cct->_conf.get_val<bool>("rgw_dmclock_enabled")){
+      clients.emplace(ClientConfig(cct));
+      counters.emplace(ClientCounters(cct));
+    }
+  }
+  operator bool() const noexcept { return counters && clients; }
+  // both the get functions below will throw
+  ClientCounters& get_counters() { return counters.value(); }
+  ClientConfig& get_clients() { return clients.value(); }
+};
 } // namespace rgw::dmclock
 
 #endif // RGW_DMCLOCK_SCHEDULER_H