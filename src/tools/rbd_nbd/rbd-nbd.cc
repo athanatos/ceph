athanatos/ceph

commit 5b5079cb3cf2887cd8755c2b6e654a6221e239db

common/config: pass entity_type down into parse_env()
No functional change, but we'll need this shortly.

Signed-off-by: Sage Weil <sage@redhat.com>

src/tools/rbd_nbd/rbd-nbd.cc
@@ -1044,7 +1044,7 @@ static int parse_args(vector<const char*>& args, std::ostream *err_msg,
   } else {
     config.parse_config_files(nullptr, nullptr, 0);
   }
-  config.parse_env();
+  config.parse_env(g_ceph_context->get_module_type());
   config.parse_argv(args);
   cfg->poolname = config.get_val<std::string>("rbd_default_pool");
 

src/common/config.cc
@@ -453,7 +453,8 @@ int md_config_t::parse_config_files(ConfigValues& values,
   return 0;
 }
 
-void md_config_t::parse_env(ConfigValues& values,
+void md_config_t::parse_env(unsigned entity_type,
+			    ConfigValues& values,
 			    const ConfigTracker& tracker,
 			    const char *args_var)
 {

src/common/config.h
@@ -129,7 +129,8 @@ public:
 			 std::ostream *warnings, int flags);
 
   // Absorb config settings from the environment
-  void parse_env(ConfigValues& values, const ConfigTracker& tracker,
+  void parse_env(unsigned entity_type,
+		 ConfigValues& values, const ConfigTracker& tracker,
 		 const char *env_var = "CEPH_ARGS");
 
   // Absorb config settings from argv

src/common/config_proxy.h
@@ -304,9 +304,10 @@ public:
     call_observers(rev_obs);
     return ret;
   }
-  void parse_env(const char *env_var = "CEPH_ARGS") {
+  void parse_env(unsigned entity_type,
+		 const char *env_var = "CEPH_ARGS") {
     std::lock_guard l{lock};
-    config.parse_env(values, obs_mgr, env_var);
+    config.parse_env(entity_type, values, obs_mgr, env_var);
   }
   int parse_argv(std::vector<const char*>& args, int level=CONF_CMDLINE) {
     std::lock_guard l{lock};

src/global/global_init.cc
@@ -137,7 +137,7 @@ void global_pre_init(
   }
 
   // environment variables override (CEPH_ARGS, CEPH_KEYRING)
-  conf.parse_env();
+  conf.parse_env(cct->get_module_type());
 
   // command line (as passed by caller)
   conf.parse_argv(args);

src/libcephfs.cc
@@ -249,7 +249,7 @@ public:
   int conf_parse_env(const char *name)
   {
     auto& conf = cct->_conf;
-    conf.parse_env(name);
+    conf.parse_env(cct->get_module_type(), name);
     conf.apply_changes(nullptr);
     return 0;
   }
@@ -382,7 +382,7 @@ extern "C" int ceph_create(struct ceph_mount_info **cmount, const char * const i
   }
 
   CephContext *cct = common_preinit(iparams, CODE_ENVIRONMENT_LIBRARY, 0);
-  cct->_conf.parse_env(); // environment variables coverride
+  cct->_conf.parse_env(cct->get_module_type()); // environment variables coverride
   cct->_conf.apply_changes(nullptr);
   int ret = ceph_create_with_context(cmount, cct);
   cct->put();

src/librados/librados_c.cc
@@ -92,7 +92,7 @@ static CephContext *rados_create_cct(const char * const clustername,
   CephContext *cct = common_preinit(*iparams, CODE_ENVIRONMENT_LIBRARY, 0);
   if (clustername)
     cct->_conf->cluster = clustername;
-  cct->_conf.parse_env(); // environment variables override
+  cct->_conf.parse_env(cct->get_module_type()); // environment variables override
   cct->_conf.apply_changes(nullptr);
 
   TracepointProvider::initialize<tracepoint_traits>(cct);
@@ -248,7 +248,7 @@ extern "C" int _rados_conf_read_file(rados_t cluster, const char *path_list)
     tracepoint(librados, rados_conf_read_file_exit, ret);
     return ret;
   }
-  conf.parse_env(); // environment variables override
+  conf.parse_env(client->cct->get_module_type()); // environment variables override
 
   conf.apply_changes(nullptr);
   client->cct->_conf.complain_about_parse_errors(client->cct);
@@ -322,7 +322,7 @@ extern "C" int _rados_conf_parse_env(rados_t cluster, const char *env)
   tracepoint(librados, rados_conf_parse_env_enter, cluster, env);
   librados::RadosClient *client = (librados::RadosClient *)cluster;
   auto& conf = client->cct->_conf;
-  conf.parse_env(env);
+  conf.parse_env(client->cct->get_module_type(), env);
   conf.apply_changes(nullptr);
   tracepoint(librados, rados_conf_parse_env_exit, 0);
   return 0;

src/test/librados_test_stub/LibradosTestStub.cc
@@ -102,7 +102,7 @@ void do_out_buffer(string& outbl, char **outbuf, size_t *outbuflen) {
 librados::TestRadosClient *create_rados_client() {
   CephInitParameters iparams(CEPH_ENTITY_TYPE_CLIENT);
   CephContext *cct = common_preinit(iparams, CODE_ENVIRONMENT_LIBRARY, 0);
-  cct->_conf.parse_env();
+  cct->_conf.parse_env(cct->get_module_type());
   cct->_conf.apply_changes(nullptr);
 
   auto rados_client =
@@ -152,7 +152,7 @@ extern "C" int rados_conf_parse_env(rados_t cluster, const char *var) {
   librados::TestRadosClient *client =
     reinterpret_cast<librados::TestRadosClient*>(cluster);
   auto& conf = client->cct()->_conf;
-  conf.parse_env(var);
+  conf.parse_env(client->cct()->get_module_type(), var);
   conf.apply_changes(NULL);
   return 0;
 }
@@ -163,7 +163,7 @@ extern "C" int rados_conf_read_file(rados_t cluster, const char *path) {
   auto& conf = client->cct()->_conf;
   int ret = conf.parse_config_files(path, NULL, 0);
   if (ret == 0) {
-    conf.parse_env();
+    conf.parse_env(client->cct()->get_module_type());
     conf.apply_changes(NULL);
     conf.complain_about_parse_errors(client->cct());
   } else if (ret == -ENOENT) {

commit 17d95657a32825ec65652894e4d54f93804627d8

rbd-nbd: fix segfault for list-mapped command
The global ceph context is not initialized when list-mapped command
is issued.

The regression was introduced by 5b5079cb3cf2887cd8755c2b6e654a6221e239db.

Signed-off-by: Mykola Golub <mgolub@suse.com>

src/tools/rbd_nbd/rbd-nbd.cc
@@ -1044,7 +1044,7 @@ static int parse_args(vector<const char*>& args, std::ostream *err_msg,
   } else {
     config.parse_config_files(nullptr, nullptr, 0);
   }
-  config.parse_env(g_ceph_context->get_module_type());
+  config.parse_env(CEPH_ENTITY_TYPE_CLIENT);
   config.parse_argv(args);
   cfg->poolname = config.get_val<std::string>("rbd_default_pool");
 