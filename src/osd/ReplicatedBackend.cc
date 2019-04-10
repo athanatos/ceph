8e0bd46d101a0391a22fc4d6f1de3de73170a2b3
Samuel Just <sjust@redhat.com>
2019-04-09 18:04:39 -0700

    osd/: unify PGBackend pull error pathways
    This patch narrows the PGBackend -> PrimaryLogPG recovery
    cancel/error interface to on_failed_pull and cancel_pull.
    
    This patch requires careful review.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/osd/ReplicatedBackend.cc
+++ src/osd/ReplicatedBackend.cc
@@ -786,8 +786,9 @@ struct C_ReplicatedBackend_OnPullComplete : GenContext<ThreadPool::TPHandle&> {
       int started = bc->start_pushes(i.hoid, obc, h);
       if (started < 0) {
 	bc->pushing[i.hoid].clear();
-	bc->get_parent()->primary_failed(i.hoid);
-	bc->get_parent()->primary_error(i.hoid, obc->obs.oi.version);
+	bc->get_parent()->on_failed_pull(
+	  { bc->get_parent()->whoami_shard() },
+	  i.hoid, obc->obs.oi.version);
       } else if (!started) {
 	bc->get_parent()->on_global_recover(
 	  i.hoid, i.stat, false);
@@ -2077,7 +2078,10 @@ done:
 	if (!error)
 	  get_parent()->on_global_recover(soid, stat, false);
 	else
-	  get_parent()->on_primary_error(soid, v);
+	  get_parent()->on_failed_pull(
+	    std::set<pg_shard_t>{ get_parent()->whoami_shard() },
+	    soid,
+	    v);
 	pushing.erase(soid);
       } else {
 	// This looks weird, but we erased the current peer and need to remember
@@ -2167,8 +2171,10 @@ void ReplicatedBackend::trim_pushed_data(
 void ReplicatedBackend::_failed_pull(pg_shard_t from, const hobject_t &soid)
 {
   dout(20) << __func__ << ": " << soid << " from " << from << dendl;
-  list<pg_shard_t> fl = { from };
-  get_parent()->failed_push(fl, soid);
+  get_parent()->on_failed_pull(
+    { from },
+    soid,
+    pulling[soid].obc->obs.oi.version);
 
   clear_pull(pulling.find(soid));
 }
