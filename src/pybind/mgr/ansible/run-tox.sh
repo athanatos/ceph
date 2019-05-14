athanatos/ceph

commit e73a9b8b92a84bce4fb1351bb2aa42ed3b62adfe

tests: mgr/tox: make run-tox.sh scripts more robust
Also take into account that, following a recent code change, WITH_PYTHON3 now
gets set to "3" instead of "ON".

Fixes: http://tracker.ceph.com/issues/39323
Signed-off-by: Nathan Cutler <ncutler@suse.com>

src/pybind/mgr/ansible/run-tox.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+function dump_envvars {
+  echo "WITH_PYTHON2: ->$WITH_PYTHON2<-"
+  echo "WITH_PYTHON3: ->$WITH_PYTHON3<-"
+  echo "TOX_PATH: ->$TOX_PATH<-"
+  echo "ENV_LIST: ->$ENV_LIST<-"
+}
+
 # run from ./ or from ../
 : ${MGR_ANSIBLE_VIRTUALENV:=$CEPH_BUILD_DIR/mgr-ansible-virtualenv}
 : ${WITH_PYTHON2:=ON}
-: ${WITH_PYTHON3:=ON}
+: ${WITH_PYTHON3:=3}
 : ${CEPH_BUILD_DIR:=$PWD/.tox}
 test -d ansible && cd ansible
 
@@ -20,10 +27,15 @@ export CEPH_BUILD_DIR=$CEPH_BUILD_DIR
 source ${MGR_ANSIBLE_VIRTUALENV}/bin/activate
 
 if [ "$WITH_PYTHON2" = "ON" ]; then
-  ENV_LIST+="py27"
+  ENV_LIST+="py27,"
 fi
-if [ "$WITH_PYTHON3" = "ON" ]; then
-  ENV_LIST+="py3"
+if [ "$WITH_PYTHON3" = "3" ]; then
+  ENV_LIST+="py3,"
 fi
+# use bash string manipulation to strip off any trailing comma
+ENV_LIST=${ENV_LIST%,}
 
-tox -c ${TOX_PATH} -e ${ENV_LIST}
+tox -c "${TOX_PATH}" -e "${ENV_LIST}"
+TOX_STATUS="$?"
+test "$TOX_STATUS" -ne "0" && dump_envvars
+exit $TOX_STATUS

src/pybind/mgr/dashboard/run-tox.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+function dump_envvars {
+  echo "WITH_PYTHON2: ->$WITH_PYTHON2<-"
+  echo "WITH_PYTHON3: ->$WITH_PYTHON3<-"
+  echo "TOX_PATH: ->$TOX_PATH<-"
+  echo "ENV_LIST: ->$ENV_LIST<-"
+}
+
 # run from ./ or from ../
 : ${CEPH_BUILD_DIR:=$PWD/.tox}
 : ${MGR_DASHBOARD_VIRTUALENV:=$CEPH_BUILD_DIR/mgr-dashboard-virtualenv}
 : ${WITH_PYTHON2:=ON}
-: ${WITH_PYTHON3:=ON}
+: ${WITH_PYTHON3:=3}
 test -d dashboard && cd dashboard
 
 if [ -e tox.ini ]; then
-    TOX_PATH=`readlink -f tox.ini`
+    TOX_PATH=$(readlink -f tox.ini)
 else
-    TOX_PATH=`readlink -f $(dirname $0)/tox.ini`
+    TOX_PATH=$(readlink -f $(dirname $0)/tox.ini)
 fi
 
 # tox.ini will take care of this.
@@ -26,12 +33,17 @@ if [ "$WITH_PYTHON2" = "ON" ]; then
     ENV_LIST+="py27-cov,py27-lint,"
   fi
 fi
-if [ "$WITH_PYTHON3" = "ON" ]; then
+if [ "$WITH_PYTHON3" = "3" ]; then
   if [[ -n "$@" ]]; then
-    ENV_LIST+="py3-run"
+    ENV_LIST+="py3-run,"
   else
     ENV_LIST+="py3-cov,py3-lint"
   fi
 fi
+# use bash string manipulation to strip off any trailing comma
+ENV_LIST=${ENV_LIST%,}
 
-tox -c ${TOX_PATH} -e "$ENV_LIST" "$@"
+tox -c "${TOX_PATH}" -e "${ENV_LIST}"
+TOX_STATUS="$?"
+test "$TOX_STATUS" -ne "0" && dump_envvars
+exit $TOX_STATUS

src/pybind/mgr/insights/run-tox.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+function dump_envvars {
+  echo "WITH_PYTHON2: ->$WITH_PYTHON2<-"
+  echo "WITH_PYTHON3: ->$WITH_PYTHON3<-"
+  echo "TOX_PATH: ->$TOX_PATH<-"
+  echo "ENV_LIST: ->$ENV_LIST<-"
+}
+
 # run from ./ or from ../
 : ${MGR_INSIGHTS_VIRTUALENV:=$CEPH_BUILD_DIR/mgr-insights-virtualenv}
 : ${WITH_PYTHON2:=ON}
-: ${WITH_PYTHON3:=ON}
+: ${WITH_PYTHON3:=3}
 : ${CEPH_BUILD_DIR:=$PWD/.tox}
 test -d insights && cd insights
 
 if [ -e tox.ini ]; then
-    TOX_PATH=`readlink -f tox.ini`
+    TOX_PATH=$(readlink -f tox.ini)
 else
-    TOX_PATH=`readlink -f $(dirname $0)/tox.ini`
+    TOX_PATH=$(readlink -f $(dirname $0)/tox.ini)
 fi
 
 # tox.ini will take care of this.
@@ -20,10 +27,15 @@ export CEPH_BUILD_DIR=$CEPH_BUILD_DIR
 source ${MGR_INSIGHTS_VIRTUALENV}/bin/activate
 
 if [ "$WITH_PYTHON2" = "ON" ]; then
-  ENV_LIST+="py27"
+  ENV_LIST+="py27,"
 fi
-if [ "$WITH_PYTHON3" = "ON" ]; then
-  ENV_LIST+="py3"
+if [ "$WITH_PYTHON3" = "3" ]; then
+  ENV_LIST+="py3,"
 fi
+# use bash string manipulation to strip off any trailing comma
+ENV_LIST=${ENV_LIST%,}
 
-tox -c ${TOX_PATH} -e ${ENV_LIST}
+tox -c "${TOX_PATH}" -e "${ENV_LIST}"
+TOX_STATUS="$?"
+test "$TOX_STATUS" -ne "0" && dump_envvars
+exit $TOX_STATUS

src/pybind/mgr/orchestrator_cli/run-tox.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+function dump_envvars {
+  echo "WITH_PYTHON2: ->$WITH_PYTHON2<-"
+  echo "WITH_PYTHON3: ->$WITH_PYTHON3<-"
+  echo "TOX_PATH: ->$TOX_PATH<-"
+  echo "ENV_LIST: ->$ENV_LIST<-"
+}
+
 # run from ./ or from ../
 : ${MGR_ORCHESTRATOR_CLI_VIRTUALENV:=$CEPH_BUILD_DIR/mgr-orchestrator_cli-virtualenv}
 : ${WITH_PYTHON2:=ON}
-: ${WITH_PYTHON3:=ON}
+: ${WITH_PYTHON3:=3}
 : ${CEPH_BUILD_DIR:=$PWD/.tox}
 test -d orchestrator_cli && cd orchestrator_cli
 
@@ -23,10 +30,17 @@ then
 fi
 
 if [ "$WITH_PYTHON2" = "ON" ]; then
-  ENV_LIST+="py27"
+  ENV_LIST+="py27,"
 fi
-if [ "$WITH_PYTHON3" = "ON" ]; then
-  ENV_LIST+=",py3"
+if [ "$WITH_PYTHON3" = "3" ]; then
+  ENV_LIST+="py3,"
 fi
+ENV_LIST=$(echo "$ENV_LIST" | sed -e 's/,$//')
 
-tox -c ${TOX_PATH} -e ${ENV_LIST}
+# use bash string manipulation to strip off any trailing comma
+ENV_LIST=${ENV_LIST%,}
+
+tox -c "${TOX_PATH}" -e "${ENV_LIST}"
+TOX_STATUS="$?"
+test "$TOX_STATUS" -ne "0" && dump_envvars
+exit $TOX_STATUS

commit 3d41152a28d1e19c87eac5a485066e928273786b

pybind/tox: handle possible WITH_PYTHON3 values other than "3"
WITH_PYTHON3 might be set to "ON" or to the python3 RPM version number
prevailing on the system - e.g. "3", "36"

Fixes: e73a9b8b92a84bce4fb1351bb2aa42ed3b62adfe
Signed-off-by: Nathan Cutler <ncutler@suse.com>

src/pybind/mgr/ansible/run-tox.sh
@@ -29,7 +29,9 @@ source ${MGR_ANSIBLE_VIRTUALENV}/bin/activate
 if [ "$WITH_PYTHON2" = "ON" ]; then
   ENV_LIST+="py27,"
 fi
-if [ "$WITH_PYTHON3" = "3" ]; then
+# WITH_PYTHON3 might be set to "ON" or to the python3 RPM version number
+# prevailing on the system - e.g. "3", "36"
+if [[ "$WITH_PYTHON3" =~ (^3|^ON) ]]; then
   ENV_LIST+="py3,"
 fi
 # use bash string manipulation to strip off any trailing comma

src/pybind/mgr/dashboard/run-tox.sh
@@ -33,7 +33,9 @@ if [ "$WITH_PYTHON2" = "ON" ]; then
     ENV_LIST+="py27-cov,py27-lint,"
   fi
 fi
-if [ "$WITH_PYTHON3" = "3" ]; then
+# WITH_PYTHON3 might be set to "ON" or to the python3 RPM version number
+# prevailing on the system - e.g. "3", "36"
+if [[ "$WITH_PYTHON3" =~ (^3|^ON) ]]; then
   if [[ -n "$@" ]]; then
     ENV_LIST+="py3-run,"
   else

src/pybind/mgr/insights/run-tox.sh
@@ -29,7 +29,9 @@ source ${MGR_INSIGHTS_VIRTUALENV}/bin/activate
 if [ "$WITH_PYTHON2" = "ON" ]; then
   ENV_LIST+="py27,"
 fi
-if [ "$WITH_PYTHON3" = "3" ]; then
+# WITH_PYTHON3 might be set to "ON" or to the python3 RPM version number
+# prevailing on the system - e.g. "3", "36"
+if [[ "$WITH_PYTHON3" =~ (^3|^ON) ]]; then
   ENV_LIST+="py3,"
 fi
 # use bash string manipulation to strip off any trailing comma

src/pybind/mgr/orchestrator_cli/run-tox.sh
@@ -32,11 +32,11 @@ fi
 if [ "$WITH_PYTHON2" = "ON" ]; then
   ENV_LIST+="py27,"
 fi
-if [ "$WITH_PYTHON3" = "3" ]; then
+# WITH_PYTHON3 might be set to "ON" or to the python3 RPM version number
+# prevailing on the system - e.g. "3", "36"
+if [[ "$WITH_PYTHON3" =~ (^3|^ON) ]]; then
   ENV_LIST+="py3,"
 fi
-ENV_LIST=$(echo "$ENV_LIST" | sed -e 's/,$//')
-
 # use bash string manipulation to strip off any trailing comma
 ENV_LIST=${ENV_LIST%,}
 