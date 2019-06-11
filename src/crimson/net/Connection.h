8449809d47126db8c2a0627bc998bf7f63fe95e8
Samuel Just <sjust@redhat.com>
2019-06-10 17:25:45 -0700

    crimson/net/Connection: add priv member for osd to use
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/net/Connection.h
+++ src/crimson/net/Connection.h
@@ -78,7 +78,29 @@ class Connection : public seastar::enable_shared_from_this<Connection> {
   }
   auto get_last_keepalive() const { return last_keepalive; }
   auto get_last_keepalive_ack() const { return last_keepalive_ack; }
+
+  seastar::shared_ptr<Connection> get_shared() {
+    return shared_from_this();
+  }
+
+  struct priv {
+    virtual ~priv() = default;
+  };
+private:
+  unique_ptr<priv> conn_priv;
+public:
+  bool has_priv() const {
+    return conn_priv != nullptr;
+  }
+  void set_priv(unique_ptr<priv> new_priv) {
+    conn_priv = std::move(new_priv);
+  }
+  priv &get_priv() {
+    ceph_assert(conn_priv);
+    return *conn_priv;
+  }
 };
+using ConnectionRef = seastar::shared_ptr<Connection>;
 
 inline ostream& operator<<(ostream& out, const Connection& conn) {
   out << "[";
