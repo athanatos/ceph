e644742a41927f11fcaed3a52404231c4708934f
Samuel Just <sjust@redhat.com>
2019-09-17 12:10:52 -0700

    osd/scheduler: introduce mClockScheduler
    mClockScheduler schedules items based on op_scheduler_class
    with configured mclock parameters.  Items which should be
    scheduled immediately (op_scheduler_class::immediate) are placed
    into a single queue as it's not clear that there's a reason to
    differentiate among them.
    
    A subsequent patch will add support for client provided mclock
    params and dmclock request state.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/osd/scheduler/OpScheduler.cc
+++ src/osd/scheduler/OpScheduler.cc
@@ -33,6 +33,7 @@ OpSchedulerRef make_scheduler(CephContext *cct)
     static const std::string index_lookup[] = { "prioritized",
 						"mclock_opclass",
 						"mclock_client",
+						"mclock_scheduler",
 						"wpq" };
     srand(time(NULL));
     unsigned which = rand() % (sizeof(index_lookup) / sizeof(index_lookup[0]));
@@ -58,6 +59,8 @@ OpSchedulerRef make_scheduler(CephContext *cct)
 	cct,
 	cct
     );
+  } else if (*type == "mclock_scheduler") {
+    return std::make_unique<mClockScheduler>(cct);
   } else if (*type == "wpq" ) {
     // default is 'wpq'
     return std::make_unique<
