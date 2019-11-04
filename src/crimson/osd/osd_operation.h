9a39cc8e8292ea2fe0e7ff623fa08e958f1361b9
Samuel Just <sjust@redhat.com>
2019-11-04 12:40:22 -0800

    working

--- src/crimson/osd/osd_operation.h
+++ src/crimson/osd/osd_operation.h
@@ -60,36 +60,39 @@ class Blocker;
  * Provides an abstraction for registering and unregistering a blocker
  * for the duration of a future becoming available.
  */
-template <typename... T>
-class blocking_future {
+template <typename Fut>
+class blocking_future_detail {
   friend class Operation;
   friend class Blocker;
   Blocker *blocker;
-  seastar::future<T...> fut;
-  blocking_future(Blocker *b, seastar::future<T...> &&f)
+  Fut fut;
+  blocking_future_detail(Blocker *b, Fut &&f)
     : blocker(b), fut(std::move(f)) {}
 
   template <typename... V, typename... U>
-  friend blocking_future<V...> make_ready_blocking_future(U&&... args);
+  friend blocking_future_detail<seastar::future<V...>> make_ready_blocking_future(U&&... args);
 
   template <typename U>
-  friend blocking_future<> join_blocking_futures(U &&u);
+  friend blocking_future_detail<seastar::future<>> join_blocking_futures(U &&u);
 
-  template <typename... U>
-  friend class blocking_future;
+  template <typename U>
+  friend class blocking_future_detail;
 
 public:
   template <typename F>
   auto then(F &&f) && {
-    using futurator = seastar::futurize<std::result_of_t<F(T&&...)>>;
-    return blocking_future<futurator::value_type>(
+    using result = decltype(std::declval<Fut>().then(f));
+    return blocking_future_detail<seastar::futurize_t<result>>(
       blocker,
       std::move(fut).then(std::forward<F>(f)));
   }
 };
 
+template <typename... T>
+using blocking_future = blocking_future_detail<seastar::future<T...>>;
+
 template <typename... V, typename... U>
-blocking_future<V...> make_ready_blocking_future(U&&... args) {
+blocking_future_detail<seastar::future<V...>> make_ready_blocking_future(U&&... args) {
   return blocking_future<V...>(
     nullptr,
     seastar::make_ready_future<V...>(std::forward<U>(args)...));
@@ -106,7 +109,7 @@ protected:
 public:
   template <typename... T>
   blocking_future<T...> make_blocking_future(seastar::future<T...> &&f) {
-    return blocking_future(this, std::move(f));
+    return blocking_future<T...>(this, std::move(f));
   }
 
   void dump(ceph::Formatter *f) const;
