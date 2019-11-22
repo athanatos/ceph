athanatos/ceph

commit b592641412ab3234bb4db90c279394af7b09aa0a

ceph-volume: don't assume SELinux
Expect selinux related binaries to not be present.

Fixes: 80ea740313c9d123520eb4c3f39f87a31dca2562
Fixes: https://tracker.ceph.com/issues/42957

Signed-off-by: Jan Fajerski <jfajerski@suse.com>

src/ceph-volume/ceph_volume/util/system.py
@@ -297,7 +297,13 @@ def set_context(path, recursive=False):
         )
         return
 
-    stdout, stderr, code = process.call(['selinuxenabled'], verbose_on_failure=False)
+    try:
+        stdout, stderr, code = process.call(['selinuxenabled'],
+                                            verbose_on_failure=False)
+    except FileNotFoundError:
+        logger.info('No SELinux found, skipping call to restorecon')
+        return
+
     if code != 0:
         logger.info('SELinux is not enabled, will not call restorecon')
         return

commit 7626526266f966c4ef5a6b8e1803324661d7794b

ceph-volume: python2 raises OSError on Popen with missing binary.
For compatibility alias FileNotFoundError to OSError.

Fixes: b592641412ab3234bb4db90c279394af7b09aa0a
Fixes: https://tracker.ceph.com/issues/42967

Signed-off-by: Jan Fajerski <jfajerski@suse.com>

src/ceph-volume/ceph_volume/util/system.py
@@ -8,6 +8,12 @@ import uuid
 from ceph_volume import process, terminal
 from . import as_string
 
+# python2 has no FileNotFoundError
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = OSError
+
 logger = logging.getLogger(__name__)
 mlogger = terminal.MultiLogger(__name__)
 