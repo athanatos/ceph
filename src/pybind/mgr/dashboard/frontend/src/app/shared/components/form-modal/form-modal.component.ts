samchoraria/ceph-ci

mgr/dashboard: Add support for blinking enclosure LEDs
Fixes: https://tracker.ceph.com/issues/42609

Signed-off-by: Volker Theile <vtheile@suse.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/components/form-modal/form-modal.component.ts
@@ -7,11 +7,17 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 import { CdFormBuilder } from '../../forms/cd-form-builder';
 
 interface CdFormFieldConfig {
-  type: 'textInput';
+  type: 'textInput' | 'select';
   name: string;
   label?: string;
   value?: any;
   required?: boolean;
+  // --- select ---
+  placeholder?: string;
+  options?: Array<{
+    text: string;
+    value: any;
+  }>;
 }
 
 @Component({

athanatos/ceph

src/pybind/mgr/dashboard/frontend/src/app/shared/components/form-modal/form-modal.component.ts
@@ -7,11 +7,17 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 import { CdFormBuilder } from '../../forms/cd-form-builder';
 
 interface CdFormFieldConfig {
-  type: 'textInput';
+  type: 'inputText' | 'select';
   name: string;
   label?: string;
   value?: any;
   required?: boolean;
+  // --- select ---
+  placeholder?: string;
+  options?: Array<{
+    text: string;
+    value: any;
+  }>;
 }
 
 @Component({