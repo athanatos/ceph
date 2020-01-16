8cf94b63c0fb8b1927882160a2128f576640fe3f
Samuel Just <sjust@redhat.com>
2020-01-15 17:20:51 -0800

    working

--- src/crimson/os/seastore/seastore_types.h
+++ src/crimson/os/seastore/seastore_types.h
@@ -7,6 +7,11 @@
 
 namespace crimson::os::seastore {
 
+using checksum_t = uint32_t;
+
+using journal_seq_t = uint64_t;
+constexpr journal_seq_t NO_DELTAS = std::numeric_limits<journal_seq_t>::max();
+
 using segment_id_t = uint32_t;
 using segment_off_t = uint32_t;
 struct paddr_t {
@@ -17,6 +22,9 @@ struct paddr_t {
 using laddr_t = uint64_t;
 
 constexpr laddr_t L_ADDR_NULL = std::numeric_limits<laddr_t>::max();
+constexpr laddr_t L_ADDR_ROOT = std::numeric_limits<laddr_t>::max() - 1;
+constexpr laddr_t L_ADDR_LBAT = std::numeric_limits<laddr_t>::max() - 2;
+
 constexpr paddr_t P_ADDR_NULL = {
   std::numeric_limits<segment_id_t>::max(),
   std::numeric_limits<segment_off_t>::max()
--- src/crimson/os/seastore/transaction_manager.h
+++ src/crimson/os/seastore/transaction_manager.h
@@ -11,22 +11,98 @@
 
 #include "include/ceph_assert.h"
 #include "crimson/os/seastore/seastore_types.h"
-#include "include/buffer_fwd.h"
+#include "include/buffer.h"
 #include "crimson/osd/exceptions.h"
 
 namespace crimson::os::seastore {
 
+/* Could be modified to be omitted for logical blocks */
+struct extent_info_t {
+  // offset inferred from position in list
+  segment_off_t length;
+  laddr_t laddr; // Encodes type for non-logical, debugging info for
+  // logical (lba tree already has a secondary lookup
+  // for checking logical block liveness)
+  // Note, we could omit this for logical blocks, replace laddr
+  // with another segment_off_t for offset, and stash the type tag in the
+  // low bits.
+};
+struct record_header_t {
+  // Fixed portion
+  segment_off_t length;         // block aligned
+  journal_seq_t seq;            // journal sequence for
+  segment_off_t tail;           // overflow for long record metadata
+  checksum_t    full_checksum;  // checksum for full record
+};
+struct record_t : record_header_t {
+  std::vector<ceph::bufferlist> extents; // block aligned, offset and length
+  ceph::bufferlist delta;
+  std::vector<extent_info_t> block_info; // information on each extent
+};
+
 class Transaction {
+  friend class TransactionManager;
+  
+  const paddr_t start;
+
 public:
+  Transaction(paddr_t start);
 };
 using TransactionRef = std::unique_ptr<Transaction>;
 
 class TransactionManager {
 public:
   TransactionRef create_transaction() {
-    return std::make_unique<Transaction>();
+    return std::make_unique<Transaction>(paddr_t{0,0});
   }
 
+  using read_ertr = crimson::errorator <
+    crimson::ct_error::input_output_error
+    >;
+
+  /**
+   * Add journaled representation of overwrite of physical offsets
+   * [addr, addr + bl.size()).
+   *
+   * As read_ertr indicates, may perform reads.
+   *
+   * @param trans [in,out] current transaction
+   * @param paddr [in]     aligned physical block address
+   * @param laddr [in]     aligned logical block address or type tag for
+   *                       non-logical blocks
+   * @param delta [in]     logical mutation -- encoding of mutation
+   *                       to block
+   * @param bytes [in]     vector of offset, bufferlist pairs representing
+   *                       physical mutation to blocks
+   * @return future for completion
+   */
+  read_ertr::future<> add_delta(
+    TransactionRef &trans,
+    paddr_t paddr,
+    laddr_t laddr,
+    ceph::bufferlist delta,
+    ceph::bufferlist bl);
+
+  /**
+   * Adds a new block containing bl.
+   *
+   * @param trans [in,out] current transaction
+   * @param laddr [in] laddr of block or type code for non-logical
+   * @param bl    [in] contents of new bloc, must be page aligned and have
+   *                   aligned length.
+   *
+   * @return 
+   */
+  paddr_t add_block(
+    TransactionRef &trans,
+    laddr_t laddr,
+    ceph::bufferlist bl);
+
+  /**
+   * Atomically submits transaction to persistence and cache
+   *
+   * @param 
+   */
   using submit_transaction_ertr = crimson::errorator<
     crimson::ct_error::input_output_error>;
   submit_transaction_ertr::future<> submit_transaction() {
