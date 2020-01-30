05477e81794452ed8098fdeacbe905fcb23fcb63
Samuel Just <sjust@redhat.com>
2020-01-30 11:08:19 -0800

    working

---
+++ src/crimson/os/seastore/lba_manager.cc
@@ -0,0 +1,13 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "crimson/os/seastore/lba_manager.h"
+
+namespace crimson::os::seastore::lba_manager {
+
+LBAManagerRef create_lba_manager() {
+  return LBAManagerRef(); /* TODO */
+}
+
+};
+
