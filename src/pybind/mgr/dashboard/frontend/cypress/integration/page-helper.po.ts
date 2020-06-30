athanatos/ceph

commit f77c4118c7559c38540d13f48b6994178f7fdb9d

mgr/dashboard: Use ng-bootstrap for Dropdown
Fixes: https://tracker.ceph.com/issues/45758
Fixes: https://tracker.ceph.com/issues/45566

Signed-off-by: Tiago Melo <tmelo@suse.com>

src/pybind/mgr/dashboard/frontend/cypress/integration/page-helper.po.ts
@@ -214,10 +214,10 @@ export abstract class PageHelper {
   filterTable(name: string, option: string) {
     this.waitDataTableToLoad();
 
-    cy.get('.tc_filter_name > a').click();
+    cy.get('.tc_filter_name > button').click();
     cy.contains(`.tc_filter_name .dropdown-item`, name).click();
 
-    cy.get('.tc_filter_option > a').click();
+    cy.get('.tc_filter_option > button').click();
     cy.contains(`.tc_filter_option .dropdown-item`, option).click();
   }
 
@@ -245,11 +245,11 @@ export abstract class PageHelper {
 
     // Clicks on table Delete button
     cy.get('.table-actions button.dropdown-toggle').first().click(); // open submenu
-    cy.get('li.delete a').click(); // click on "delete" menu item
+    cy.get('button.delete').click(); // click on "delete" menu item
 
     // Confirms deletion
-    cy.get('.custom-control-label').click();
-    cy.contains('button', 'Delete').click();
+    cy.get('cd-modal .custom-control-label').click();
+    cy.contains('cd-modal button', 'Delete').click();
 
     // Wait for modal to close
     cy.get('cd-modal').should('not.exist');

src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table-actions/table-actions.component.html
@@ -1,5 +1,4 @@
-<div class="btn-group"
-     dropdown>
+<div class="btn-group">
   <ng-container *ngIf="getCurrentButton() as action">
     <button type="button"
             class="btn btn-{{btnColor}}"
@@ -7,33 +6,36 @@
             (click)="useClickAction(action)"
             [routerLink]="useRouterLink(action)"
             [preserveFragment]="action.preserveFragment ? '' : null">
-      <i [ngClass]="[action.icon]"></i><span>{{ action.name }}</span>
+
+      <i [ngClass]="[action.icon]"></i>
+      <span>{{ action.name }}</span>
     </button>
   </ng-container>
-  <button type="button"
-          dropdownToggle
-          *ngIf="showDropDownActions()"
-          class="btn btn-{{btnColor}} dropdown-toggle dropdown-toggle-split">
-    <ng-container *ngIf="dropDownOnly">{{ dropDownOnly }} </ng-container>
-    <span *ngIf="!dropDownOnly"
-          class="sr-only"></span>
-  </button>
-  <ul *dropdownMenu
-      class="dropdown-menu"
-      role="menu">
-    <ng-container *ngFor="let action of dropDownActions">
-      <li role="menuitem"
-          class="{{ toClassName(action['name']) }}"
-          data-toggle="tooltip"
-          title="{{ useDisableDesc(action) }}">
-        <a class="dropdown-item"
-           (click)="useClickAction(action)"
-           [routerLink]="useRouterLink(action)"
-           [preserveFragment]="action.preserveFragment ? '' : null"
-           [ngClass]="{'disabled': disableSelectionAction(action)}">
-          <i [ngClass]="[action.icon]"></i><span>{{ action.name }}</span>
-        </a>
-      </li>
-    </ng-container>
-  </ul>
+  <div class="btn-group"
+       ngbDropdown
+       placement="bottom-right"
+       role="group"
+       aria-label="Button group with nested dropdown">
+    <button class="btn btn-{{btnColor}} dropdown-toggle-split"
+            *ngIf="showDropDownActions()"
+            ngbDropdownToggle>
+      <ng-container *ngIf="dropDownOnly">{{ dropDownOnly }} </ng-container>
+      <span *ngIf="!dropDownOnly"
+            class="sr-only"></span>
+    </button>
+    <div class="dropdown-menu"
+         ngbDropdownMenu>
+      <ng-container *ngFor="let action of dropDownActions">
+        <button ngbDropdownItem
+                class="{{ toClassName(action['name']) }}"
+                (click)="useClickAction(action)"
+                [routerLink]="useRouterLink(action)"
+                [preserveFragment]="action.preserveFragment ? '' : null"
+                [disabled]="disableSelectionAction(action)">
+          <i [ngClass]="[action.icon]"></i>
+          <span>{{ action.name }}</span>
+        </button>
+      </ng-container>
+    </div>
+  </div>
 </div>

src/pybind/mgr/dashboard/frontend/cypress/integration/block/images.po.ts
@@ -51,7 +51,7 @@ export class ImagesPageHelper extends PageHelper {
 
     // click on the drop down and selects the move to trash option
     cy.get('.table-actions button.dropdown-toggle').first().click();
-    cy.get('li.move-to-trash').click();
+    cy.get('button.move-to-trash').click();
 
     cy.contains('button', 'Move Image').should('be.visible').click();
 
@@ -71,12 +71,12 @@ export class ImagesPageHelper extends PageHelper {
     cy.contains('button', 'Restore').click();
 
     // wait for pop-up to be visible (checks for title of pop-up)
-    cy.get('#name').should('be.visible');
+    cy.get('cd-modal #name').should('be.visible');
 
     // If a new name for the image is passed, it changes the name of the image
     if (newName !== undefined) {
       // click name box and send new name
-      cy.get('#name').clear().type(newName);
+      cy.get('cd-modal #name').clear().type(newName);
     }
 
     cy.contains('button', 'Restore Image').click();

src/pybind/mgr/dashboard/frontend/src/app/app.module.ts
@@ -12,8 +12,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtModule } from '@auth0/angular-jwt';
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
-
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -45,7 +43,6 @@ export function jwtTokenGetter() {
     CoreModule,
     SharedModule,
     CephModule,
-    BsDropdownModule.forRoot(),
     JwtModule.forRoot({
       config: {
         tokenGetter: jwtTokenGetter

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/block.module.ts
@@ -7,7 +7,6 @@ import { NgbNavModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { TreeModule } from 'angular-tree-component';
 import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { ActionLabels, URLVerbs } from '../../shared/constants/app.constants';
@@ -59,7 +58,6 @@ import { RbdTrashRestoreModalComponent } from './rbd-trash-restore-modal/rbd-tra
     FormsModule,
     ReactiveFormsModule,
     NgbNavModule,
-    BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
     NgbTooltipModule,
     ModalModule.forRoot(),

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/iscsi-target-details/iscsi-target-details.component.ts
@@ -280,7 +280,7 @@ export class IscsiTargetDetailsComponent implements OnChanges, OnInit {
       const tempData = this.metadata[node.data.cdId] || {};
 
       if (node.data.cdId === 'root') {
-        this.detailTable?.toggleColumn({ target: { name: 'default', checked: true } });
+        this.detailTable?.toggleColumn({ prop: 'default', isHidden: true });
         this.data = _.map(this.settings.target_default_controls, (value, key) => {
           value = this.format(value);
           return {
@@ -300,7 +300,7 @@ export class IscsiTargetDetailsComponent implements OnChanges, OnInit {
           });
         }
       } else if (node.data.cdId.toString().startsWith('disk_')) {
-        this.detailTable?.toggleColumn({ target: { name: 'default', checked: true } });
+        this.detailTable?.toggleColumn({ prop: 'default', isHidden: true });
         this.data = _.map(this.settings.disk_default_controls[tempData.backstore], (value, key) => {
           value = this.format(value);
           return {
@@ -326,7 +326,7 @@ export class IscsiTargetDetailsComponent implements OnChanges, OnInit {
           }
         });
       } else {
-        this.detailTable?.toggleColumn({ target: { name: 'default', checked: false } });
+        this.detailTable?.toggleColumn({ prop: 'default', isHidden: false });
         this.data = _.map(tempData, (value, key) => {
           return {
             displayName: key,

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/mirroring/daemon-list/daemon-list.component.spec.ts
@@ -2,8 +2,6 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
 import { configureTestBed, i18nProviders } from '../../../../../testing/unit-test-helper';
 import { SharedModule } from '../../../../shared/shared.module';
 import { MirrorHealthColorPipe } from '../mirror-health-color.pipe';
@@ -15,12 +13,7 @@ describe('DaemonListComponent', () => {
 
   configureTestBed({
     declarations: [DaemonListComponent, MirrorHealthColorPipe],
-    imports: [
-      BrowserAnimationsModule,
-      SharedModule,
-      BsDropdownModule.forRoot(),
-      HttpClientTestingModule
-    ],
+    imports: [BrowserAnimationsModule, SharedModule, HttpClientTestingModule],
     providers: i18nProviders
   });
 

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/mirroring/image-list/image-list.component.spec.ts
@@ -3,7 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgbNavModule, NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { configureTestBed, i18nProviders } from '../../../../../testing/unit-test-helper';
 import { SharedModule } from '../../../../shared/shared.module';
@@ -19,7 +18,6 @@ describe('ImageListComponent', () => {
     imports: [
       BrowserAnimationsModule,
       SharedModule,
-      BsDropdownModule.forRoot(),
       NgbNavModule,
       NgbProgressbarModule,
       HttpClientTestingModule

commit 4ba09dcd180c98905d306d8a03a785e73df999bc

mgr/dashboard: Fix title of disabled table actions
Fix regression introduced in f77c4118c7559c38540d13f48b6994178f7fdb9d.
Title attribute was unintentionally removed from the table actions.

Fixes: https://tracker.ceph.com/issues/46153

Signed-off-by: Tiago Melo <tmelo@suse.com>

src/pybind/mgr/dashboard/frontend/cypress/integration/page-helper.po.ts
@@ -57,6 +57,7 @@ export abstract class PageHelper {
       this.navigateTo();
       this.getFirstTableCell(name).click();
     }
+    cy.contains('Creating...').should('not.exist');
     cy.contains('button', 'Edit').click();
     this.expectBreadcrumbText('Edit');
   }

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-snapshot-list/rbd-snapshot-actions.model.ts
@@ -93,7 +93,7 @@ export class RbdSnapshotActionsModel {
   }
 
   getCloneDisableDesc(featuresName: string[]): string | undefined {
-    if (!featuresName.includes('layering')) {
+    if (!featuresName?.includes('layering')) {
       return this.i18n('Parent image must support Layering');
     }
 

src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table-actions/table-actions.component.html
@@ -1,12 +1,12 @@
 <div class="btn-group">
   <ng-container *ngIf="getCurrentButton() as action">
     <button type="button"
+            title="{{ useDisableDesc(action) }}"
             class="btn btn-{{btnColor}}"
             [ngClass]="{'disabled': disableSelectionAction(action)}"
             (click)="useClickAction(action)"
             [routerLink]="useRouterLink(action)"
             [preserveFragment]="action.preserveFragment ? '' : null">
-
       <i [ngClass]="[action.icon]"></i>
       <span>{{ action.name }}</span>
     </button>
@@ -28,6 +28,7 @@
       <ng-container *ngFor="let action of dropDownActions">
         <button ngbDropdownItem
                 class="{{ toClassName(action['name']) }}"
+                title="{{ useDisableDesc(action) }}"
                 (click)="useClickAction(action)"
                 [routerLink]="useRouterLink(action)"
                 [preserveFragment]="action.preserveFragment ? '' : null"

src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table-actions/table-actions.component.scss
@@ -1,4 +1,4 @@
-.dropdown-menu > .disabled > a {
+button.disabled {
   cursor: default !important;
   pointer-events: auto;
 }