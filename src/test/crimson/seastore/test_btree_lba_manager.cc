ce2ccb620d3ab5278dd1b1e2bb2855ef2a6e1b52
Samuel Just <sjust@redhat.com>
2020-08-26 14:50:16 -0700

    crimson/os/seastore/transaction: introduce lazy transactions
    This way, we can do a bulk scan of the store without building up
    an unbounded amount of state in Transaction::read_set.  Note that
    such transactions will not be snapshot isolated.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/test/crimson/seastore/test_btree_lba_manager.cc
+++ src/test/crimson/seastore/test_btree_lba_manager.cc
@@ -131,6 +131,14 @@ struct btree_lba_manager_test :
     return t;
   }
 
+  auto create_lazy_transaction() {
+    auto t = test_transaction_t{
+      make_lazy_transaction(),
+      test_lba_mappings
+    };
+    return t;
+  }
+
   void submit_test_transaction(test_transaction_t t) {
     submit_transaction(std::move(t.t)).get0();
     test_lba_mappings.swap(t.mappings);
