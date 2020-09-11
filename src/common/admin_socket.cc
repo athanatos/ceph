athanatos/ceph

commit dc75434ebbf0e30b9ac56b83c3712e00c4dd4d5d

common: fix validate type CephBool cause 'invalid command json'
Fixes: https://tracker.ceph.com/issues/46985
Signed-off-by: Yanhu Cao <gmayyyha@gmail.com>

src/common/admin_socket.cc
@@ -512,8 +512,7 @@ void AdminSocket::execute_command(
   }
 
   // make sure one of the registered commands with this prefix validates.
-  while (!validate_cmd(m_cct, p->second.desc, cmdmap, errss)) {
-    ++p;
+  if (!validate_cmd(m_cct, p->second.desc, cmdmap, errss)) {
     if (p->first != prefix) {
       delete f;
       return on_finish(-EINVAL, "invalid command json", empty);

commit 187dfac3c3992b0ff80fbd63537d39f7b1c57303

common/admin_socket: always validate the parameters
this change addresses the regression introduced by
dc75434ebbf0e30b9ac56b83c3712e00c4dd4d5d.

before this change, we reply the client with a failure only if
both the prefix and the arguments do not match.

after this change, both the prefix and argument are checked.

Fixes: https://tracker.ceph.com/issues/47179
Signed-off-by: Kefu Chai <kchai@redhat.com>

src/common/admin_socket.cc
@@ -540,19 +540,19 @@ AdminSocket::find_matched_hook(std::string& prefix,
   // hook takes the same lock that was held during calls to
   // register/unregister, and set in_hook to allow unregister to wait for us
   // before removing this hook.
-  auto p = hooks.find(prefix);
-  if (p == hooks.cend()) {
+  auto [hooks_begin, hooks_end] = hooks.equal_range(prefix);
+  if (hooks_begin == hooks_end) {
     return {ENOENT, nullptr};
   }
   // make sure one of the registered commands with this prefix validates.
   stringstream errss;
-  if (!validate_cmd(m_cct, p->second.desc, cmdmap, errss)) {
-    if (p->first != prefix) {
-      return {EINVAL, nullptr};
+  for (auto hook = hooks_begin; hook != hooks_end; ++hook) {
+    if (validate_cmd(m_cct, hook->second.desc, cmdmap, errss)) {
+      in_hook = true;
+      return {0, hook->second.hook};
     }
   }
-  in_hook = true;
-  return {0, p->second.hook};
+  return {EINVAL, nullptr};
 }
 
 void AdminSocket::queue_tell_command(cref_t<MCommand> m)