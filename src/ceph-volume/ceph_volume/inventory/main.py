athanatos/ceph

commit 84e2d57e31470d9e75dd5c0fffb8e8f6be7a5e0b

ceph-volume inventory: add option to filter unwanted devices
Some device we never want to pass to the batch subcommand. For now this
includes devices that have a partition or are mounted on the machine.
One goal is to filter the root device, so it is not included on a batch
command and thus would contribute to its implicit sizing calculation.

Signed-off-by: Jan Fajerski <jfajerski@suse.com>

src/ceph-volume/ceph_volume/inventory/main.py
@@ -31,17 +31,24 @@ class Inventory(object):
             default='plain',
             help='Output format',
         )
+        parser.add_argument(
+            '--filter-for-batch',
+            action='store_true',
+            help=('Filter devices unsuitable to pass to an OSD service spec, '
+                  'no effect when <path> is passed'),
+            default=False,
+        )
         self.args = parser.parse_args(self.argv)
         if self.args.path:
             self.format_report(Device(self.args.path))
         else:
-            self.format_report(Devices())
+            self.format_report(Devices(self.args.filter_for_batch))
 
     def get_report(self):
         if self.args.path:
             return Device(self.args.path).json_report()
         else:
-            return Devices().json_report()
+            return Devices(self.args.filter_for_batch).json_report()
 
     def format_report(self, inventory):
         if self.args.format == 'json':

src/ceph-volume/ceph_volume/util/device.py
@@ -4,7 +4,7 @@ import os
 from functools import total_ordering
 from ceph_volume import sys_info, process
 from ceph_volume.api import lvm
-from ceph_volume.util import disk
+from ceph_volume.util import disk, system
 from ceph_volume.util.lsmdisk import LSMDisk
 from ceph_volume.util.constants import ceph_disk_guids
 
@@ -27,13 +27,15 @@ class Devices(object):
     A container for Device instances with reporting
     """
 
-    def __init__(self, devices=None):
+    def __init__(self, devices=None, filter_for_batch=False):
         if not sys_info.devices:
             sys_info.devices = disk.get_devices()
         self.devices = [Device(k) for k in
                             sys_info.devices.keys()]
+        if filter_for_batch:
+            self.devices = [d for d in self.devices if d.available_lvm_batch]
 
-    def pretty_report(self, all=True):
+    def pretty_report(self):
         output = [
             report_template.format(
                 dev='Device Path',
@@ -497,6 +499,14 @@ class Device(object):
             rejected.append('LVM detected')
 
         return len(rejected) == 0, rejected
+
+    @property
+    def available_lvm_batch(self):
+        if self.sys_api.get("partitions"):
+            return False
+        if system.device_is_mounted(self.path):
+            return False
+        return self.is_device or self.is_lv
 
 
 class CephDiskDevice(object):

src/ceph-volume/ceph_volume/tests/devices/lvm/test_common.py
@@ -1,4 +1,3 @@
-import pytest
 from ceph_volume.devices.lvm import common
 
 

src/pybind/mgr/cephadm/module.py
@@ -1541,7 +1541,7 @@ To check that the host is reachable:
             out, err, code = self._run_cephadm(
                 host, 'osd',
                 'ceph-volume',
-                ['--', 'inventory', '--format=json'])
+                ['--', 'inventory', '--format=json', '--filter-for-batch'])
             if code:
                 return 'host %s ceph-volume inventory returned %d: %s' % (
                     host, code, err)

commit ce2a54f86f2554d7faa9a4f41bb5a0b32ab19fc4

ceph-volume: pass filter_for_batch as keyword argument
This PR also removes an unused ctor argument in the Devices class.

Fixes: 84e2d57e31470d9e75dd5c0fffb8e8f6be7a5e0b

Signed-off-by: Jan Fajerski <jfajerski@suse.com>

src/ceph-volume/ceph_volume/inventory/main.py
@@ -42,13 +42,13 @@ class Inventory(object):
         if self.args.path:
             self.format_report(Device(self.args.path))
         else:
-            self.format_report(Devices(self.args.filter_for_batch))
+            self.format_report(Devices(filter_for_batch=self.args.filter_for_batch))
 
     def get_report(self):
         if self.args.path:
             return Device(self.args.path).json_report()
         else:
-            return Devices(self.args.filter_for_batch).json_report()
+            return Devices(filter_for_batch=self.args.filter_for_batch).json_report()
 
     def format_report(self, inventory):
         if self.args.format == 'json':

src/ceph-volume/ceph_volume/util/device.py
@@ -27,7 +27,7 @@ class Devices(object):
     A container for Device instances with reporting
     """
 
-    def __init__(self, devices=None, filter_for_batch=False):
+    def __init__(self, filter_for_batch=False):
         if not sys_info.devices:
             sys_info.devices = disk.get_devices()
         self.devices = [Device(k) for k in