0e24544c9e992548ddc95c64e76b1cca22ea4f16
Samuel Just <sjust@redhat.com>
2021-02-02 03:57:28 +0000

    working

--- src/crimson/os/seastore/onode_manager/staged-fltree/fltree_onode_manager.h
+++ src/crimson/os/seastore/onode_manager/staged-fltree/fltree_onode_manager.h
@@ -4,27 +4,99 @@
 #pragma once
 
 #include "crimson/os/seastore/onode_manager.h"
+#include "crimson/os/seastore/onode_manager/staged-fltree/node_extent_manager/seastore.h"
+#include "crimson/os/seastore/onode_manager/staged-fltree/value.h"
 #include "crimson/os/seastore/onode_manager/staged-fltree/tree.h"
 
 namespace crimson::os::seastore::onode {
 
+struct FLTreeOnode : Onode, Value {
+  static constexpr value_magic_t HEADER_MAGIC = value_magic_t::ONODE;
+
+  enum class status_t {
+    NEW,
+    STABLE,
+    MUTATED,
+    DELETED
+  } status;
+
+  // TODO: can be a union
+  using cursor_t = Btree<FLTreeOnode>::Cursor;
+  std::optional<cursor_t> cursor;
+  std::optional<onode_layout_t> mutated;
+
+  FLTreeOnode(FLTreeOnode&&) = default;
+  FLTreeOnode& operator=(FLTreeOnode&&) = delete;
+
+  FLTreeOnode(const FLTreeOnode&) = default;
+  FLTreeOnode& operator=(const FLTreeOnode&) = delete;
+
+  template <typename... T>
+  FLTreeOnode(T&&... args) : Value(std::forward<T>(args)...) {}
+
+  struct Recorder : public ValueDeltaRecorder {
+    Recorder(bufferlist &bl) : ValueDeltaRecorder(bl) {}
+    
+    value_magic_t get_header_magic() const final {
+      return value_magic_t::ONODE;
+    }
+    
+    void apply_value_delta(
+      ceph::bufferlist::const_iterator&,
+      NodeExtentMutable&,
+      laddr_t) final {
+      // TODO
+    }
+  };
+
+  onode_layout_t &get_layout() const final {
+    return *(static_cast<onode_layout_t*>(nullptr));
+  }
+#if 0
+  void mark_mutable() final {
+    // TODO
+  }
+#endif
+};
+
+using OnodeTree = Btree<FLTreeOnode>;
+
 class FLTreeOnodeManager : public crimson::os::seastore::OnodeManager {
+  OnodeTree tree;
+
 public:
-  open_ertr::future<OnodeRef> get_or_create_onode(
+  FLTreeOnodeManager(TransactionManager &tm) :
+    tree(std::make_unique<SeastoreNodeExtentManager>(
+	   tm, laddr_t{})) {}
+    
+  
+  get_or_create_onode_ret get_or_create_onode(
     Transaction &trans,
     const ghobject_t &hoid) final {
-    return open_ertr::make_ready_future<OnodeRef>();
+    return tree.find(trans, hoid
+    ).safe_then([this, &trans, &hoid](auto cursor)
+		-> get_or_create_onode_ret {
+      
+      return seastar::make_ready_future<OnodeRef>(
+	OnodeRef(new FLTreeOnode(cursor.value()))
+      );
+    }).handle_error(
+      get_or_create_onode_ertr::pass_further{},
+      crimson::ct_error::assert_all{
+	"Invalid error in FLTreeOnodeManager::get_or_create_onode"
+      }
+    );
   }
-  open_ertr::future<std::vector<OnodeRef>> get_or_create_onodes(
+  get_or_create_onodes_ret get_or_create_onodes(
     Transaction &trans,
     const std::vector<ghobject_t> &hoids) final {
-    return open_ertr::make_ready_future<std::vector<OnodeRef>>();
+    return seastar::make_ready_future<std::vector<OnodeRef>>();
   }
 
-  write_ertr::future<> write_dirty(
+  write_dirty_ret write_dirty(
     Transaction &trans,
     const std::vector<OnodeRef> &onodes) final {
-    return write_ertr::now();
+    return seastar::now();
   }
 
   ~FLTreeOnodeManager();
