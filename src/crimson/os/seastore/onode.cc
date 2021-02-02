50d3846aee081a2717f70b7634d231b705413f83
Samuel Just <sjust@redhat.com>
2021-02-02 13:06:55 -0800

    working

--- src/crimson/os/seastore/onode.cc
+++ src/crimson/os/seastore/onode.cc
@@ -10,7 +10,7 @@ std::ostream& operator<<(std::ostream &out, const Onode &rhs)
 {
   auto &layout = rhs.get_layout();
   return out << "Onode("
-	     << "size=" << layout.size
+	     << "size=" << static_cast<uint32_t>(layout.size)
 	     << ")";
 }
 
