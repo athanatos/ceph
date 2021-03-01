sunilangadi2/ceph-ci

mgr/cephadm: Adapt cephadm to new orch interface
Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

src/pybind/mgr/cephadm/tests/fixtures.py
@@ -14,7 +14,7 @@ import pytest
 
 from cephadm import CephadmOrchestrator
 from cephadm.services.osd import RemoveUtil, OSD
-from orchestrator import raise_if_exception, Completion, HostSpec
+from orchestrator import raise_if_exception, HostSpec, OrchResult
 from tests import mock
 
 
@@ -87,30 +87,8 @@ def osd_obj():
 
 
 def wait(m, c):
-    # type: (CephadmOrchestrator, Completion) -> Any
-    m.process([c])
-
-    try:
-        import pydevd  # if in debugger
-        in_debug = True
-    except ImportError:
-        in_debug = False
-
-    if in_debug:
-        while True:    # don't timeout
-            if c.is_finished:
-                raise_if_exception(c)
-                return c.result
-            time.sleep(0.1)
-    else:
-        for i in range(30):
-            if i % 10 == 0:
-                m.process([c])
-            if c.is_finished:
-                raise_if_exception(c)
-                return c.result
-            time.sleep(0.1)
-    assert False, "timeout" + str(c._state)
+    # type: (CephadmOrchestrator, OrchResult) -> Any
+    return raise_if_exception(c)
 
 
 @contextmanager

athanatos/ceph

src/pybind/mgr/cephadm/tests/fixtures.py
@@ -1,4 +1,3 @@
-import time
 import fnmatch
 from contextlib import contextmanager
 
@@ -12,7 +11,7 @@ except ImportError:
     pass
 
 from cephadm import CephadmOrchestrator
-from orchestrator import raise_if_exception, Completion, HostSpec
+from orchestrator import raise_if_exception, OrchResult, HostSpec
 from tests import mock
 
 
@@ -64,31 +63,8 @@ def with_cephadm_module(module_options=None, store=None):
 
 
 def wait(m, c):
-    # type: (CephadmOrchestrator, Completion) -> Any
-    m.process([c])
-
-    try:
-        # if in debugger
-        import pydevd  # noqa: F401
-        in_debug = True
-    except ImportError:
-        in_debug = False
-
-    if in_debug:
-        while True:    # don't timeout
-            if c.is_finished:
-                raise_if_exception(c)
-                return c.result
-            time.sleep(0.1)
-    else:
-        for i in range(30):
-            if i % 10 == 0:
-                m.process([c])
-            if c.is_finished:
-                raise_if_exception(c)
-                return c.result
-            time.sleep(0.1)
-    assert False, "timeout" + str(c._state)
+    # type: (CephadmOrchestrator, OrchResult) -> Any
+    return raise_if_exception(c)
 
 
 @contextmanager