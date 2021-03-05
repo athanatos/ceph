8f2f427d413712bc552a3d19964e6162a2e56361
Samuel Just <sjust@redhat.com>
2021-03-04 23:03:38 -0800

    crimson/os/seastore/.../btree_extent_manager: update formatting, comments
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/extentmap_manager/btree/btree_extentmap_manager.h
+++ src/crimson/os/seastore/extentmap_manager/btree/btree_extentmap_manager.h
@@ -38,26 +38,43 @@ class BtreeExtentMapManager : public ExtentMapManager {
    */
   using get_root_ertr = TransactionManager::read_extent_ertr;
   using get_root_ret = get_root_ertr::future<ExtMapNodeRef>;
-  get_root_ret get_extmap_root(const extmap_root_t &extmap_root, Transaction &t);
+  get_root_ret get_extmap_root(
+    const extmap_root_t &extmap_root,
+    Transaction &t);
 
   using insert_lextent_ertr = TransactionManager::read_extent_ertr;
-  using insert_lextent_ret = insert_lextent_ertr::future<extent_mapping_t >;
-  insert_lextent_ret insert_lextent(extmap_root_t &extmap_root, Transaction &t,
-                                    ExtMapNodeRef extent, objaddr_t lo,
-                                    lext_map_val_t val);
+  using insert_lextent_ret = insert_lextent_ertr::future<extent_mapping_t>;
+  insert_lextent_ret insert_lextent(
+    extmap_root_t &extmap_root,
+    Transaction &t,
+    ExtMapNodeRef extent,
+    objaddr_t lo,
+    lext_map_val_t val);
 
 public:
-  explicit BtreeExtentMapManager(TransactionManager &tm);
+  explicit BtreeExtentMapManager(
+    TransactionManager &tm);
 
-  initialize_extmap_ret initialize_extmap(Transaction &t) final;
+  initialize_extmap_ret initialize_extmap(
+    Transaction &t) final;
 
-  find_lextent_ret find_lextent(const extmap_root_t &extmap_root, Transaction &t, objaddr_t lo, extent_len_t len) final;
+  find_lextent_ret find_lextent(
+    const extmap_root_t &extmap_root,
+    Transaction &t,
+    objaddr_t lo,
+    extent_len_t len) final;
 
-  add_lextent_ret add_lextent(extmap_root_t &extmap_root, Transaction &t, objaddr_t lo, lext_map_val_t val) final;
+  add_lextent_ret add_lextent(
+    extmap_root_t &extmap_root,
+    Transaction &t,
+    objaddr_t lo,
+    lext_map_val_t val) final;
 
-  rm_lextent_ret rm_lextent(extmap_root_t &extmap_root, Transaction &t, objaddr_t lo, lext_map_val_t val) final;
-
-
+  rm_lextent_ret rm_lextent(
+    extmap_root_t &extmap_root,
+    Transaction &t,
+    objaddr_t lo,
+    lext_map_val_t val) final;
 };
 using BtreeExtentMapManagerRef = std::unique_ptr<BtreeExtentMapManager>;
 
