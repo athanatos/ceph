thotz/ceph-ci

crimson/osd: use interruptible future in the PG layer
Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

src/crimson/osd/ec_backend.cc
@@ -12,7 +12,7 @@ ECBackend::ECBackend(shard_id_t shard,
   // todo
 }
 
-ECBackend::ll_read_errorator::future<ceph::bufferlist>
+ECBackend::interruptible_ll_read_errorator::future<ceph::bufferlist>
 ECBackend::_read(const hobject_t& hoid,
                  const uint64_t off,
                  const uint64_t len,
@@ -22,7 +22,7 @@ ECBackend::_read(const hobject_t& hoid,
   return seastar::make_ready_future<bufferlist>();
 }
 
-seastar::future<crimson::osd::acked_peers_t>
+ECBackend::interruptible_future<crimson::osd::acked_peers_t>
 ECBackend::_submit_transaction(std::set<pg_shard_t>&& pg_shards,
                                const hobject_t& hoid,
                                ceph::os::Transaction&& txn,

src/crimson/osd/ec_backend.h
@@ -22,11 +22,9 @@ public:
   }
   void on_actingset_changed(peering_info_t pi) final {}
 private:
-  ll_read_errorator::future<ceph::bufferlist> _read(const hobject_t& hoid,
-                                                    uint64_t off,
-                                                    uint64_t len,
-                                                    uint32_t flags) override;
-  seastar::future<crimson::osd::acked_peers_t>
+  interruptible_ll_read_errorator::future<ceph::bufferlist>
+  _read(const hobject_t& hoid, uint64_t off, uint64_t len, uint32_t flags) override;
+  interruptible_future<crimson::osd::acked_peers_t>
   _submit_transaction(std::set<pg_shard_t>&& pg_shards,
 		      const hobject_t& hoid,
 		      ceph::os::Transaction&& txn,

src/crimson/osd/ops_executer.cc
@@ -27,7 +27,7 @@ namespace {
 
 namespace crimson::osd {
 
-OpsExecuter::call_errorator::future<> OpsExecuter::do_op_call(OSDOp& osd_op)
+OpsExecuter::interruptible_call_errorator::future<> OpsExecuter::do_op_call(OSDOp& osd_op)
 {
   std::string cname, mname;
   ceph::bufferlist indata;
@@ -149,7 +149,7 @@ static watch_info_t create_watch_info(const OSDOp& osd_op,
   };
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_watch(
+OpsExecuter::interruptible_watch_errorator::future<> OpsExecuter::do_op_watch_subop_watch(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -191,7 +191,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_watch(
     });
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_reconnect(
+OpsExecuter::interruptible_watch_errorator::future<> OpsExecuter::do_op_watch_subop_reconnect(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -206,7 +206,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_reconnect(
   }
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_unwatch(
+OpsExecuter::interruptible_watch_errorator::future<> OpsExecuter::do_op_watch_subop_unwatch(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -246,7 +246,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_unwatch(
     });
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_ping(
+OpsExecuter::interruptible_watch_errorator::future<> OpsExecuter::do_op_watch_subop_ping(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -270,7 +270,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_ping(
   return seastar::now();
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch(
+OpsExecuter::interruptible_watch_errorator::future<> OpsExecuter::do_op_watch(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -303,7 +303,7 @@ static uint64_t get_next_notify_id(epoch_t e)
   return (((uint64_t)e) << 32) | ((uint64_t)(next_notify_id++));
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify(
+OpsExecuter::interruptible_watch_errorator::future<> OpsExecuter::do_op_notify(
   OSDOp& osd_op,
   const ObjectState& os)
 {
@@ -362,7 +362,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify(
   });
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify_ack(
+OpsExecuter::interruptible_watch_errorator::future<> OpsExecuter::do_op_notify_ack(
   OSDOp& osd_op,
   const ObjectState& os)
 {
@@ -420,7 +420,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify_ack(
   });
 }
 
-OpsExecuter::osd_op_errorator::future<>
+OpsExecuter::interruptible_errorated_future<OpsExecuter::osd_op_errorator>
 OpsExecuter::execute_op(OSDOp& osd_op)
 {
   // TODO: dispatch via call table?
@@ -458,7 +458,8 @@ OpsExecuter::execute_op(OSDOp& osd_op)
       return backend.get_xattrs(os, osd_op);
     });
   case CEPH_OSD_OP_RMXATTR:
-    return do_write_op([&osd_op] (auto& backend, auto& os, auto& txn) {
+    return do_write_op(
+      [&osd_op] (auto& backend, auto& os, auto& txn) {
       return backend.rm_xattr(os, osd_op, txn);
     }, true);
   case CEPH_OSD_OP_CREATE:
@@ -641,7 +642,7 @@ static inline std::unique_ptr<const PGLSFilter> get_pgls_filter(
   return filter;
 }
 
-static seastar::future<hobject_t> pgls_filter(
+static PG::interruptible_future<hobject_t> pgls_filter(
   const PGLSFilter& filter,
   const PGBackend& backend,
   const hobject_t& sobj)
@@ -649,7 +650,7 @@ static seastar::future<hobject_t> pgls_filter(
   if (const auto xattr = filter.get_xattr(); !xattr.empty()) {
     logger().debug("pgls_filter: filter is interested in xattr={} for obj={}",
                    xattr, sobj);
-    return backend.getxattr(sobj, xattr).safe_then(
+    return backend.getxattr(sobj, xattr).safe_then_interruptible(
       [&filter, sobj] (ceph::bufferptr bp) {
         logger().debug("pgls_filter: got xvalue for obj={}", sobj);
 
@@ -674,7 +675,7 @@ static seastar::future<hobject_t> pgls_filter(
   }
 }
 
-static seastar::future<ceph::bufferlist> do_pgnls_common(
+static PG::interruptible_future<ceph::bufferlist> do_pgnls_common(
   const hobject_t& pg_start,
   const hobject_t& pg_end,
   const PGBackend& backend,
@@ -690,8 +691,9 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
     throw std::invalid_argument("outside of PG bounds");
   }
 
-  return backend.list_objects(lower_bound, limit).then(
-    [&backend, filter, nspace](auto&& ret) {
+  return backend.list_objects(lower_bound, limit).then_interruptible(
+    [&backend, filter, nspace](auto&& ret)
+    -> PG::interruptible_future<std::tuple<std::vector<hobject_t>, hobject_t>> {
       auto& [objects, next] = ret;
       auto in_my_namespace = [&nspace](const hobject_t& obj) {
         using crimson::common::local_conf;
@@ -703,7 +705,8 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
           return obj.get_namespace() == nspace;
         }
       };
-      auto to_pglsed = [&backend, filter] (const hobject_t& obj) {
+      auto to_pglsed = [&backend, filter] (const hobject_t& obj)
+		       -> PG::interruptible_future<hobject_t> {
         // this transformation looks costly. However, I don't have any
         // reason to think PGLS* operations are critical for, let's say,
         // general performance.
@@ -732,7 +735,7 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
             std::tuple<std::vector<hobject_t>, hobject_t>>(
               std::make_tuple(std::move(items), std::move(next)));
       });
-  }).then(
+  }).then_interruptible(
     [pg_end] (auto&& ret) {
       auto& [items, next] = ret;
       auto is_matched = [] (const auto& obj) {
@@ -756,7 +759,7 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
   });
 }
 
-static seastar::future<> do_pgnls(
+static PG::interruptible_future<> do_pgnls(
   const PG& pg,
   const std::string& nspace,
   OSDOp& osd_op)
@@ -777,13 +780,13 @@ static seastar::future<> do_pgnls(
                          nspace,
                          osd_op.op.pgls.count,
                          nullptr /* no filter */)
-    .then([&osd_op](bufferlist bl) {
+    .then_interruptible([&osd_op](bufferlist bl) {
       osd_op.outdata = std::move(bl);
       return seastar::now();
   });
 }
 
-static seastar::future<> do_pgnls_filtered(
+static PG::interruptible_future<> do_pgnls_filtered(
   const PG& pg,
   const std::string& nspace,
   OSDOp& osd_op)
@@ -821,14 +824,14 @@ static seastar::future<> do_pgnls_filtered(
                              nspace,
                              osd_op.op.pgls.count,
                              filter.get())
-        .then([&osd_op](bufferlist bl) {
+        .then_interruptible([&osd_op](bufferlist bl) {
           osd_op.outdata = std::move(bl);
           return seastar::now();
       });
   });
 }
 
-static seastar::future<ceph::bufferlist> do_pgls_common(
+static PG::interruptible_future<ceph::bufferlist> do_pgls_common(
   const hobject_t& pg_start,
   const hobject_t& pg_end,
   const PGBackend& backend,
@@ -845,12 +848,13 @@ static seastar::future<ceph::bufferlist> do_pgls_common(
   }
 
   using entries_t = decltype(pg_ls_response_t::entries);
-  return backend.list_objects(lower_bound, limit).then(
+  return backend.list_objects(lower_bound, limit).then_interruptible(
     [&backend, filter, nspace](auto&& ret) {
       auto& [objects, next] = ret;
-      return seastar::when_all(
-        seastar::map_reduce(std::move(objects),
-          [&backend, filter, nspace](const hobject_t& obj) {
+      return PG::interruptor::when_all(
+        PG::interruptor::map_reduce(std::move(objects),
+          [&backend, filter, nspace](const hobject_t& obj)
+	  -> PG::interruptible_future<hobject_t>{
             if (obj.get_namespace() == nspace) {
               if (filter) {
                 return pgls_filter(*filter, backend, obj);
@@ -869,7 +873,7 @@ static seastar::future<ceph::bufferlist> do_pgls_common(
             return entries;
           }),
         seastar::make_ready_future<hobject_t>(next));
-    }).then([pg_end](auto&& ret) {
+    }).then_interruptible([pg_end](auto&& ret) {
       auto entries = std::move(std::get<0>(ret).get0());
       auto next = std::move(std::get<1>(ret).get0());
       pg_ls_response_t response;
@@ -883,7 +887,7 @@ static seastar::future<ceph::bufferlist> do_pgls_common(
   });
 }
 
-static seastar::future<> do_pgls(
+static PG::interruptible_future<> do_pgls(
    const PG& pg,
    const std::string& nspace,
    OSDOp& osd_op)
@@ -905,13 +909,13 @@ static seastar::future<> do_pgls(
 			nspace,
 			osd_op.op.pgls.count,
 			nullptr /* no filter */)
-    .then([&osd_op](bufferlist bl) {
+    .then_interruptible([&osd_op](bufferlist bl) {
       osd_op.outdata = std::move(bl);
       return seastar::now();
     });
 }
 
-static seastar::future<> do_pgls_filtered(
+static PG::interruptible_future<> do_pgls_filtered(
   const PG& pg,
   const std::string& nspace,
   OSDOp& osd_op)
@@ -949,14 +953,14 @@ static seastar::future<> do_pgls_filtered(
                             nspace,
                             osd_op.op.pgls.count,
                             filter.get())
-        .then([&osd_op](bufferlist bl) {
+        .then_interruptible([&osd_op](bufferlist bl) {
           osd_op.outdata = std::move(bl);
           return seastar::now();
       });
   });
 }
 
-seastar::future<>
+PgOpsExecuter::interruptible_future<>
 PgOpsExecuter::execute_op(OSDOp& osd_op)
 {
   logger().warn("handling op {}", ceph_osd_op_name(osd_op.op.op));

athanatos/ceph

src/crimson/osd/ec_backend.cc
@@ -12,7 +12,7 @@ ECBackend::ECBackend(shard_id_t shard,
   // todo
 }
 
-ECBackend::ll_read_errorator::future<ceph::bufferlist>
+ECBackend::ll_read_ierrorator::future<ceph::bufferlist>
 ECBackend::_read(const hobject_t& hoid,
                  const uint64_t off,
                  const uint64_t len,
@@ -22,7 +22,7 @@ ECBackend::_read(const hobject_t& hoid,
   return seastar::make_ready_future<bufferlist>();
 }
 
-seastar::future<crimson::osd::acked_peers_t>
+ECBackend::interruptible_future<crimson::osd::acked_peers_t>
 ECBackend::_submit_transaction(std::set<pg_shard_t>&& pg_shards,
                                const hobject_t& hoid,
                                ceph::os::Transaction&& txn,

src/crimson/osd/ec_backend.h
@@ -22,11 +22,9 @@ public:
   }
   void on_actingset_changed(peering_info_t pi) final {}
 private:
-  ll_read_errorator::future<ceph::bufferlist> _read(const hobject_t& hoid,
-                                                    uint64_t off,
-                                                    uint64_t len,
-                                                    uint32_t flags) override;
-  seastar::future<crimson::osd::acked_peers_t>
+  ll_read_ierrorator::future<ceph::bufferlist>
+  _read(const hobject_t& hoid, uint64_t off, uint64_t len, uint32_t flags) override;
+  interruptible_future<crimson::osd::acked_peers_t>
   _submit_transaction(std::set<pg_shard_t>&& pg_shards,
 		      const hobject_t& hoid,
 		      ceph::os::Transaction&& txn,

src/crimson/osd/ops_executer.cc
@@ -28,7 +28,7 @@ namespace {
 
 namespace crimson::osd {
 
-OpsExecuter::call_errorator::future<> OpsExecuter::do_op_call(OSDOp& osd_op)
+OpsExecuter::call_ierrorator::future<> OpsExecuter::do_op_call(OSDOp& osd_op)
 {
   std::string cname, mname;
   ceph::bufferlist indata;
@@ -150,7 +150,7 @@ static watch_info_t create_watch_info(const OSDOp& osd_op,
   };
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_watch(
+OpsExecuter::watch_ierrorator::future<> OpsExecuter::do_op_watch_subop_watch(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -192,7 +192,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_watch(
     });
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_reconnect(
+OpsExecuter::watch_ierrorator::future<> OpsExecuter::do_op_watch_subop_reconnect(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -207,7 +207,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_reconnect(
   }
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_unwatch(
+OpsExecuter::watch_ierrorator::future<> OpsExecuter::do_op_watch_subop_unwatch(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -247,7 +247,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_unwatch(
     });
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_ping(
+OpsExecuter::watch_ierrorator::future<> OpsExecuter::do_op_watch_subop_ping(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -271,7 +271,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch_subop_ping(
   return seastar::now();
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_watch(
+OpsExecuter::watch_ierrorator::future<> OpsExecuter::do_op_watch(
   OSDOp& osd_op,
   ObjectState& os,
   ceph::os::Transaction& txn)
@@ -304,7 +304,7 @@ static uint64_t get_next_notify_id(epoch_t e)
   return (((uint64_t)e) << 32) | ((uint64_t)(next_notify_id++));
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify(
+OpsExecuter::watch_ierrorator::future<> OpsExecuter::do_op_notify(
   OSDOp& osd_op,
   const ObjectState& os)
 {
@@ -363,7 +363,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify(
   });
 }
 
-OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify_ack(
+OpsExecuter::watch_ierrorator::future<> OpsExecuter::do_op_notify_ack(
   OSDOp& osd_op,
   const ObjectState& os)
 {
@@ -421,7 +421,7 @@ OpsExecuter::watch_errorator::future<> OpsExecuter::do_op_notify_ack(
   });
 }
 
-OpsExecuter::osd_op_errorator::future<>
+OpsExecuter::interruptible_errorated_future<OpsExecuter::osd_op_errorator>
 OpsExecuter::execute_op(OSDOp& osd_op)
 {
   // TODO: dispatch via call table?
@@ -459,7 +459,8 @@ OpsExecuter::execute_op(OSDOp& osd_op)
       return backend.get_xattrs(os, osd_op);
     });
   case CEPH_OSD_OP_RMXATTR:
-    return do_write_op([&osd_op] (auto& backend, auto& os, auto& txn) {
+    return do_write_op(
+      [&osd_op] (auto& backend, auto& os, auto& txn) {
       return backend.rm_xattr(os, osd_op, txn);
     }, true);
   case CEPH_OSD_OP_CREATE:
@@ -642,7 +643,7 @@ static inline std::unique_ptr<const PGLSFilter> get_pgls_filter(
   return filter;
 }
 
-static seastar::future<hobject_t> pgls_filter(
+static PG::interruptible_future<hobject_t> pgls_filter(
   const PGLSFilter& filter,
   const PGBackend& backend,
   const hobject_t& sobj)
@@ -650,7 +651,7 @@ static seastar::future<hobject_t> pgls_filter(
   if (const auto xattr = filter.get_xattr(); !xattr.empty()) {
     logger().debug("pgls_filter: filter is interested in xattr={} for obj={}",
                    xattr, sobj);
-    return backend.getxattr(sobj, xattr).safe_then(
+    return backend.getxattr(sobj, xattr).safe_then_interruptible(
       [&filter, sobj] (ceph::bufferptr bp) {
         logger().debug("pgls_filter: got xvalue for obj={}", sobj);
 
@@ -675,7 +676,7 @@ static seastar::future<hobject_t> pgls_filter(
   }
 }
 
-static seastar::future<ceph::bufferlist> do_pgnls_common(
+static PG::interruptible_future<ceph::bufferlist> do_pgnls_common(
   const hobject_t& pg_start,
   const hobject_t& pg_end,
   const PGBackend& backend,
@@ -691,8 +692,9 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
     throw std::invalid_argument("outside of PG bounds");
   }
 
-  return backend.list_objects(lower_bound, limit).then(
-    [&backend, filter, nspace](auto&& ret) {
+  return backend.list_objects(lower_bound, limit).then_interruptible(
+    [&backend, filter, nspace](auto&& ret)
+    -> PG::interruptible_future<std::tuple<std::vector<hobject_t>, hobject_t>> {
       auto& [objects, next] = ret;
       auto in_my_namespace = [&nspace](const hobject_t& obj) {
         using crimson::common::local_conf;
@@ -704,7 +706,8 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
           return obj.get_namespace() == nspace;
         }
       };
-      auto to_pglsed = [&backend, filter] (const hobject_t& obj) {
+      auto to_pglsed = [&backend, filter] (const hobject_t& obj)
+		       -> PG::interruptible_future<hobject_t> {
         // this transformation looks costly. However, I don't have any
         // reason to think PGLS* operations are critical for, let's say,
         // general performance.
@@ -733,7 +736,7 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
             std::tuple<std::vector<hobject_t>, hobject_t>>(
               std::make_tuple(std::move(items), std::move(next)));
       });
-  }).then(
+  }).then_interruptible(
     [pg_end] (auto&& ret) {
       auto& [items, next] = ret;
       auto is_matched = [] (const auto& obj) {
@@ -757,7 +760,7 @@ static seastar::future<ceph::bufferlist> do_pgnls_common(
   });
 }
 
-static seastar::future<> do_pgnls(
+static PG::interruptible_future<> do_pgnls(
   const PG& pg,
   const std::string& nspace,
   OSDOp& osd_op)
@@ -778,13 +781,13 @@ static seastar::future<> do_pgnls(
                          nspace,
                          osd_op.op.pgls.count,
                          nullptr /* no filter */)
-    .then([&osd_op](bufferlist bl) {
+    .then_interruptible([&osd_op](bufferlist bl) {
       osd_op.outdata = std::move(bl);
       return seastar::now();
   });
 }
 
-static seastar::future<> do_pgnls_filtered(
+static PG::interruptible_future<> do_pgnls_filtered(
   const PG& pg,
   const std::string& nspace,
   OSDOp& osd_op)
@@ -822,14 +825,14 @@ static seastar::future<> do_pgnls_filtered(
                              nspace,
                              osd_op.op.pgls.count,
                              filter.get())
-        .then([&osd_op](bufferlist bl) {
+        .then_interruptible([&osd_op](bufferlist bl) {
           osd_op.outdata = std::move(bl);
           return seastar::now();
       });
   });
 }
 
-static seastar::future<ceph::bufferlist> do_pgls_common(
+static PG::interruptible_future<ceph::bufferlist> do_pgls_common(
   const hobject_t& pg_start,
   const hobject_t& pg_end,
   const PGBackend& backend,
@@ -846,12 +849,13 @@ static seastar::future<ceph::bufferlist> do_pgls_common(
   }
 
   using entries_t = decltype(pg_ls_response_t::entries);
-  return backend.list_objects(lower_bound, limit).then(
+  return backend.list_objects(lower_bound, limit).then_interruptible(
     [&backend, filter, nspace](auto&& ret) {
       auto& [objects, next] = ret;
-      return seastar::when_all(
-        seastar::map_reduce(std::move(objects),
-          [&backend, filter, nspace](const hobject_t& obj) {
+      return PG::interruptor::when_all(
+        PG::interruptor::map_reduce(std::move(objects),
+          [&backend, filter, nspace](const hobject_t& obj)
+	  -> PG::interruptible_future<hobject_t>{
             if (obj.get_namespace() == nspace) {
               if (filter) {
                 return pgls_filter(*filter, backend, obj);
@@ -870,7 +874,7 @@ static seastar::future<ceph::bufferlist> do_pgls_common(
             return entries;
           }),
         seastar::make_ready_future<hobject_t>(next));
-    }).then([pg_end](auto&& ret) {
+    }).then_interruptible([pg_end](auto&& ret) {
       auto entries = std::move(std::get<0>(ret).get0());
       auto next = std::move(std::get<1>(ret).get0());
       pg_ls_response_t response;
@@ -884,7 +888,7 @@ static seastar::future<ceph::bufferlist> do_pgls_common(
   });
 }
 
-static seastar::future<> do_pgls(
+static PG::interruptible_future<> do_pgls(
    const PG& pg,
    const std::string& nspace,
    OSDOp& osd_op)
@@ -906,13 +910,13 @@ static seastar::future<> do_pgls(
 			nspace,
 			osd_op.op.pgls.count,
 			nullptr /* no filter */)
-    .then([&osd_op](bufferlist bl) {
+    .then_interruptible([&osd_op](bufferlist bl) {
       osd_op.outdata = std::move(bl);
       return seastar::now();
     });
 }
 
-static seastar::future<> do_pgls_filtered(
+static PG::interruptible_future<> do_pgls_filtered(
   const PG& pg,
   const std::string& nspace,
   OSDOp& osd_op)
@@ -950,14 +954,14 @@ static seastar::future<> do_pgls_filtered(
                             nspace,
                             osd_op.op.pgls.count,
                             filter.get())
-        .then([&osd_op](bufferlist bl) {
+        .then_interruptible([&osd_op](bufferlist bl) {
           osd_op.outdata = std::move(bl);
           return seastar::now();
       });
   });
 }
 
-seastar::future<>
+PgOpsExecuter::interruptible_future<>
 PgOpsExecuter::execute_op(OSDOp& osd_op)
 {
   logger().warn("handling op {}", ceph_osd_op_name(osd_op.op.op));