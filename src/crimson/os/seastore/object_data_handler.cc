5ce8cf3d575775d9e916472ba328335cb83e532f
Samuel Just <sjust@redhat.com>
2021-04-09 18:04:59 -0700

    crimson/os/seastore/object_data_handler: add logic for handling object data
    Signed-off-by: Samuel Just <sjust@redhat.com>

---
+++ src/crimson/os/seastore/object_data_handler.cc
@@ -0,0 +1,478 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include <utility>
+#include <functional>
+
+#include "crimson/os/seastore/object_data_handler.h"
+
+namespace crimson::os::seastore {
+
+/**
+ * MAX_OBJECT_SIZE
+ *
+ * For now, we allocated a fixe region of laddr space of size MAX_OBJECT_SIZE
+ * for any object.  In the future, once we have the ability to remap logical
+ * mappings (necessary for clone), we'll add the ability to grow and shrink
+ * these regions and remove this assumption.
+ */
+static constexpr extent_len_t MAX_OBJECT_SIZE = 16<<20;
+#define assert_aligned(x) ceph_assert(((x)%ctx.tm.get_block_size()) == 0)
+
+using context_t = ObjectDataHandler::context_t;
+using get_ertr = ObjectDataHandler::write_ertr;
+auto read_pin(
+  context_t ctx,
+  LBAPinRef pin) {
+  return ctx.tm.pin_to_extent<TestBlock>(
+    ctx.t,
+    std::move(pin)
+  ).handle_error(
+    get_ertr::pass_further{},
+    crimson::ct_error::assert_all{ "read_pin: invalid error" }
+  );
+}
+
+struct extent_to_write_t {
+  laddr_t addr = L_ADDR_NULL;
+  extent_len_t len;
+  std::optional<bufferlist> to_write;
+
+  extent_to_write_t() = default;
+  extent_to_write_t(const extent_to_write_t &) = default;
+  extent_to_write_t(extent_to_write_t &&) = default;
+
+  extent_to_write_t(laddr_t addr, bufferlist to_write)
+    : addr(addr), len(to_write.length()), to_write(to_write) {}
+
+  extent_to_write_t(laddr_t addr, extent_len_t len)
+    : addr(addr), len(len) {}
+};
+using extent_to_write_list_t = std::list<extent_to_write_t>;
+
+ObjectDataHandler::write_ret do_removals(
+  context_t ctx,
+  lba_pin_list_t &pins)
+{
+  return crimson::do_for_each(
+    pins.begin(),
+    pins.end(),
+    [ctx](auto &pin) {
+      return ctx.tm.dec_ref(
+	ctx.t,
+	pin->get_laddr()
+      ).safe_then(
+	[](auto){},
+	ObjectDataHandler::write_ertr::pass_further{},
+	crimson::ct_error::assert_all{
+	  "object_data_handler::do_removals invalid error"
+	}
+      );
+    });
+}
+
+ObjectDataHandler::write_ret do_insertions(
+  context_t ctx,
+  extent_to_write_list_t &to_write)
+{
+  return crimson::do_for_each(
+    to_write.begin(),
+    to_write.end(),
+    [ctx](auto &region) {
+      if (region.to_write) {
+	assert_aligned(region.addr);
+	assert_aligned(region.len);
+	ceph_assert(region.len == region.to_write->length());
+	return ctx.tm.alloc_extent<TestBlock>(
+	  ctx.t,
+	  region.addr,
+	  region.len
+	).safe_then([ctx, &region](auto extent) {
+	  ceph_assert(extent->get_laddr() == region.addr);
+	  ceph_assert(extent->get_length() == region.len);
+	  auto iter = region.to_write->cbegin();
+	  iter.copy(region.len, extent->get_bptr().c_str());
+	  return ObjectDataHandler::write_ertr::now();
+	});
+      } else {
+	return ctx.tm.reserve_region(
+	  ctx.t,
+	  region.addr,
+	  region.len
+	).safe_then([&region](auto pin) {
+	  ceph_assert(pin->get_length() == region.len);
+	  ceph_assert(pin->get_laddr() == region.addr);
+	  return ObjectDataHandler::write_ertr::now();
+	});
+      }
+    });
+}
+
+using get_ret_bare = std::pair<
+  std::optional<extent_to_write_t>,
+  std::optional<bufferptr>>;
+using get_ret = get_ertr::future<get_ret_bare>;
+get_ret get_left(context_t ctx, LBAPinRef &pin, laddr_t offset)
+{
+  const auto pin_offset = pin->get_laddr();
+  assert_aligned(pin_offset);
+  ceph_assert(offset >= pin_offset);
+  if (offset == pin_offset) {
+    return get_ertr::make_ready_future<get_ret_bare>(
+      std::nullopt,
+      std::nullopt);
+  } else if (pin->get_paddr().is_zero()) {
+    auto aligned_offset = p2align(offset, (uint64_t)ctx.tm.get_block_size());
+    assert_aligned(aligned_offset);
+    ceph_assert(aligned_offset <= offset);
+    auto zero_extent_len = aligned_offset - pin_offset;
+    assert_aligned(zero_extent_len);
+    auto zero_prepend_len = offset - zero_extent_len;
+    return get_ertr::make_ready_future<get_ret_bare>(
+      (zero_extent_len == 0
+       ? std::nullopt
+       : std::make_optional(extent_to_write_t(pin_offset, zero_extent_len))),
+      bufferptr(ceph::buffer::create(zero_prepend_len, 0))
+    );
+  } else {
+    auto to_prepend = pin->get_length() - (offset - pin->get_laddr());
+    return read_pin(ctx, pin->duplicate()
+    ).safe_then([to_prepend](auto extent) {
+      return get_ertr::make_ready_future<get_ret_bare>(
+	std::nullopt,
+	bufferptr(extent->get_bptr(), 0, to_prepend));
+    });
+  }
+};
+
+get_ret get_right(context_t ctx, LBAPinRef &pin, laddr_t end)
+{
+  const auto pin_end = pin->get_laddr() + pin->get_length();
+  assert_aligned(pin_end);
+  ceph_assert(pin_end >= end);
+  if (end == pin_end) {
+    return get_ertr::make_ready_future<get_ret_bare>(
+      std::nullopt,
+      std::nullopt);
+  } else if (pin->get_paddr().is_zero()) {
+    auto aligned_end = p2roundup(end, (uint64_t)ctx.tm.get_block_size());
+    assert_aligned(aligned_end);
+    ceph_assert(aligned_end >= end);
+    auto zero_suffix_len = aligned_end - end;
+    auto zero_extent_len = (pin->get_laddr() + pin->get_length()) - aligned_end;
+    assert_aligned(zero_extent_len);
+    return get_ertr::make_ready_future<get_ret_bare>(
+      (zero_extent_len == 0
+       ? std::nullopt
+       : std::make_optional(extent_to_write_t(aligned_end, zero_extent_len))),
+      bufferptr(ceph::buffer::create(zero_suffix_len, 0))
+    );
+  } else {
+    auto to_postpend = pin->get_length() - (pin_end - end);
+    return read_pin(ctx, pin->duplicate()
+    ).safe_then([to_postpend](auto extent) {
+      return get_ertr::make_ready_future<get_ret_bare>(
+	std::nullopt,
+	bufferptr(extent->get_bptr(), 0, to_postpend));
+    });
+  }
+};
+
+template <typename F>
+auto with_object_data(
+  ObjectDataHandler::context_t ctx,
+  F &&f)
+{
+  return seastar::do_with(
+    ctx.onode.get_layout().object_data.get(),
+    std::forward<F>(f),
+    [&](auto &object_data, auto &f) {
+      return std::invoke(f, object_data
+      ).safe_then([&] {
+	if (object_data.must_update()) {
+	  ctx.onode.get_mutable_layout(ctx.t).object_data.update(object_data);
+	}
+	return seastar::now();
+      });
+    });
+}
+
+ObjectDataHandler::write_ret ObjectDataHandler::prepare_data_reservation(
+  context_t ctx,
+  object_data_t &object_data,
+  extent_len_t size)
+{
+  ceph_assert(size <= MAX_OBJECT_SIZE);
+  if (!object_data.is_null()) {
+    ceph_assert(object_data.get_reserved_data_len() == MAX_OBJECT_SIZE);
+    return write_ertr::now();
+  } else {
+    return ctx.tm.reserve_region(
+      ctx.t,
+      0 /* TODO -- pass hint based on object hash */,
+      MAX_OBJECT_SIZE
+    ).safe_then([size, &object_data](auto pin) {
+      ceph_assert(pin->get_length() == MAX_OBJECT_SIZE);
+      object_data.update_reserved(
+	pin->get_laddr(),
+	pin->get_length());
+      return write_ertr::now();
+    });
+  }
+}
+
+ObjectDataHandler::clear_ret ObjectDataHandler::trim_data_reservation(
+  context_t ctx, object_data_t &object_data, extent_len_t size)
+{
+  ceph_assert(!object_data.is_null());
+  ceph_assert(size <= object_data.get_reserved_data_len());
+  return seastar::do_with(
+    lba_pin_list_t(),
+    extent_to_write_list_t(),
+    [ctx, size, &object_data](auto &pins, auto &to_write) {
+      return ctx.tm.get_pins(
+	ctx.t,
+	object_data.get_reserved_data_base() + size,
+	object_data.get_reserved_data_len() - size
+      ).safe_then([ctx, size, &pins, &object_data](auto _pins) {
+	_pins.swap(pins);
+	ceph_assert(pins.size());
+	return get_left(
+	  ctx, pins.front(),
+	  object_data.get_reserved_data_base() + size);
+      }).safe_then([ctx, size, &to_write, &object_data](auto p) {
+	if (p.first) {
+	  if (p.first->to_write) {
+	    // tail had data, fill remainder with zeros
+	    to_write.emplace_back(std::move(*p.first));
+	    to_write.emplace_back(
+	      extent_to_write_t{
+		object_data.get_reserved_data_base() + size,
+		object_data.get_reserved_data_len() - size
+	      });
+	  } else {
+	    // tail was zero, merge with remainder, fill with zeros
+	    to_write.emplace_back(
+	      extent_to_write_t{
+		p.first->addr,
+		object_data.get_reserved_data_len() -
+		static_cast<extent_len_t>(
+		  p.first->addr - object_data.get_reserved_data_base())
+	      });
+	  }
+	  return do_insertions(ctx, to_write);
+	} else {
+	  return clear_ertr::now();
+	}
+      }).safe_then([ctx, &pins] {
+	return do_removals(ctx, pins);
+      }).safe_then([size, &object_data] {
+	if (size == 0) {
+	  object_data.clear();
+	} else {
+	  object_data.update_len(size);
+	}
+	return ObjectDataHandler::clear_ertr::now();
+      });
+    });
+}
+
+extent_to_write_list_t get_buffers(laddr_t offset, bufferlist &bl)
+{
+  auto ret = extent_to_write_list_t();
+  ret.emplace_back(offset, bl);
+  return ret;
+};
+
+ObjectDataHandler::write_ret ObjectDataHandler::overwrite(
+  context_t ctx,
+  laddr_t _offset,
+  bufferlist &&bl,
+  lba_pin_list_t &&pins)
+{
+  return seastar::do_with(
+    _offset,
+    std::move(bl),
+    std::move(pins),
+    extent_to_write_list_t(),
+    [this, ctx](laddr_t &offset, auto &bl, auto &pins, auto &to_write) {
+      ceph_assert(pins.size() >= 1);
+      auto pin_begin = pins.front()->get_laddr();
+      ceph_assert(pin_begin <= offset);
+      auto pin_end = pins.back()->get_laddr() + pins.back()->get_length();
+      ceph_assert(pin_end >= (offset > bl.length()));
+
+      return get_left(
+	ctx,
+	pins.front(),
+	offset
+      ).safe_then([this, ctx, &offset, &bl, &pins, &to_write](auto p) {
+	auto &[left_extent, headptr] = p;
+	if (left_extent) to_write.push_front(std::move(*left_extent));
+	if (headptr) {
+	  bufferlist newbl;
+	  newbl.append(*headptr);
+	  newbl.append(bl);
+	  bl.swap(newbl);
+	  offset -= headptr->length();
+	  assert_aligned(offset);
+	}
+	return get_right(
+	  ctx,
+	  pins.back(),
+	  offset + bl.length());
+      }).safe_then([this, ctx, &offset, &bl, &pins, &to_write](auto p) {
+	auto &[right_extent, tailptr] = p;
+	if (tailptr) {
+	  bl.append(*tailptr);
+	  assert_aligned(bl.length());
+	}
+	to_write.splice(to_write.end(), get_buffers(offset, bl));
+	if (right_extent) {
+	  to_write.push_back(std::move(*right_extent));
+	}
+	return write_ertr::now();
+      }).safe_then([this, ctx, &pins] {
+	return do_removals(ctx, pins);
+      }).safe_then([this, ctx, &to_write] {
+	return do_insertions(ctx, to_write);
+      });
+    });
+}
+
+ObjectDataHandler::write_ret ObjectDataHandler::write(
+  context_t ctx,
+  objaddr_t offset,
+  const bufferlist &bl)
+{
+  assert_aligned(offset);
+  assert_aligned(bl.length());
+  return with_object_data(
+    ctx,
+    [this, ctx, offset, &bl](auto &object_data) {
+      return prepare_data_reservation(
+	ctx,
+	object_data,
+	p2roundup(offset + bl.length(), ctx.tm.get_block_size())
+      ).safe_then([this, ctx, offset, &object_data, &bl] {
+	auto logical_offset = object_data.get_reserved_data_base() + offset;
+	return ctx.tm.get_pins(
+	  ctx.t,
+	  logical_offset,
+	  bl.length()
+	).safe_then([this, ctx, offset, logical_offset, &object_data, &bl](
+		      auto pins) {
+	  return overwrite(ctx, logical_offset, bufferlist(bl), std::move(pins));
+	});
+      });
+    });
+}
+
+ObjectDataHandler::read_ret ObjectDataHandler::read(
+  context_t ctx,
+  objaddr_t obj_offset,
+  extent_len_t len)
+{
+  return seastar::do_with(
+    bufferlist(),
+    [this, ctx, obj_offset, len](auto &ret) {
+      return with_object_data(
+	ctx,
+	[this, ctx, obj_offset, len, &ret](const auto &object_data) {
+	  /* Assumption: callers ensure that onode size is <= reserved
+	   * size and that len is adjusted here prior to call */
+	  ceph_assert(!object_data.is_null());
+	  ceph_assert((obj_offset + len) <= object_data.get_reserved_data_len());
+	  ceph_assert(len > 0);
+	  laddr_t loffset =
+	    object_data.get_reserved_data_base() + obj_offset;
+	  return ctx.tm.get_pins(
+	    ctx.t,
+	    loffset,
+	    len
+	  ).safe_then([this, ctx, loffset, len, &ret](auto _pins) {
+	    // offset~len falls within reserved region and len > 0
+	    ceph_assert(_pins.size() >= 1);
+	    ceph_assert((*_pins.begin())->get_laddr() <= loffset);
+	    return seastar::do_with(
+	      std::move(_pins),
+	      loffset,
+	      [this, ctx, loffset, len, &ret](auto &pins, auto &current) {
+		return crimson::do_for_each(
+		  std::begin(pins),
+		  std::end(pins),
+		  [this, ctx, loffset, len, &current, &ret](auto &pin)
+		  -> read_ertr::future<> {
+		    ceph_assert(current <= (loffset + len));
+		    ceph_assert(
+		      (loffset + len) > pin->get_laddr());
+		    laddr_t end = std::min(
+		      pin->get_laddr() + pin->get_length(),
+		      loffset + len);
+		    if (pin->get_paddr().is_zero()) {
+		      ceph_assert(end > current); // See LBAManager::get_mappings
+		      ret.append_zero(end - current);
+		      current = end;
+		      return seastar::now();
+		    } else {
+		      return ctx.tm.pin_to_extent<TestBlock>(
+			ctx.t,
+			std::move(pin)
+		      ).safe_then([&ret, &current, end](auto extent) {
+			ceph_assert(
+			  (extent->get_laddr() + extent->get_length()) >= end);
+			ceph_assert(end > current);
+			ret.append(
+			  bufferptr(
+			    extent->get_bptr(),
+			    current - extent->get_laddr(),
+			    end - current));
+			current = end;
+			return seastar::now();
+		      }).handle_error(
+			read_ertr::pass_further{},
+			crimson::ct_error::assert_all{
+			  "ObjectDataHandler::read hit invalid error"
+			}
+		      );
+		    }
+		  });
+	      });
+	  });
+	}).safe_then([&ret] {
+	  return std::move(ret);
+	});
+    });
+}
+
+ObjectDataHandler::truncate_ret ObjectDataHandler::truncate(
+  context_t ctx,
+  objaddr_t offset)
+{
+  return with_object_data(
+    ctx,
+    [this, ctx, offset](auto &object_data) {
+      if (offset < object_data.get_reserved_data_len()) {
+	return trim_data_reservation(ctx, object_data, offset);
+      } else if (offset > object_data.get_reserved_data_len()) {
+	return prepare_data_reservation(
+	  ctx,
+	  object_data,
+	  offset);
+      } else {
+	return truncate_ertr::now();
+      }
+    });
+}
+
+ObjectDataHandler::clear_ret ObjectDataHandler::clear(
+  context_t ctx)
+{
+  return with_object_data(
+    ctx,
+    [this, ctx](auto &object_data) {
+      return trim_data_reservation(ctx, object_data, 0);
+    });
+}
+
+}
---
+++ src/test/crimson/seastore/test_object_data_handler.cc
@@ -0,0 +1,160 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "test/crimson/gtest_seastar.h"
+#include "test/crimson/seastore/transaction_manager_test_state.h"
+
+#include "crimson/os/seastore/onode.h"
+#include "crimson/os/seastore/object_data_handler.h"
+
+using namespace crimson;
+using namespace crimson::os;
+using namespace crimson::os::seastore;
+
+namespace {
+  [[maybe_unused]] seastar::logger& logger() {
+    return crimson::get_logger(ceph_subsys_test);
+  }
+}
+
+class TestOnode : public Onode {
+  onode_layout_t layout;
+  bool dirty = false;
+
+public:
+  const onode_layout_t &get_layout() const final {
+    return layout;
+  }
+  onode_layout_t &get_mutable_layout(Transaction &t) final {
+    dirty = true;
+    return layout;
+  }
+  bool is_dirty() const { return dirty; }
+  ~TestOnode() final = default;
+};
+
+struct object_data_handler_test_t:
+  public seastar_test_suite_t,
+  TMTestState {
+  OnodeRef onode;
+
+  bufferptr known_contents;
+  extent_len_t size = 0;
+
+  object_data_handler_test_t() {}
+
+  auto submit_transaction(TransactionRef &&t) {
+    return tm->submit_transaction(std::move(t)
+    ).safe_then([this] {
+      return segment_cleaner->run_until_halt();
+    });
+  }
+
+  void write(Transaction &t, objaddr_t offset, extent_len_t len, char fill) {
+    ceph_assert(offset + len <= known_contents.length());
+    size = std::max<extent_len_t>(size, offset + len);
+    memset(
+      known_contents.c_str() + offset,
+      fill,
+      len);
+    bufferlist bl;
+    bl.append(
+      bufferptr(
+	known_contents,
+	offset,
+	len));
+    return ObjectDataHandler().write(
+      ObjectDataHandler::context_t{
+	*tm,
+	t,
+	*onode,
+      },
+      offset,
+      bl).unsafe_get0();
+  }
+  void write(objaddr_t offset, extent_len_t len, char fill) {
+    auto t = tm->create_transaction();
+    write(*t, offset, len, fill);
+    return submit_transaction(std::move(t)).unsafe_get0();
+  }
+
+  void read(Transaction &t, objaddr_t offset, extent_len_t len) {
+    ceph_assert(offset + len <= size);
+    bufferlist bl = ObjectDataHandler().read(
+      ObjectDataHandler::context_t{
+	*tm,
+	t,
+	*onode
+      },
+      offset,
+      len).unsafe_get0();
+    bufferlist known;
+    known.append(
+      bufferptr(
+	known_contents,
+	offset,
+	len));
+    EXPECT_EQ(bl.length(), known.length());
+    EXPECT_EQ(bl, known);
+  }
+  void read(objaddr_t offset, extent_len_t len) {
+    auto t = tm->create_transaction();
+    read(*t, offset, len);
+  }
+
+  seastar::future<> set_up_fut() final {
+    onode = new TestOnode{};
+    known_contents = buffer::create(4<<20 /* 4MB */);
+    size = 0;
+    return tm_setup();
+  }
+
+  seastar::future<> tear_down_fut() final {
+    onode.reset();
+    size = 0;
+    return tm_teardown();
+  }
+};
+
+TEST_F(object_data_handler_test_t, single_write_read)
+{
+  run_async([this] {
+    write(1<<20, 4<<10, 'c');
+    read(1<<20, 4<<10);
+  });
+}
+
+TEST_F(object_data_handler_test_t, single_write_unaligned_read_right)
+{
+  run_async([this] {
+    write(1<<20, 8<<10, 'c');
+    read(1<<20, (4<<10) + 511);
+  });
+}
+
+TEST_F(object_data_handler_test_t, single_write_unaligned_read_left)
+{
+  run_async([this] {
+    write(1<<20, 8<<10, 'c');
+    read((1<<20) - 511, (4<<10) + 511);
+  });
+}
+
+TEST_F(object_data_handler_test_t, single_write_unaligned_read_both)
+{
+  run_async([this] {
+    write(1<<20, 8<<10, 'c');
+    read((1<<20) - 511, 4<<10);
+  });
+}
+
+TEST_F(object_data_handler_test_t, multi_write_aligned_read)
+{
+  run_async([this] {
+    write((1<<20) - (4<<10), 4<<10, 'a');
+    write(1<<20, 4<<10, 'b');
+    write((1<<20) + (4<<10), 4<<10, 'c');
+
+    read((1<<20) - (4<<10), 12<<10);
+  });
+}
