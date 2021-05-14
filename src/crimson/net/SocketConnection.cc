athanatos/ceph

commit 0a9e455e36556491b76e0bf2497f1d3173584be7

crimson/net: Overload conn::send() method to take unique_ptr
Signed-off-by: Amnon Hanuhov <ahanukov@redhat.com>

src/crimson/net/SocketConnection.cc
@@ -69,6 +69,12 @@ bool SocketConnection::peer_wins() const
   return (messenger.get_myaddr() > peer_addr || policy.server);
 }
 
+seastar::future<> SocketConnection::send(MessageURef msg)
+{
+  assert(seastar::this_shard_id() == shard_id());
+  return protocol->send(MessageRef{msg.release()});
+}
+
 seastar::future<> SocketConnection::send(MessageRef msg)
 {
   assert(seastar::this_shard_id() == shard_id());

src/crimson/net/Connection.h
@@ -126,6 +126,8 @@ class Connection : public seastar::enable_shared_from_this<Connection> {
 #endif
 
   /// send a message over a connection that has completed its handshake
+  virtual seastar::future<> send(MessageURef msg) = 0;
+  // The version with MessageRef will be dropped in the future
   virtual seastar::future<> send(MessageRef msg) = 0;
 
   /// send a keepalive message over a connection that has completed its

src/crimson/net/SocketConnection.h
@@ -70,6 +70,7 @@ class SocketConnection : public Connection {
   bool peer_wins() const;
 #endif
 
+  seastar::future<> send(MessageURef msg) override;
   seastar::future<> send(MessageRef msg) override;
 
   seastar::future<> keepalive() override;

commit 5c5caa4fff6761a378ee6047a3f16b72cf66692a

crimson/net: Set add_ref to false when creating a MessageRef in conn::send()
we should not increase the ref count when transferring the ownership from a
MessageURef instance to a MessageRef instance.

to address the memory leak introduced by
0a9e455e36556491b76e0bf2497f1d3173584be7.

Signed-off-by: Amnon Hanuhov <ahanukov@redhat.com>

src/crimson/net/SocketConnection.cc
@@ -72,7 +72,7 @@ bool SocketConnection::peer_wins() const
 seastar::future<> SocketConnection::send(MessageURef msg)
 {
   assert(seastar::this_shard_id() == shard_id());
-  return protocol->send(MessageRef{msg.release()});
+  return protocol->send(MessageRef{msg.release(), false});
 }
 
 seastar::future<> SocketConnection::send(MessageRef msg)