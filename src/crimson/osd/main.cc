athanatos/ceph

commit e38b84301b354c6d62e943d2f4f7c77c1b814885

crimson/osd/main: do cleanup using defer()
since we do the startup in a seastar thread, we have the luxury of doing
cleanup using the RAII machinery.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/osd/main.cc
@@ -214,12 +214,12 @@ int main(int argc, char* argv[])
             );
           }
           sharded_conf().start(init_params.name, cluster_name).get();
-          seastar::engine().at_exit([] {
-            return sharded_conf().stop();
+          auto stop_conf = seastar::defer([] {
+            sharded_conf().stop().get();
           });
           sharded_perf_coll().start().get();
-          seastar::engine().at_exit([] {
-            return sharded_perf_coll().stop();
+          auto stop_perf_coll = seastar::defer([] {
+            sharded_perf_coll().stop().get();
           });
           local_conf().parse_config_files(conf_file_list).get();
           local_conf().parse_argv(ceph_args).get();
@@ -263,8 +263,8 @@ int main(int argc, char* argv[])
               local_conf().get_val<uuid_d>("osd_uuid"),
               local_conf().get_val<uuid_d>("fsid")).get();
           }
-          seastar::engine().at_exit([&] {
-            return osd.stop();
+          auto stop_osd = seastar::defer([&] {
+            osd.stop().get();
           });
           if (config.count("mkkey") || config.count("mkfs")) {
             return EXIT_SUCCESS;

commit 179288957355823898beca806f0a726beab88506

crimson/osd: wait for SIGINT and SIGTERM before stopping
this change addresses an regression introduced by
e38b84301b354c6d62e943d2f4f7c77c1b814885. as we should not stop
services without being asked to do so.

in this change, signal handler for SIGINT and SIGTERM is registered to
handle these signals, and in the seastar thread, we wait until any of
these two signals is caught.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/osd/CMakeLists.txt
@@ -10,6 +10,7 @@ add_executable(crimson-osd
   pg_meta.cc
   replicated_backend.cc
   shard_services.cc
+  stop_signal.cc
   object_context.cc
   ops_executer.cc
   osd_operation.cc

src/crimson/osd/main.cc
@@ -23,6 +23,7 @@
 #include "global/pidfile.h"
 
 #include "osd.h"
+#include "stop_signal.h"
 
 using config_t = crimson::common::ConfigProxy;
 
@@ -208,6 +209,7 @@ int main(int argc, char* argv[])
       return seastar::async([&] {
         try {
           FatalSignal fatal_signal;
+          StopSignal should_stop;
           if (config.count("debug")) {
             seastar::global_logger_registry().set_all_loggers_level(
               seastar::log_level::debug
@@ -271,6 +273,10 @@ int main(int argc, char* argv[])
           } else {
             osd.invoke_on(0, &crimson::osd::OSD::start).get();
           }
+          seastar::fprint(std::cout, "crimson startup completed.");
+          should_stop.wait().get();
+          seastar::fprint(std::cout, "crimson shutting down.");
+          // stop()s registered using defer() are called here
         } catch (...) {
           seastar::fprint(std::cerr, "FATAL: startup failed: %s\n", std::current_exception());
           return EXIT_FAILURE;

src/crimson/osd/stop_signal.cc
@@ -1,0 +1,33 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "stop_signal.h"
+
+#include <csignal>
+#include <seastar/core/reactor.hh>
+
+StopSignal::StopSignal()
+{
+  seastar::engine().handle_signal(SIGINT, [this] { signaled(); });
+  seastar::engine().handle_signal(SIGTERM, [this] { signaled(); });
+}
+
+StopSignal::~StopSignal()
+{
+  seastar::engine().handle_signal(SIGINT, [] {});
+  seastar::engine().handle_signal(SIGTERM, [] {});
+}
+
+seastar::future<> StopSignal::wait()
+{
+  return should_stop.wait([this] { return caught; });
+}
+
+void StopSignal::signaled()
+{
+  if (caught) {
+    return;
+  }
+  caught = true;
+  should_stop.signal();
+}

src/crimson/osd/stop_signal.h
@@ -1,0 +1,21 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include <seastar/core/condition-variable.hh>
+#include <seastar/core/sharded.hh>
+
+class StopSignal {
+public:
+  StopSignal();
+  ~StopSignal();
+  seastar::future<> wait();
+
+private:
+  void signaled();
+
+private:
+  bool caught;
+  seastar::condition_variable should_stop;
+};