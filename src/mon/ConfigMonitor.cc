sunilangadi2/ceph-ci

mgr,mon: s/boost::optional/std::optional/
since the encoding schemes of boost::optional and std::optional are
identical, they can be used interchangeably.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/mon/ConfigMonitor.cc
@@ -587,7 +587,7 @@ bool ConfigMonitor::prepare_command(MonOpRequestRef op)
       bl.append(value);
       pending[key] = bl;
     } else {
-      pending[key] = boost::none;
+      pending[key].emplace();
     }
     goto update;
   } else if (prefix == "config reset") {
@@ -614,7 +614,7 @@ bool ConfigMonitor::prepare_command(MonOpRequestRef op)
 	  bl.append(*i.second.first);
 	  pending[i.first] = bl;
 	} else if (i.second.second) {
-	  pending[i.first] = boost::none;
+	  pending[i.first].emplace();
 	}
       }
     }
@@ -798,7 +798,7 @@ void ConfigMonitor::load_config()
       if (p != renamed_pacific.end()) {
 	if (mon.monmap->min_mon_release >= ceph_release_t::pacific) {
 	  // schedule a cleanup
-	  pending_cleanup[key] = boost::none;
+	  pending_cleanup[key].emplace();
 	  pending_cleanup[who + "/" + p->second] = it->value();
 	}
 	// continue loading under the new name
@@ -831,18 +831,18 @@ void ConfigMonitor::load_config()
     if (who.size() &&
 	!ConfigMap::parse_mask(who, &section_name, &mopt.mask)) {
       derr << __func__ << " invalid mask for key " << key << dendl;
-      pending_cleanup[key] = boost::none;
+      pending_cleanup[key].emplace();
     } else if (opt->has_flag(Option::FLAG_NO_MON_UPDATE)) {
       dout(10) << __func__ << " NO_MON_UPDATE option '"
 	       << name << "' = '" << value << "' for " << name
 	       << dendl;
-      pending_cleanup[key] = boost::none;
+      pending_cleanup[key].emplace();
     } else {
       if (section_name.empty()) {
 	// we prefer global/$option instead of just $option
 	derr << __func__ << " adding global/ prefix to key '" << key << "'"
 	     << dendl;
-	pending_cleanup[key] = boost::none;
+	pending_cleanup[key].emplace();
 	pending_cleanup["global/"s + key] = it->value();
       }
       Section *section = &config_map.global;;

src/mon/KVMonitor.cc
@@ -298,7 +298,7 @@ bool KVMonitor::prepare_command(MonOpRequestRef op)
   else if (prefix == "config-key del" ||
 	   prefix == "config-key rm") {
     ss << "key deleted";
-    pending[key] = boost::none;
+    pending[key].emplace();
     goto update;
   }
   else {
@@ -375,7 +375,7 @@ void KVMonitor::do_osd_destroy(int32_t id, uuid_d& uuid)
     if (iter->key().find(prefix) != 0) {
       break;
     }
-    pending[iter->key()] = boost::none;
+    pending[iter->key()].emplace();
   }
 
   propose_pending();
@@ -491,7 +491,7 @@ bool KVMonitor::maybe_send_update(Subscription *sub)
       int err = get_version(cur, bl);
       ceph_assert(err == 0);
 
-      std::map<std::string,boost::optional<ceph::buffer::list>> pending;
+      std::map<std::string,std::optional<ceph::buffer::list>> pending;
       auto p = bl.cbegin();
       ceph::decode(pending, p);
 

src/mon/KVMonitor.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include <boost/optional.hpp>
+#include <optional>
 
 #include "mon/PaxosService.h"
 
@@ -14,7 +14,7 @@ extern const std::string KV_PREFIX;
 class KVMonitor : public PaxosService
 {
   version_t version = 0;
-  std::map<std::string,boost::optional<ceph::buffer::list>> pending;
+  std::map<std::string,std::optional<ceph::buffer::list>> pending;
 
   bool _have_prefix(const string &prefix);
 
@@ -64,6 +64,6 @@ public:
     pending[key] = v;
   }
   void enqueue_rm(const std::string& key) {
-    pending[key] = boost::none;
+    pending[key].emplace();
   }
 };

athanatos/ceph

src/mon/ConfigMonitor.cc
@@ -587,7 +587,7 @@ bool ConfigMonitor::prepare_command(MonOpRequestRef op)
       bl.append(value);
       pending[key] = bl;
     } else {
-      pending[key] = boost::none;
+      pending[key].reset();
     }
     goto update;
   } else if (prefix == "config reset") {
@@ -614,7 +614,7 @@ bool ConfigMonitor::prepare_command(MonOpRequestRef op)
 	  bl.append(*i.second.first);
 	  pending[i.first] = bl;
 	} else if (i.second.second) {
-	  pending[i.first] = boost::none;
+	  pending[i.first].reset();
 	}
       }
     }
@@ -798,7 +798,7 @@ void ConfigMonitor::load_config()
       if (p != renamed_pacific.end()) {
 	if (mon.monmap->min_mon_release >= ceph_release_t::pacific) {
 	  // schedule a cleanup
-	  pending_cleanup[key] = boost::none;
+	  pending_cleanup[key].reset();
 	  pending_cleanup[who + "/" + p->second] = it->value();
 	}
 	// continue loading under the new name
@@ -831,18 +831,18 @@ void ConfigMonitor::load_config()
     if (who.size() &&
 	!ConfigMap::parse_mask(who, &section_name, &mopt.mask)) {
       derr << __func__ << " invalid mask for key " << key << dendl;
-      pending_cleanup[key] = boost::none;
+      pending_cleanup[key].reset();
     } else if (opt->has_flag(Option::FLAG_NO_MON_UPDATE)) {
       dout(10) << __func__ << " NO_MON_UPDATE option '"
 	       << name << "' = '" << value << "' for " << name
 	       << dendl;
-      pending_cleanup[key] = boost::none;
+      pending_cleanup[key].reset();
     } else {
       if (section_name.empty()) {
 	// we prefer global/$option instead of just $option
 	derr << __func__ << " adding global/ prefix to key '" << key << "'"
 	     << dendl;
-	pending_cleanup[key] = boost::none;
+	pending_cleanup[key].reset();
 	pending_cleanup["global/"s + key] = it->value();
       }
       Section *section = &config_map.global;;

src/mon/KVMonitor.cc
@@ -298,7 +298,7 @@ bool KVMonitor::prepare_command(MonOpRequestRef op)
   else if (prefix == "config-key del" ||
 	   prefix == "config-key rm") {
     ss << "key deleted";
-    pending[key] = boost::none;
+    pending[key].reset();
     goto update;
   }
   else {
@@ -375,7 +375,7 @@ void KVMonitor::do_osd_destroy(int32_t id, uuid_d& uuid)
     if (iter->key().find(prefix) != 0) {
       break;
     }
-    pending[iter->key()] = boost::none;
+    pending[iter->key()].reset();
   }
 
   propose_pending();
@@ -491,7 +491,7 @@ bool KVMonitor::maybe_send_update(Subscription *sub)
       int err = get_version(cur, bl);
       ceph_assert(err == 0);
 
-      std::map<std::string,boost::optional<ceph::buffer::list>> pending;
+      std::map<std::string,std::optional<ceph::buffer::list>> pending;
       auto p = bl.cbegin();
       ceph::decode(pending, p);
 

src/mon/KVMonitor.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include <boost/optional.hpp>
+#include <optional>
 
 #include "mon/PaxosService.h"
 
@@ -14,7 +14,7 @@ extern const std::string KV_PREFIX;
 class KVMonitor : public PaxosService
 {
   version_t version = 0;
-  std::map<std::string,boost::optional<ceph::buffer::list>> pending;
+  std::map<std::string,std::optional<ceph::buffer::list>> pending;
 
   bool _have_prefix(const string &prefix);
 
@@ -64,6 +64,6 @@ public:
     pending[key] = v;
   }
   void enqueue_rm(const std::string& key) {
-    pending[key] = boost::none;
+    pending[key].reset();
   }
 };