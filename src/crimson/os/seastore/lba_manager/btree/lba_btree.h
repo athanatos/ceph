7fe622213044a63bbaed4a53db0526f8352718ac
Samuel Just <sjust@redhat.com>
2021-07-14 18:51:37 +0000

    working

--- src/crimson/os/seastore/lba_manager/btree/lba_btree.h
+++ src/crimson/os/seastore/lba_manager/btree/lba_btree.h
@@ -42,8 +42,16 @@ public:
     iter_t &operator=(iter_t &&) = default;
 
     operator iter_t<!is_const>() const {
-      static_assert(!is_const);
+      static_assert(is_const);
       return iter_t<!is_const>(*this);
+    }
+
+    using advance_iertr = base_iertr;
+    using advance_ret = base_iertr::future<iter_t>;
+    advance_ret next() {
+      return advance_ret(
+	interruptible::ready_future_marker{},
+	*this);
     }
 
     // Work nicely with for loops without requiring a nested type.
@@ -53,6 +61,7 @@ public:
 
     const laddr_t &get_key();
     const lba_map_val_t &get_val();
+    bool is_end() const { return false; /* TODO */ }
   };
 
   LBABtree(lba_root_t root) : root(root) {}
@@ -60,27 +69,72 @@ public:
   using iterator = iter_t<false>;
   using const_iterator = iter_t<true>;
 
-  using lower_bound_iertr = base_iertr;
+  using bound_iertr = base_iertr;
   template <bool is_const>
-  using lower_bound_ret = base_iertr::future<iter_t<is_const>>;
-  lower_bound_ret<true> lower_bound(
+  using bound_ret = base_iertr::future<iter_t<is_const>>;
+  bound_ret<true> lower_bound(
     op_context_t c,
     laddr_t addr) const;
-  lower_bound_ret<false> lower_bound(
+  bound_ret<false> lower_bound(
     op_context_t c,
     laddr_t addr);
+  bound_ret<true> upper_bound(
+    op_context_t c,
+    laddr_t addr) const {
+    return lower_bound(
+      c, addr
+    ).si_then([this, addr](auto iter) {
+      if (!iter->is_end() && iter->get_key() == addr) {
+	return iter.next();
+      } else {
+	return bound_ret<true>(
+	  interruptible::ready_future_marker{},
+	  iter);
+      }
+    });
+  }
+  bound_ret<false> upper_bound(
+    op_context_t c,
+    laddr_t addr) {
+    return lower_bound(
+      c, addr
+    ).si_then([this, addr](auto iter) {
+      if (!iter->is_end() && iter->get_key() == addr) {
+	return iter.next();
+      } else {
+	return bound_ret<false>(
+	  interruptible::ready_future_marker{},
+	  iter);
+      }
+    });
+  }
 
+  /**
+   * insert
+   *
+   * @param c [in] op context
+   * @param iter [in] hint, insertion constant if immediately prior to iter
+   * @param laddr [in] addr at which to insert
+   * @param val [in] val to insert
+   */
   using insert_iertr = base_iertr;
   using insert_ret = insert_iertr::future<>;
   insert_ret insert(
-    op_context_t c,
-    iterator iter,
+    op_context_t c,   ///< [in] op context
+    iterator iter,    ///< [in] hint, insertion constant time if immediately after iter
     laddr_t laddr,
-    lba_map_val_t val);
+    lba_map_val_t val
+  );
   insert_ret insert(
     op_context_t c,
     laddr_t laddr,
-    lba_map_val_t val);
+    lba_map_val_t val) {
+    return upper_bound(
+      c, laddr
+    ).si_then([this, c, laddr, val](auto iter) {
+      return insert(c, iter, laddr, val);
+    });
+  }
 
 
   using remove_iertr = base_iertr;
