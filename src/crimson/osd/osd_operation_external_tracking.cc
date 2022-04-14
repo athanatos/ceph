27ec7c826a897389ead03ce3c4771287538b7243
Radosław Zarzyński <rzarzyns@redhat.com>
2022-04-14 13:17:17 +0200

    WIP, crimson/osd: add support for historic op tracking.
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

---
+++ src/crimson/osd/osd_operation_external_tracking.cc
@@ -0,0 +1,21 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "crimson/osd/osd_operation_external_tracking.h"
+
+#if 0
+namespace {
+  seastar::logger& logger() {
+    return crimson::get_logger(ceph_subsys_osd);
+  }
+}
+
+namespace crimson::osd {
+
+void HistoricBackend::handle(ClientRequest::CompletionEvent&,
+                             const Operation&)
+{
+}
+
+} // namespace crimson::osd
+#endif
