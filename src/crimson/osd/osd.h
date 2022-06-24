c73d7a386952a0fb9247792020a6c7c17ae48833
Samuel Just <sjust@redhat.com>
2022-06-24 23:39:28 +0000

    crimson/osd: introduce pg_shard_manager to clarify shard-local vs osd-wide state
    This commits begins to change ShardServices to be the interface by which
    PGs access shard local and osd wide state.  Future work will further
    clarify this interface boundary and introduce machinery to mediate cold
    path access to state on remote shards.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/osd/osd.h
+++ src/crimson/osd/osd.h
@@ -20,8 +20,7 @@
 #include "crimson/mgr/client.h"
 #include "crimson/net/Dispatcher.h"
 #include "crimson/osd/osdmap_service.h"
-#include "crimson/osd/state.h"
-#include "crimson/osd/shard_services.h"
+#include "crimson/osd/pg_shard_manager.h"
 #include "crimson/osd/osdmap_gate.h"
 #include "crimson/osd/pg_map.h"
 #include "crimson/osd/osd_operations/peering_event.h"
@@ -80,8 +79,6 @@ class OSD final : public crimson::net::Dispatcher,
   crimson::os::FuturizedStore& store;
   std::unique_ptr<OSDMeta> meta_coll;
 
-  OSDState state;
-
   /// _first_ epoch we were marked up (after this process started)
   epoch_t boot_epoch = 0;
   /// _most_recent_ epoch we were marked up
@@ -111,7 +108,8 @@ class OSD final : public crimson::net::Dispatcher,
   void handle_authentication(const EntityName& name,
 			     const AuthCapsInfo& caps) final;
 
-  crimson::osd::ShardServices shard_services;
+  crimson::osd::PGShardManager pg_shard_manager;
+  crimson::osd::ShardServices &shard_services;
 
   std::unique_ptr<Heartbeat> heartbeat;
   seastar::timer<seastar::lowres_clock> tick_timer;
@@ -209,10 +207,8 @@ private:
   seastar::future<> start_asok_admin();
 
 public:
-  OSD_OSDMapGate osdmap_gate;
-
   ShardServices &get_shard_services() {
-    return shard_services;
+    return pg_shard_manager.get_shard_services();
   }
 
   seastar::future<> consume_map(epoch_t epoch);
@@ -246,70 +242,6 @@ private:
 public:
   Ref<PG> get_pg(spg_t pgid);
   seastar::future<> send_beacon();
-
-  template <typename T, typename... Args>
-  auto start_pg_operation(Args&&... args) {
-    auto op = shard_services.registry.create_operation<T>(
-      std::forward<Args>(args)...);
-    auto &logger = crimson::get_logger(ceph_subsys_osd);
-    logger.debug("{}: starting {}", *op, __func__);
-    auto &opref = *op;
-
-    auto fut = opref.template enter_stage<>(
-      opref.get_connection_pipeline().await_active
-    ).then([this, &opref, &logger] {
-      logger.debug("{}: start_pg_operation in await_active stage", opref);
-      return state.when_active();
-    }).then([&logger, &opref] {
-      logger.debug("{}: start_pg_operation active, entering await_map", opref);
-      return opref.template enter_stage<>(
-	opref.get_connection_pipeline().await_map);
-    }).then([this, &logger, &opref] {
-      logger.debug("{}: start_pg_operation await_map stage", opref);
-      using OSDMapBlockingEvent =
-	OSD_OSDMapGate::OSDMapBlocker::BlockingEvent;
-      return opref.template with_blocking_event<OSDMapBlockingEvent>(
-	[this, &opref](auto &&trigger) {
-	  return osdmap_gate.wait_for_map(
-	    std::move(trigger),
-	    opref.get_epoch(),
-	    &shard_services
-	  );
-	});
-    }).then([&logger, &opref](auto epoch) {
-      logger.debug("{}: got map {}, entering get_pg", opref, epoch);
-      return opref.template enter_stage<>(
-	opref.get_connection_pipeline().get_pg);
-    }).then([this, &logger, &opref] {
-      logger.debug("{}: in get_pg", opref);
-      if constexpr (T::can_create()) {
-	logger.debug("{}: can_create", opref);
-	return opref.template with_blocking_event<
-	  PGMap::PGCreationBlockingEvent
-	  >([this, &opref](auto &&trigger) {
-	    std::ignore = this; // avoid clang warning
-	    return get_or_create_pg(
-	      std::move(trigger),
-	      opref.get_pgid(), opref.get_epoch(),
-	      std::move(opref.get_create_info()));
-	  });
-      } else {
-	logger.debug("{}: !can_create", opref);
-	return opref.template with_blocking_event<
-	  PGMap::PGCreationBlockingEvent
-	  >([this, &opref](auto &&trigger) {
-	    std::ignore = this; // avoid clang warning
-	    return wait_for_pg(std::move(trigger), opref.get_pgid());
-	  });
-      }
-    }).then([this, &logger, &opref](Ref<PG> pgref) {
-      logger.debug("{}: have_pg", opref);
-      return opref.with_pg(shard_services, pgref);
-    }).then([op] { /* Retain refcount on op until completion */ });
-
-    return std::make_pair(std::move(op), std::move(fut));
-  }
-
 
 private:
   LogClient log_client;
