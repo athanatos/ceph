22c3b58d0c705b3c6b93a152a626592414945851
Samuel Just <sjust@redhat.com>
2022-09-08 23:12:45 +0000

    crimson/net: Message::conn needs to be a foreign_ptr
    There are two main consequences of this:
    1. Messages can't be default copy constructed in crimson.  MMonCommand
       seems to be the only user, and we simply add a copy constructor that
       duplicates data portions of the message.
    2. We can't casually copy-construct the conn into other structures.
       The main user here is watch/notify.  We use copy() explicitely
       to populate the object_context structures and avoid passing
       ConnectionFRef by value.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/msg/Message.h
+++ src/msg/Message.h
@@ -246,8 +246,10 @@ class Message : public RefCountedObject {
 public:
 #ifdef WITH_SEASTAR
   using ConnectionRef = crimson::net::ConnectionRef;
+  using ConnectionFRef = crimson::net::ConnectionFRef;
 #else
   using ConnectionRef = ::ConnectionRef;
+  using ConnectionFRef = ::ConnectionRef;
 #endif // WITH_SEASTAR
 
 protected:
@@ -268,7 +270,7 @@ protected:
   /* time at which message was fully read */
   utime_t recv_complete_stamp;
 
-  ConnectionRef connection;
+  ConnectionFRef connection;
 
   uint32_t magic = 0;
 
@@ -343,7 +345,7 @@ protected:
       completion_hook->complete(0);
   }
 public:
-  const ConnectionRef& get_connection() const { return connection; }
+  const ConnectionFRef& get_connection() const { return connection; }
   void set_connection(ConnectionRef c) {
     connection = std::move(c);
   }
