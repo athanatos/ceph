c35463de7b5131326582b8f955507b2f57beecb4
Samuel Just <sjust@redhat.com>
2023-02-01 19:55:19 -0800

    osd/scrubber/pg_scrubber: lower logging level on update_scrub_job message
    Signed-off-by: <dnm>

--- src/osd/scrubber/pg_scrubber.cc
+++ src/osd/scrubber/pg_scrubber.cc
@@ -542,7 +542,7 @@ void PgScrubber::update_scrub_job(const requested_scrub_t& request_flags)
   dout(10) << fmt::format("{}: flags:<{}>", __func__, request_flags) << dendl;
   // verify that the 'in_q' status matches our "Primariority"
   if (m_scrub_job && is_primary() && !m_scrub_job->in_queues) {
-    dout(1) << __func__ << " !!! primary but not scheduled! " << dendl;
+    dout(10) << __func__ << " !!! primary but not scheduled! " << dendl;
   }
 
   if (is_primary() && m_scrub_job) {
