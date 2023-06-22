athanatos/ceph

commit 24772a19a72c26fe45d323f892c54bb39afa75c4

mds: do not trim cache when creating system file
Fixes: https://tracker.ceph.com/issues/51023
Signed-off-by: Xiubo Li <xiubli@redhat.com>

src/mds/MDCache.cc
@@ -13273,7 +13273,9 @@ void MDCache::upkeep_main(void)
         if (active_with_clients) {
           trim_client_leases();
         }
-        trim();
+        if (is_open()) {
+          trim();
+        }
         if (active_with_clients) {
           auto recall_flags = Server::RecallFlags::ENFORCE_MAX|Server::RecallFlags::ENFORCE_LIVENESS;
           if (cache_toofull()) {

commit c21cf14f8261ada2e6418e3829a05cc3ba259b26

mds: trim cache during standby-replay
Fixes: 24772a19a72c26fe45d323f892c54bb39afa75c4
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mds/MDCache.cc
@@ -13595,7 +13595,7 @@ void MDCache::upkeep_main(void)
         if (active_with_clients) {
           trim_client_leases();
         }
-        if (is_open()) {
+        if (is_open() || mds->is_standby_replay()) {
           trim();
         }
         if (active_with_clients) {