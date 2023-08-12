a54916c27ac0f354a21f9da31f0f2d8637235da5
Samuel Just <sjust@redhat.com>
2023-08-12 00:38:40 +0000

    crimson/osd: scrub integration with crimson
    Signed-off-by: Samuel Just <sjust@redhat.com>

---
+++ src/crimson/osd/scrub/pg_scrubber.cc
@@ -0,0 +1,289 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
+// vim: ts=8 sw=2 smarttab expandtab
+
+#include "crimson/common/log.h"
+#include "crimson/osd/pg.h"
+#include "crimson/osd/osd_operations/scrub_events.h"
+#include "messages/MOSDRepScrub.h"
+#include "messages/MOSDRepScrubMap.h"
+#include "pg_scrubber.h"
+
+SET_SUBSYS(osd);
+
+namespace crimson::osd::scrub {
+
+void PGScrubber::dump_detail(Formatter *f) const
+{
+  f->dump_stream("pgid") << pg.get_pgid();
+}
+
+PGScrubber::PGScrubber(PG &pg) : pg(pg), dpp(pg), machine(*this) {}
+
+void PGScrubber::on_primary_active_clean()
+{
+  LOG_PREFIX(PGScrubber::on_primary_active_clean);
+  DEBUGDPP("", pg);
+  machine.process_event(PrimaryActivate{});
+}
+
+void PGScrubber::on_replica_activate()
+{
+  LOG_PREFIX(PGScrubber::on_replica_activate);
+  DEBUGDPP("", pg);
+  machine.process_event(ReplicaActivate{});
+}
+
+void PGScrubber::on_interval_change()
+{
+  LOG_PREFIX(PGScrubber::on_interval_change);
+  DEBUGDPP("", pg);
+  /* Once reservations and scheduling are introduced, we'll need an
+   * IntervalChange event to drop remote resources (they'll be automatically
+   * released on the other side */
+  machine.process_event(Reset{});
+  waiting_for_update = std::nullopt;
+  ceph_assert(!blocked);
+}
+
+void PGScrubber::on_log_update(eversion_t v)
+{
+  LOG_PREFIX(PGScrubber::on_interval_change);
+  if (waiting_for_update && v >= *waiting_for_update) {
+    DEBUGDPP("waiting_for_update: {}, v: {}", pg, *waiting_for_update, v);
+    machine.process_event(await_update_complete_t{});
+    waiting_for_update = std::nullopt;
+  }
+}
+
+void PGScrubber::handle_scrub_requested(bool deep)
+{
+  LOG_PREFIX(PGScrubber::handle_scrub_requested);
+  DEBUGDPP("deep: {}", pg, deep);
+  machine.process_event(StartScrub{deep});
+}
+
+void PGScrubber::handle_scrub_message(Message &_m)
+{
+  LOG_PREFIX(PGScrubber::handle_scrub_requested);
+  switch (_m.get_type()) {
+  case MSG_OSD_REP_SCRUB: {
+    MOSDRepScrub &m = *static_cast<MOSDRepScrub*>(&_m);
+    DEBUGDPP("MOSDRepScrub: {}", pg, m);
+    machine.process_event(ReplicaScan{
+	m.start, m.end, m.scrub_from, m.deep
+      });
+    break;
+  }
+  case MSG_OSD_REP_SCRUBMAP: {
+    MOSDRepScrubMap &m = *static_cast<MOSDRepScrubMap*>(&_m);
+    DEBUGDPP("MOSDRepScrubMap: {}", pg, m);
+    ScrubMap map;
+    auto iter = m.get_data().cbegin();
+    ::decode(map, iter);
+    machine.process_event(scan_range_complete_t{
+	std::make_pair(m.from, std::move(map))
+      });
+    break;
+  }
+  default:
+    DEBUGDPP("invalid message: {}", pg, _m);
+    ceph_assert(is_scrub_message(_m));
+  }
+}
+
+PGScrubber::ifut<> PGScrubber::wait_scrub(
+  PGScrubber::BlockingEvent::TriggerI&& trigger,
+  const hobject_t &hoid)
+{
+  LOG_PREFIX(PGScrubber::wait_scrub);
+  if (blocked && (hoid >= blocked->begin) && (hoid < blocked->end)) {
+    DEBUGDPP("blocked: {}, hoid: {}", pg, *blocked, hoid);
+    return trigger.maybe_record_blocking(
+      blocked->p.get_shared_future(),
+      *this);
+  } else {
+    return seastar::now();
+  }
+}
+
+void PGScrubber::notify_scrub_start(bool deep)
+{
+  LOG_PREFIX(PGScrubber::notify_scrub_start);
+  DEBUGDPP("deep: {}", pg, deep);
+  pg.peering_state.state_set(PG_STATE_SCRUBBING);
+  if (deep) {
+    pg.peering_state.state_set(PG_STATE_DEEP_SCRUB);
+  }
+  pg.publish_stats_to_osd();
+}
+
+void PGScrubber::notify_scrub_end(bool deep)
+{
+  LOG_PREFIX(PGScrubber::notify_scrub_end);
+  DEBUGDPP("deep: {}", pg, deep);
+  pg.peering_state.state_clear(PG_STATE_SCRUBBING);
+  if (deep) {
+    pg.peering_state.state_clear(PG_STATE_DEEP_SCRUB);
+  }
+  pg.publish_stats_to_osd();
+}
+
+const std::set<pg_shard_t> &PGScrubber::get_ids_to_scrub() const
+{
+  return pg.peering_state.get_actingset();
+}
+
+chunk_validation_policy_t PGScrubber::get_policy() const
+{
+  return chunk_validation_policy_t{
+    pg.get_primary(),
+    std::nullopt /* stripe_info, populate when EC is implemented */,
+    crimson::common::local_conf().get_val<Option::size_t>(
+      "osd_max_object_size"),
+    crimson::common::local_conf().get_val<std::string>(
+      "osd_hit_set_namespace"),
+    crimson::common::local_conf().get_val<Option::size_t>(
+      "osd_deep_scrub_large_omap_object_value_sum_threshold"),
+    crimson::common::local_conf().get_val<uint64_t>(
+      "osd_deep_scrub_large_omap_object_key_threshold")
+  };
+}
+
+void PGScrubber::request_range(const hobject_t &start)
+{
+  LOG_PREFIX(PGScrubber::request_range);
+  DEBUGDPP("start: {}", pg, start);
+  std::ignore = pg.shard_services.start_operation<ScrubFindRange>(
+    start, &pg);
+}
+
+/* TODOSAM: This isn't actually enough.  Here, classic would
+ * hold the pg lock from the wait_scrub through to IO submission.
+ * ClientRequest, however, isn't in the processing ExclusivePhase
+ * bit yet, and so this check may miss ops between the wait_scrub
+ * check and adding the IO to the log. */
+
+void PGScrubber::reserve_range(const hobject_t &start, const hobject_t &end)
+{
+  LOG_PREFIX(PGScrubber::reserve_range);
+  DEBUGDPP("start: {}, end: {}", pg, start, end);
+  std::ignore = pg.shard_services.start_operation<ScrubReserveRange>(
+    start, end, &pg);
+}
+
+void PGScrubber::release_range()
+{
+  LOG_PREFIX(PGScrubber::release_range);
+  ceph_assert(blocked);
+  DEBUGDPP("blocked: {}", pg, *blocked);
+  pg.background_io_mutex.unlock();
+  blocked->p.set_value();
+  blocked = std::nullopt;
+}
+
+void PGScrubber::scan_range(
+  pg_shard_t target,
+  eversion_t version,
+  bool deep,
+  const hobject_t &start,
+  const hobject_t &end)
+{
+  LOG_PREFIX(PGScrubber::scan_range);
+  DEBUGDPP("target: {}, version: {}, deep: {}, start: {}, end: {}",
+	   pg, target, version, deep, start, end);
+  if (target == pg.get_pg_whoami()) {
+    std::ignore = pg.shard_services.start_operation<ScrubScan>(
+      &pg, deep, true /* local */, start, end
+    );
+  } else {
+    std::ignore = pg.shard_services.send_to_osd(
+      target.osd,
+      crimson::make_message<MOSDRepScrub>(
+	spg_t(pg.get_pgid().pgid, target.shard),
+	version,
+	pg.get_osdmap_epoch(),
+	pg.get_osdmap_epoch(),
+	start,
+	end,
+	deep,
+	false /* allow preemption -- irrelevant for replicas TODO */,
+	64 /* priority, TODO */,
+	false /* high_priority TODO */),
+      pg.get_osdmap_epoch());
+  }
+}
+
+// TODOSAM: probably can't send an event syncronously
+void PGScrubber::await_update(const eversion_t &version)
+{
+  LOG_PREFIX(PGScrubber::await_update);
+  DEBUGDPP("version: {}", pg, version);
+  ceph_assert(!waiting_for_update);
+  waiting_for_update = version;
+  auto& log = pg.peering_state.get_pg_log().get_log().log;
+  on_log_update(log.empty() ? eversion_t() : log.rbegin()->version);
+}
+
+void PGScrubber::generate_and_submit_chunk_result(
+  const hobject_t &begin,
+  const hobject_t &end,
+  bool deep)
+{
+  LOG_PREFIX(PGScrubber::generate_and_submit_chunk_result);
+  DEBUGDPP("begin: {}, end: {}, deep: {}", pg, begin, end, deep);
+  std::ignore = pg.shard_services.start_operation<ScrubScan>(
+    &pg, deep, false /* not local */, begin, end
+  );
+}
+
+void PGScrubber::emit_chunk_result(
+  const request_range_result_t &range,
+  chunk_result_t &&result)
+{
+  LOG_PREFIX(PGScrubber::emit_chunk_result);
+  if (result.has_errors()) {
+    ERRORDPP(
+      "Scrub errors found. range: {}, result: {}",
+      pg, range, result);
+  } else {
+    DEBUGDPP("Chunk complete. range: {}", pg, range);
+  }
+}
+
+void PGScrubber::emit_scrub_result(
+  bool deep,
+  object_stat_sum_t in_stats)
+{
+  LOG_PREFIX(PGScrubber::emit_scrub_result);
+  DEBUGDPP("", pg);
+  pg.peering_state.update_stats(
+    [this, deep, &in_stats](auto &history, auto &pg_stats) {
+      foreach_scrub_maintained_stat(
+	[deep, &pg_stats, &in_stats](
+	  const auto &name, auto statptr, bool skip_for_shallow) {
+	  if (deep && !skip_for_shallow) {
+	    pg_stats.stats.sum.*statptr = in_stats.*statptr;
+	  }
+	});
+      foreach_scrub_checked_stat(
+	[this, FNAME, &pg_stats, &in_stats](
+	  const auto &name, auto statptr, const auto &invalid_predicate) {
+	  if (!invalid_predicate(pg_stats) &&
+	      (in_stats.*statptr != pg_stats.stats.sum.*statptr)) {
+	    ERRORDPP(
+	      "stat mismatch for {}: scrub: {}, pg: {}",
+	      pg, name, in_stats.*statptr, pg_stats.stats.sum.*statptr);
+	    ++pg_stats.stats.sum.num_shallow_scrub_errors;
+	  }
+	});
+      history.last_scrub = pg.peering_state.get_info().last_update;
+      auto now = ceph_clock_now();
+      history.last_scrub_stamp = now;
+      if (deep) {
+	history.last_deep_scrub_stamp = now;
+      }
+      return false; // notify_scrub_end will flush stats to osd
+    });
+}
+
+}
