10855e0afe732669d28d10d4decff09689d8e4e9
Samuel Just <sjust@redhat.com>
2023-11-16 20:20:07 -0800

    crush: add msr tunables
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crush/CrushCompiler.cc
+++ src/crush/CrushCompiler.cc
@@ -321,6 +321,13 @@ int CrushCompiler::decompile(ostream &out)
   if (crush.get_allowed_bucket_algs() != CRUSH_LEGACY_ALLOWED_BUCKET_ALGS)
     out << "tunable allowed_bucket_algs " << crush.get_allowed_bucket_algs()
 	<< "\n";
+  if (crush.get_choose_msr_total_tries() != 0)
+    out << "tunable choose_msr_total_tries " << crush.get_choose_msr_total_tries()
+	<< "\n";
+  if (crush.get_choose_msr_local_collision_tries() != 0)
+    out << "tunable choose_msr_local_collision_tries "
+	<< crush.get_choose_msr_local_collision_tries()
+	<< "\n";
 
   out << "\n# devices\n";
   for (int i=0; i<crush.get_max_devices(); i++) {
@@ -538,6 +545,10 @@ int CrushCompiler::parse_tunable(iter_t const& i)
     crush.set_straw_calc_version(val);
   else if (name == "allowed_bucket_algs")
     crush.set_allowed_bucket_algs(val);
+  else if (name == "choose_msr_total_tries")
+    crush.set_choose_msr_total_tries(val);
+  else if (name == "choose_msr_local_collision_tries")
+    crush.set_choose_msr_local_collision_tries(val);
   else {
     err << "tunable " << name << " not recognized" << std::endl;
     return -1;
