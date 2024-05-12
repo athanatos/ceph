athanatos/ceph

commit f354a56da34635e2ddfcfddef21134748adf7621

Striper: Add function 'assemble_result(CephContext *cct, char *buffer, size_t len)' in StripedReadResult.
For sparse read, it can avoid copying zero around.

Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

src/osdc/Striper.cc
@@ -388,3 +388,38 @@ void Striper::StripedReadResult::assemble_result(CephContext *cct,
   partial.clear();
 }
 
+void Striper::StripedReadResult::assemble_result(CephContext *cct, char *buffer, size_t length)
+{
+
+  assert(buffer && length == total_intended_len);
+
+  map<uint64_t,pair<bufferlist,uint64_t> >::reverse_iterator p = partial.rbegin();
+  if (p == partial.rend())
+    return;
+
+  uint64_t curr = length;
+  uint64_t end = p->first + p->second.second;
+  while (p != partial.rend()) {
+    // sanity check
+    ldout(cct, 0) << "assemble_result(" << this << ") " << p->first << "~" << p->second.second
+		   << " " << p->second.first.length() << " bytes"
+		   << dendl;
+    assert(p->first == end - p->second.second);
+    end = p->first;
+
+    size_t len = p->second.first.length();
+    assert(curr >= p->second.second);
+    curr -= p->second.second;
+    if (len < p->second.second) {
+      if (len)
+	p->second.first.copy(0, len, buffer + curr);
+      memset(buffer + curr + len, 0, p->second.second - len);
+    } else {
+      p->second.first.copy(0, len, buffer + curr);
+    }
+    ++p;
+  }
+  partial.clear();
+  assert(curr == 0);
+}
+

src/librbd/AioCompletion.cc
@@ -44,21 +44,25 @@ namespace librbd {
                    << "read_buf=" << reinterpret_cast<void*>(read_buf) << ", "
                    << "real_bl=" <<  reinterpret_cast<void*>(read_bl) << dendl;
     if (rval >= 0 && aio_type == AIO_TYPE_READ) {
-      // FIXME: make the destriper write directly into a buffer so
-      // that we avoid shuffling pointers and copying zeros around.
-      bufferlist bl;
-      destriper.assemble_result(cct, bl, true);
+      if (read_buf && !read_bl) {
+	destriper.assemble_result(cct, read_buf, read_buf_len);
+      } else {
+	// FIXME: make the destriper write directly into a buffer so
+	// that we avoid shuffling pointers and copying zeros around.
+	bufferlist bl;
+	destriper.assemble_result(cct, bl, true);
 
-      if (read_buf) {
-	assert(bl.length() == read_buf_len);
-	bl.copy(0, read_buf_len, read_buf);
-	ldout(cct, 20) << "copied resulting " << bl.length()
-		       << " bytes to " << (void*)read_buf << dendl;
-      }
-      if (read_bl) {
-	ldout(cct, 20) << "moving resulting " << bl.length()
-		       << " bytes to bl " << (void*)read_bl << dendl;
-	read_bl->claim(bl);
+	if (read_buf) {
+	  assert(bl.length() == read_buf_len);
+	  bl.copy(0, read_buf_len, read_buf);
+	  ldout(cct, 20) << "copied resulting " << bl.length()
+	    << " bytes to " << (void*)read_buf << dendl;
+	}
+	if (read_bl) {
+	  ldout(cct, 20) << " moving resulting " << bl.length()
+	    << " bytes to bl " << (void*)read_bl << dendl;
+	  read_bl->claim(bl);
+	}
       }
     }
   }

src/osdc/Striper.h
@@ -98,6 +98,12 @@ class CephContext;
 	const vector<pair<uint64_t,uint64_t> >& buffer_extents);
 
       void assemble_result(CephContext *cct, bufferlist& bl, bool zero_tail);
+
+      /**
+       * @buffer copy read data into buffer
+       * @len the length of buffer
+       */
+      void assemble_result(CephContext *cct, char *buffer, size_t len);
     };
 
   };

commit 8d8520a79bc1958a3fc243924214c430f6806f0b

osdc/Striper: relax assert in assemble_result() for raw buffers
The previous commit uncovered an old bug, introduced in commit
f354a56da346 ("Striper: Add function 'assemble_result(CephContext *cct,
char *buffer, size_t len)' in StripedReadResult.").  The buffer is
allowed to be NULL when assemble_result() is called -- it becomes
a problem only if some data was read.

Strengthen ZeroLengthRead and ZeroLengthWrite tests to cover both
a bogus pointer and a NULL pointer for buffer.

Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

src/osdc/Striper.cc
@@ -485,12 +485,13 @@ void Striper::StripedReadResult::assemble_result(CephContext *cct,
 
 void Striper::StripedReadResult::assemble_result(CephContext *cct, char *buffer, size_t length)
 {
-
-  ceph_assert(buffer && length == total_intended_len);
+  ceph_assert(length == total_intended_len);
 
   map<uint64_t,pair<bufferlist,uint64_t> >::reverse_iterator p = partial.rbegin();
   if (p == partial.rend())
     return;
+
+  ceph_assert(buffer);
 
   uint64_t curr = length;
   uint64_t end = p->first + p->second.second;

src/test/librbd/test_librbd.cc
@@ -8683,8 +8683,12 @@ TEST_F(TestLibRBD, ZeroLengthWrite)
   ASSERT_EQ(0, create_image(ioctx, name.c_str(), size, &order));
   ASSERT_EQ(0, rbd_open(ioctx, name.c_str(), &image, NULL));
 
-  char read_data[1];
+  const char data[] = "blah";
+  ASSERT_EQ(0, rbd_write(image, 0, 0, data));
+  ASSERT_EQ(0, rbd_write(image, 0, 0, (char*)0x123));
   ASSERT_EQ(0, rbd_write(image, 0, 0, NULL));
+
+  char read_data[1];
   ASSERT_EQ(1, rbd_read(image, 0, 1, read_data));
   ASSERT_EQ('\0', read_data[0]);
 
@@ -8736,6 +8740,8 @@ TEST_F(TestLibRBD, ZeroLengthRead)
 
   char read_data[1];
   ASSERT_EQ(0, rbd_read(image, 0, 0, read_data));
+  ASSERT_EQ(0, rbd_read(image, 0, 0, (char*)0x123));
+  ASSERT_EQ(0, rbd_read(image, 0, 0, NULL));
 
   ASSERT_EQ(0, rbd_close(image));
 