athanatos/ceph

commit 345264f6b7d84a4740d97ed495f47bbf2a26de2c

ceph-volume: introduce baseobjectstore class
This introduces a refactor in order to ease the addition or removal
of an OSD objectstore backend in ceph-volume.

Fixes: https://tracker.ceph.com/issues/61827

Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>
Co-authored-by: Rongqi Sun <sunrongqi@huawei.com>

src/ceph-volume/ceph_volume/objectstore/rawbluestore.py
@@ -1,0 +1,181 @@
+import logging
+import json
+import os
+from .bluestore import BlueStore
+from ceph_volume import terminal, decorators, conf, process
+from ceph_volume.util import system, disk
+from ceph_volume.util import prepare as prepare_utils
+from ceph_volume.util import encryption as encryption_utils
+from ceph_volume.devices.lvm.common import rollback_osd
+from ceph_volume.devices.raw.list import direct_report
+from typing import Any, Dict, List, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import argparse
+
+logger = logging.getLogger(__name__)
+
+
+class RawBlueStore(BlueStore):
+    def __init__(self, args: "argparse.Namespace") -> None:
+        super().__init__(args)
+        if hasattr(self.args, 'data'):
+            self.block_device_path = self.args.data
+        if hasattr(self.args, 'block_db'):
+            self.db_device_path = self.args.block_db
+        if hasattr(self.args, 'block_wal'):
+            self.wal_device_path = self.args.block_wal
+
+    def prepare_dmcrypt(self) -> None:
+        """
+        Helper for devices that are encrypted. The operations needed for
+        block, db, wal, devices are all the same
+        """
+        key = self.secrets['dmcrypt_key']
+
+        for device, device_type in [(self.block_device_path, 'block'),
+                                    (self.db_device_path, 'db'),
+                                    (self.wal_device_path, 'wal')]:
+
+            if device:
+                kname = disk.lsblk(device)['KNAME']
+                mapping = 'ceph-{}-{}-{}-dmcrypt'.format(self.osd_fsid,
+                                                         kname,
+                                                         device_type)
+                # format data device
+                encryption_utils.luks_format(
+                    key,
+                    device
+                )
+                encryption_utils.luks_open(
+                    key,
+                    device,
+                    mapping
+                )
+                self.__dict__[f'{device_type}_device_path'] = \
+                    '/dev/mapper/{}'.format(mapping)
+
+    def safe_prepare(self,
+                     args: Optional["argparse.Namespace"] = None) -> None:
+        """
+        An intermediate step between `main()` and `prepare()` so that we can
+        capture the `self.osd_id` in case we need to rollback
+
+        :param args: Injected args, usually from `raw create` which compounds
+                     both `prepare` and `create`
+        """
+        if args is not None:
+            self.args = args  # This should be moved (to __init__ ?)
+        try:
+            self.prepare()
+        except Exception:
+            logger.exception('raw prepare was unable to complete')
+            logger.info('will rollback OSD ID creation')
+            rollback_osd(self.args, self.osd_id)
+            raise
+        dmcrypt_log = 'dmcrypt' if hasattr(args, 'dmcrypt') else 'clear'
+        terminal.success("ceph-volume raw {} prepare "
+                         "successful for: {}".format(dmcrypt_log,
+                                                     self.args.data))
+
+    @decorators.needs_root
+    def prepare(self) -> None:
+        if self.encrypted:
+            self.secrets['dmcrypt_key'] = \
+                os.getenv('CEPH_VOLUME_DMCRYPT_SECRET')
+        self.osd_fsid = system.generate_uuid()
+        crush_device_class = self.args.crush_device_class
+        if crush_device_class:
+            self.secrets['crush_device_class'] = crush_device_class
+
+        tmpfs = not self.args.no_tmpfs
+        if self.args.block_wal:
+            self.wal = self.args.block_wal
+        if self.args.block_db:
+            self.db = self.args.block_db
+
+        # reuse a given ID if it exists, otherwise create a new ID
+        self.osd_id = prepare_utils.create_id(
+            self.osd_fsid, json.dumps(self.secrets))
+
+        if self.secrets.get('dmcrypt_key'):
+            self.prepare_dmcrypt()
+
+        self.prepare_osd_req(tmpfs=tmpfs)
+
+        # prepare the osd filesystem
+        self.osd_mkfs()
+
+    def _activate(self,
+                  meta: Dict[str, Any],
+                  tmpfs: bool) -> None:
+        # find the osd
+        osd_id = meta['osd_id']
+        osd_uuid = meta['osd_uuid']
+
+        # mount on tmpfs the osd directory
+        self.osd_path = '/var/lib/ceph/osd/%s-%s' % (conf.cluster, osd_id)
+        if not system.path_is_mounted(self.osd_path):
+            # mkdir -p and mount as tmpfs
+            prepare_utils.create_osd_path(osd_id, tmpfs=tmpfs)
+
+        # XXX This needs to be removed once ceph-bluestore-tool can deal with
+        # symlinks that exist in the osd dir
+
+        self.unlink_bs_symlinks()
+
+        # Once symlinks are removed, the osd dir can be 'primed again. chown
+        # first, regardless of what currently exists so that ``prime-osd-dir``
+        # can succeed even if permissions are somehow messed up
+        system.chown(self.osd_path)
+        prime_command = [
+            'ceph-bluestore-tool',
+            'prime-osd-dir',
+            '--path', self.osd_path,
+            '--no-mon-config',
+            '--dev', meta['device'],
+        ]
+        process.run(prime_command)
+
+        # always re-do the symlink regardless if it exists, so that the block,
+        # block.wal, and block.db devices that may have changed can be mapped
+        # correctly every time
+        prepare_utils.link_block(meta['device'], osd_id)
+
+        if 'device_db' in meta:
+            prepare_utils.link_db(meta['device_db'], osd_id, osd_uuid)
+
+        if 'device_wal' in meta:
+            prepare_utils.link_wal(meta['device_wal'], osd_id, osd_uuid)
+
+        system.chown(self.osd_path)
+        terminal.success("ceph-volume raw activate "
+                         "successful for osd ID: %s" % osd_id)
+
+    @decorators.needs_root
+    def activate(self,
+                 devs: List[str],
+                 start_osd_id: str,
+                 start_osd_uuid: str,
+                 tmpfs: bool) -> None:
+        """
+        :param args: The parsed arguments coming from the CLI
+        """
+        assert devs or start_osd_id or start_osd_uuid
+        found = direct_report(devs)
+
+        activated_any = False
+        for osd_uuid, meta in found.items():
+            osd_id = meta['osd_id']
+            if start_osd_id is not None and str(osd_id) != str(start_osd_id):
+                continue
+            if start_osd_uuid is not None and osd_uuid != start_osd_uuid:
+                continue
+            logger.info('Activating osd.%s uuid %s cluster %s' % (
+                osd_id, osd_uuid, meta['ceph_fsid']))
+            self._activate(meta,
+                           tmpfs=tmpfs)
+            activated_any = True
+
+        if not activated_any:
+            raise RuntimeError('did not find any matching OSD to activate')

src/ceph-volume/ceph_volume/activate/main.py
@@ -3,8 +3,8 @@
 import argparse
 
 from ceph_volume import terminal
-from ceph_volume.devices.lvm.activate import Activate as LVMActivate
-from ceph_volume.devices.raw.activate import Activate as RAWActivate
+from ceph_volume.objectstore.lvmbluestore import LvmBlueStore as LVMActivate
+from ceph_volume.objectstore.rawbluestore import RawBlueStore as RAWActivate
 from ceph_volume.devices.simple.activate import Activate as SimpleActivate
 
 
@@ -44,27 +44,24 @@ class Activate(object):
 
         # first try raw
         try:
-            RAWActivate([]).activate(
-                devs=None,
-                start_osd_id=self.args.osd_id,
-                start_osd_uuid=self.args.osd_uuid,
-                tmpfs=not self.args.no_tmpfs,
-                systemd=not self.args.no_systemd,
-            )
+            raw_activate = RAWActivate([])
+            raw_activate.activate(None,
+                                  self.args.osd_id,
+                                  self.args.osd_uuid,
+                                  not self.args.no_tmpfs)
             return
         except Exception as e:
             terminal.info(f'Failed to activate via raw: {e}')
 
         # then try lvm
         try:
-            LVMActivate([]).activate(
-                argparse.Namespace(
-                    osd_id=self.args.osd_id,
-                    osd_fsid=self.args.osd_uuid,
-                    no_tmpfs=self.args.no_tmpfs,
-                    no_systemd=self.args.no_systemd,
-                )
-            )
+            lvm_activate = LVMActivate(argparse.Namespace(
+                no_tmpfs=self.args.no_tmpfs,
+                no_systemd=self.args.no_systemd,
+                osd_fsid=self.args.osd_uuid))
+            lvm_activate.activate(None,
+                                  self.args.osd_id,
+                                  self.args.osd_uuid)
             return
         except Exception as e:
             terminal.info(f'Failed to activate via LVM: {e}')

src/ceph-volume/ceph_volume/devices/lvm/activate.py
@@ -1,216 +1,20 @@
 from __future__ import print_function
 import argparse
 import logging
-import os
 from textwrap import dedent
-from ceph_volume import process, conf, decorators, terminal, configuration
-from ceph_volume.util import system, disk
-from ceph_volume.util import prepare as prepare_utils
-from ceph_volume.util import encryption as encryption_utils
-from ceph_volume.systemd import systemctl
-from ceph_volume.api import lvm as api
-from .listing import direct_report
+from ceph_volume import objectstore
 
 
 logger = logging.getLogger(__name__)
 
 
-
-def get_osd_device_path(osd_lvs, device_type, dmcrypt_secret=None):
-    """
-    ``device_type`` can be one of ``db``, ``wal`` or ``block`` so that we can
-     query LVs on system and fallback to querying the uuid if that is not
-     present.
-
-    Return a path if possible, failing to do that a ``None``, since some of
-    these devices are optional.
-    """
-    osd_block_lv = None
-    for lv in osd_lvs:
-        if lv.tags.get('ceph.type') == 'block':
-            osd_block_lv = lv
-            break
-    if osd_block_lv:
-        is_encrypted = osd_block_lv.tags.get('ceph.encrypted', '0') == '1'
-        logger.debug('Found block device (%s) with encryption: %s', osd_block_lv.name, is_encrypted)
-        uuid_tag = 'ceph.%s_uuid' % device_type
-        device_uuid = osd_block_lv.tags.get(uuid_tag)
-        if not device_uuid:
-            return None
-
-    device_lv = None
-    for lv in osd_lvs:
-        if lv.tags.get('ceph.type') == device_type:
-            device_lv = lv
-            break
-    if device_lv:
-        if is_encrypted:
-            encryption_utils.luks_open(dmcrypt_secret, device_lv.lv_path, device_uuid)
-            return '/dev/mapper/%s' % device_uuid
-        return device_lv.lv_path
-
-    # this could be a regular device, so query it with blkid
-    physical_device = disk.get_device_from_partuuid(device_uuid)
-    if physical_device:
-        if is_encrypted:
-            encryption_utils.luks_open(dmcrypt_secret, physical_device, device_uuid)
-            return '/dev/mapper/%s' % device_uuid
-        return physical_device
-
-    raise RuntimeError('could not find %s with uuid %s' % (device_type, device_uuid))
-
-
-def activate_bluestore(osd_lvs, no_systemd=False, no_tmpfs=False):
-    for lv in osd_lvs:
-        if lv.tags.get('ceph.type') == 'block':
-            osd_block_lv = lv
-            break
-    else:
-        raise RuntimeError('could not find a bluestore OSD to activate')
-
-    is_encrypted = osd_block_lv.tags.get('ceph.encrypted', '0') == '1'
-    if is_encrypted and conf.dmcrypt_no_workqueue is None:
-        encryption_utils.set_dmcrypt_no_workqueue()
-    dmcrypt_secret = None
-    osd_id = osd_block_lv.tags['ceph.osd_id']
-    conf.cluster = osd_block_lv.tags['ceph.cluster_name']
-    osd_fsid = osd_block_lv.tags['ceph.osd_fsid']
-    configuration.load_ceph_conf_path(osd_block_lv.tags['ceph.cluster_name'])
-    configuration.load()
-
-    # mount on tmpfs the osd directory
-    osd_path = '/var/lib/ceph/osd/%s-%s' % (conf.cluster, osd_id)
-    if not system.path_is_mounted(osd_path):
-        # mkdir -p and mount as tmpfs
-        prepare_utils.create_osd_path(osd_id, tmpfs=not no_tmpfs)
-    # XXX This needs to be removed once ceph-bluestore-tool can deal with
-    # symlinks that exist in the osd dir
-    for link_name in ['block', 'block.db', 'block.wal']:
-        link_path = os.path.join(osd_path, link_name)
-        if os.path.exists(link_path):
-            os.unlink(os.path.join(osd_path, link_name))
-    # encryption is handled here, before priming the OSD dir
-    if is_encrypted:
-        osd_lv_path = '/dev/mapper/%s' % osd_block_lv.lv_uuid
-        lockbox_secret = osd_block_lv.tags['ceph.cephx_lockbox_secret']
-        encryption_utils.write_lockbox_keyring(osd_id, osd_fsid, lockbox_secret)
-        dmcrypt_secret = encryption_utils.get_dmcrypt_key(osd_id, osd_fsid)
-        encryption_utils.luks_open(dmcrypt_secret, osd_block_lv.lv_path, osd_block_lv.lv_uuid)
-    else:
-        osd_lv_path = osd_block_lv.lv_path
-
-    db_device_path = get_osd_device_path(osd_lvs, 'db', dmcrypt_secret=dmcrypt_secret)
-    wal_device_path = get_osd_device_path(osd_lvs, 'wal', dmcrypt_secret=dmcrypt_secret)
-
-    # Once symlinks are removed, the osd dir can be 'primed again. chown first,
-    # regardless of what currently exists so that ``prime-osd-dir`` can succeed
-    # even if permissions are somehow messed up
-    system.chown(osd_path)
-    prime_command = [
-        'ceph-bluestore-tool', '--cluster=%s' % conf.cluster,
-        'prime-osd-dir', '--dev', osd_lv_path,
-        '--path', osd_path, '--no-mon-config']
-
-    process.run(prime_command)
-    # always re-do the symlink regardless if it exists, so that the block,
-    # block.wal, and block.db devices that may have changed can be mapped
-    # correctly every time
-    process.run(['ln', '-snf', osd_lv_path, os.path.join(osd_path, 'block')])
-    system.chown(os.path.join(osd_path, 'block'))
-    system.chown(osd_path)
-    if db_device_path:
-        destination = os.path.join(osd_path, 'block.db')
-        process.run(['ln', '-snf', db_device_path, destination])
-        system.chown(db_device_path)
-        system.chown(destination)
-    if wal_device_path:
-        destination = os.path.join(osd_path, 'block.wal')
-        process.run(['ln', '-snf', wal_device_path, destination])
-        system.chown(wal_device_path)
-        system.chown(destination)
-
-    if no_systemd is False:
-        # enable the ceph-volume unit for this OSD
-        systemctl.enable_volume(osd_id, osd_fsid, 'lvm')
-
-        # enable the OSD
-        systemctl.enable_osd(osd_id)
-
-        # start the OSD
-        systemctl.start_osd(osd_id)
-    terminal.success("ceph-volume lvm activate successful for osd ID: %s" % osd_id)
-
-
 class Activate(object):
-
     help = 'Discover and mount the LVM device associated with an OSD ID and start the Ceph OSD'
 
-    def __init__(self, argv):
+    def __init__(self, argv, args=None):
+        self.objectstore = None
         self.argv = argv
-
-    @decorators.needs_root
-    def activate_all(self, args):
-        listed_osds = direct_report()
-        osds = {}
-        for osd_id, devices in listed_osds.items():
-            # the metadata for all devices in each OSD will contain
-            # the FSID which is required for activation
-            for device in devices:
-                fsid = device.get('tags', {}).get('ceph.osd_fsid')
-                if fsid:
-                    osds[fsid] = osd_id
-                    break
-        if not osds:
-            terminal.warning('Was unable to find any OSDs to activate')
-            terminal.warning('Verify OSDs are present with "ceph-volume lvm list"')
-            return
-        for osd_fsid, osd_id in osds.items():
-            if not args.no_systemd and systemctl.osd_is_active(osd_id):
-                terminal.warning(
-                    'OSD ID %s FSID %s process is active. Skipping activation' % (osd_id, osd_fsid)
-                )
-            else:
-                terminal.info('Activating OSD ID %s FSID %s' % (osd_id, osd_fsid))
-                self.activate(args, osd_id=osd_id, osd_fsid=osd_fsid)
-
-    @decorators.needs_root
-    def activate(self, args, osd_id=None, osd_fsid=None):
-        """
-        :param args: The parsed arguments coming from the CLI
-        :param osd_id: When activating all, this gets populated with an
-                       existing OSD ID
-        :param osd_fsid: When activating all, this gets populated with an
-                         existing OSD FSID
-        """
-        osd_id = osd_id if osd_id else args.osd_id
-        osd_fsid = osd_fsid if osd_fsid else args.osd_fsid
-
-        if osd_id and osd_fsid:
-            tags = {'ceph.osd_id': osd_id, 'ceph.osd_fsid': osd_fsid}
-        elif not osd_id and osd_fsid:
-            tags = {'ceph.osd_fsid': osd_fsid}
-        elif osd_id and not osd_fsid:
-            raise RuntimeError('could not activate osd.{}, please provide the '
-                               'osd_fsid too'.format(osd_id))
-        else:
-            raise RuntimeError('Please provide both osd_id and osd_fsid')
-        lvs = api.get_lvs(tags=tags)
-        if not lvs:
-            raise RuntimeError('could not find osd.%s with osd_fsid %s' %
-                               (osd_id, osd_fsid))
-
-        # This argument is only available when passed in directly or via
-        # systemd, not when ``create`` is being used
-        # placeholder when a new objectstore support will be added
-        if getattr(args, 'auto_detect_objectstore', False):
-            logger.info('auto detecting objectstore')
-            return activate_bluestore(lvs, args.no_systemd)
-
-        # explicit 'objectstore' flags take precedence
-        if getattr(args, 'bluestore', False):
-            activate_bluestore(lvs, args.no_systemd, getattr(args, 'no_tmpfs', False))
-        elif any('ceph.block_device' in lv.tags for lv in lvs):
-            activate_bluestore(lvs, args.no_systemd, getattr(args, 'no_tmpfs', False))
+        self.args = args
 
     def main(self):
         sub_command_help = dedent("""
@@ -257,6 +61,14 @@ class Activate(object):
             help='force bluestore objectstore activation',
         )
         parser.add_argument(
+            '--objectstore',
+            dest='objectstore',
+            help='The OSD objectstore.',
+            default='bluestore',
+            choices=['bluestore', 'seastore'],
+            type=str,
+        )
+        parser.add_argument(
             '--all',
             dest='activate_all',
             action='store_true',
@@ -273,11 +85,15 @@ class Activate(object):
             action='store_true',
             help='Do not use a tmpfs mount for OSD data dir'
         )
-        if len(self.argv) == 0:
+        if len(self.argv) == 0 and self.args is None:
             print(sub_command_help)
             return
-        args = parser.parse_args(self.argv)
-        if args.activate_all:
-            self.activate_all(args)
+        if self.args is None:
+            self.args = parser.parse_args(self.argv)
+        if self.args.bluestore:
+            self.args.objectstore = 'bluestore'
+        self.objectstore = objectstore.mapping['LVM'][self.args.objectstore](args=self.args)
+        if self.args.activate_all:
+            self.objectstore.activate_all()
         else:
-            self.activate(args)
+            self.objectstore.activate()

src/ceph-volume/ceph_volume/devices/lvm/batch.py
@@ -234,9 +234,17 @@ class Batch(object):
                   'are passed in DEVICES'),
         )
         parser.add_argument(
+            '--objectstore',
+            dest='objectstore',
+            help='The OSD objectstore.',
+            default='bluestore',
+            choices=['bluestore', 'seastore'],
+            type=str,
+        )
+        parser.add_argument(
             '--bluestore',
             action='store_true',
-            help='bluestore objectstore (default)',
+            help='bluestore objectstore (default). (DEPRECATED: use --objectstore instead)',
         )
         parser.add_argument(
             '--report',
@@ -323,6 +331,8 @@ class Batch(object):
             type=arg_validators.valid_osd_id
         )
         self.args = parser.parse_args(argv)
+        if self.args.bluestore:
+            self.args.objectstore = 'bluestore'
         self.parser = parser
         for dev_list in ['', 'db_', 'wal_']:
             setattr(self, '{}usable'.format(dev_list), [])
@@ -383,11 +393,6 @@ class Batch(object):
         if not self.args.devices:
             return self.parser.print_help()
 
-        # Default to bluestore here since defaulting it in add_argument may
-        # cause both to be True
-        if not self.args.bluestore:
-            self.args.bluestore = True
-
         if (self.args.auto and not self.args.db_devices and not
             self.args.wal_devices):
             self._sort_rotational_disks()
@@ -398,7 +403,7 @@ class Batch(object):
                                      self.args.db_devices,
                                      self.args.wal_devices)
 
-        plan = self.get_plan(self.args)
+        plan = self.get_deployment_layout()
 
         if self.args.report:
             self.report(plan)
@@ -425,43 +430,38 @@ class Batch(object):
         for osd in plan:
             args = osd.get_args(defaults)
             if self.args.prepare:
-                p = Prepare([])
-                p.safe_prepare(argparse.Namespace(**args))
+                p = Prepare([], args=argparse.Namespace(**args))
+                p.main()
             else:
-                c = Create([])
-                c.create(argparse.Namespace(**args))
+                c = Create([], args=argparse.Namespace(**args))
+                c.create()
 
-
-    def get_plan(self, args):
-        if args.bluestore:
-            plan = self.get_deployment_layout(args, args.devices, args.db_devices,
-                                              args.wal_devices)
-        return plan
-
-    def get_deployment_layout(self, args, devices, fast_devices=[],
-                              very_fast_devices=[]):
+    def get_deployment_layout(self):
         '''
         The methods here are mostly just organization, error reporting and
         setting up of (default) args. The heavy lifting code for the deployment
         layout can be found in the static get_*_osds and get_*_fast_allocs
         functions.
         '''
+        devices = self.args.devices
+        fast_devices = self.args.db_devices
+        very_fast_devices = self.args.wal_devices
         plan = []
         phys_devs, lvm_devs = separate_devices_from_lvs(devices)
         mlogger.debug(('passed data devices: {} physical,'
                        ' {} LVM').format(len(phys_devs), len(lvm_devs)))
 
-        plan.extend(get_physical_osds(phys_devs, args))
+        plan.extend(get_physical_osds(phys_devs, self.args))
 
-        plan.extend(get_lvm_osds(lvm_devs, args))
+        plan.extend(get_lvm_osds(lvm_devs, self.args))
 
         num_osds = len(plan)
         if num_osds == 0:
             mlogger.info('All data devices are unavailable')
             return plan
-        requested_osds = args.osds_per_device * len(phys_devs) + len(lvm_devs)
+        requested_osds = self.args.osds_per_device * len(phys_devs) + len(lvm_devs)
 
-        if args.bluestore:
+        if self.args.objectstore == 'bluestore':
             fast_type = 'block_db'
         fast_allocations = self.fast_allocations(fast_devices,
                                                  requested_osds,
@@ -491,7 +491,7 @@ class Batch(object):
             if fast_devices:
                 osd.add_fast_device(*fast_allocations.pop(),
                                     type_=fast_type)
-            if very_fast_devices and args.bluestore:
+            if very_fast_devices and self.args.objectstore == 'bluestore':
                 osd.add_very_fast_device(*very_fast_allocations.pop())
         return plan
 

src/ceph-volume/ceph_volume/devices/lvm/common.py
@@ -36,6 +36,13 @@ def rollback_osd(args, osd_id=None):
 
 
 common_args = {
+    '--objectstore': {
+        'dest': 'objectstore',
+        'help': 'The OSD objectstore.',
+        'default': 'bluestore',
+        'choices': ['bluestore', 'seastore'],
+        'type': str,
+    },
     '--data': {
         'help': 'OSD data path. A physical device or logical volume',
         'required': True,
@@ -86,7 +93,7 @@ common_args = {
 bluestore_args = {
     '--bluestore': {
         'action': 'store_true',
-        'help': 'Use the bluestore objectstore',
+        'help': 'Use the bluestore objectstore. (DEPRECATED: use --objectstore instead)',
     },
     '--block.db': {
         'dest': 'block_db',

commit 6b22da9897f99e05c7a3e8fecd914a2047c40f23

ceph-volume: pass self.osd_id to create_id() call
This fixes a regression
(most likely introduced with 345264f6b7d)

Current (broken) behavior:
```
[root@devel1-1 /]# ceph osd tree
ID  CLASS  WEIGHT  TYPE NAME     STATUS     REWEIGHT  PRI-AFF
-1              0  root default
 0              0  osd.0         destroyed         0  1.00000
[root@devel1-1 /]# ceph-volume raw prepare --bluestore --data /dev/vdb --osd-id 0
Running command: /usr/bin/ceph-authtool --gen-print-key
Running command: /usr/bin/ceph-authtool --gen-print-key
Running command: /usr/bin/ceph --cluster ceph --name client.bootstrap-osd --keyring /var/lib/ceph/bootstrap-osd/ceph.keyring -i - osd new 543e0268-ea66-451f-a2f6-95ae60ab6146
Running command: /usr/bin/mount -t tmpfs tmpfs /var/lib/ceph/osd/ceph-1
Running command: /usr/sbin/restorecon /var/lib/ceph/osd/ceph-1
Running command: /usr/bin/chown -R ceph:ceph /dev/vdb
Running command: /usr/bin/ln -s /dev/vdb /var/lib/ceph/osd/ceph-1/block
Running command: /usr/bin/ceph --cluster ceph --name client.bootstrap-osd --keyring /var/lib/ceph/bootstrap-osd/ceph.keyring mon getmap -o /var/lib/ceph/osd/ceph-1/activate.monmap
 stderr: got monmap epoch 1
--> Creating keyring file for osd.1
Running command: /usr/bin/chown -R ceph:ceph /var/lib/ceph/osd/ceph-1/keyring
Running command: /usr/bin/chown -R ceph:ceph /var/lib/ceph/osd/ceph-1/
Running command: /usr/bin/ceph-osd --cluster ceph --osd-objectstore bluestore --mkfs -i 1 --monmap /var/lib/ceph/osd/ceph-1/activate.monmap --keyfile - --osd-data /var/lib/ceph/osd/ceph-1/ --osd-uuid 543e0268-ea66-451f-a2f6-95ae60ab6146 --setuser ceph --setgroup ceph
 stderr: 2024-09-04T11:46:54.283+0000 7f0027d2b640 -1 bluestore(/var/lib/ceph/osd/ceph-1//block) _read_bdev_label unable to decode label /var/lib/ceph/osd/ceph-1//block at offset 102: void bluestore_bdev_label_t::decode(ceph::buffer::v15_2_0::list::const_iterator&) decode past end of struct encoding: Malformed input [buffer:3]

src/ceph-volume/ceph_volume/objectstore/rawbluestore.py
@@ -21,7 +21,7 @@ class RawBlueStore(BlueStore):
         super().__init__(args)
         self.method = 'raw'
         self.devices: List[str] = getattr(args, 'devices', [])
-        self.osd_id = getattr(self.args, 'osd_id', '')
+        self.osd_id = getattr(self.args, 'osd_id', None)
         self.osd_fsid = getattr(self.args, 'osd_fsid', '')
         self.block_device_path = getattr(self.args, 'data', '')
         self.db_device_path = getattr(self.args, 'block_db', '')
@@ -95,7 +95,7 @@ class RawBlueStore(BlueStore):
 
         # reuse a given ID if it exists, otherwise create a new ID
         self.osd_id = prepare_utils.create_id(
-            self.osd_fsid, json.dumps(self.secrets))
+            self.osd_fsid, json.dumps(self.secrets), self.osd_id)
 
         if self.encrypted:
             self.prepare_dmcrypt()