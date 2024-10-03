82628c95b5243550ce3dc614de481d98295556d1
Samuel Just <sjust@redhat.com>
2024-10-03 00:41:34 +0000

    crimson: introduce RAII style obc lock mechanic
    Currently, we rely on ObjectContextLoader::with_* wrappers to load,
    lock, and guarrantee release of obcs.  That mechanism works well enough,
    but the execution pathway is pretty tough to read as it spans
    [Internal]ClientRequest, PG, ObjectContextLoader, ObjectContext, and
    tri_mutex.  This mechanism cuts out PG and ObjectContext (mostly) and
    uses coroutine support for auto variables to make the interface easier
    to understand.
    
    This mechanism will also allow a future PR to access the ObjectContext
    state prior to loading it.  This will be important to using the
    ObjectContext memory to host per-object pipeline states.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/osd/object_context_loader.h
+++ src/crimson/osd/object_context_loader.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <seastar/core/future.hh>
+#include <seastar/util/defer.hh>
 #include "crimson/common/errorator.h"
+#include "crimson/common/log.h"
 #include "crimson/osd/object_context.h"
 #include "crimson/osd/pg_backend.h"
+#include "osd/object_state_fmt.h"
 
 namespace crimson::osd {
 class ObjectContextLoader {
@@ -28,6 +31,105 @@ public:
     ::crimson::interruptible::interruptible_errorator<
       ::crimson::osd::IOInterruptCondition,
       load_obc_ertr>;
+
+  class Manager {
+    ObjectContextLoader &loader;
+    hobject_t target;
+
+    Manager() = delete;
+    template <typename T>
+    Manager(ObjectContextLoader &loader, T &&t)
+      : loader(loader), target(std::forward<T>(t)) {}
+    Manager(const Manager &) = delete;
+    Manager &operator=(const Manager &o) = delete;
+
+    struct options_t {
+      bool resolve_clone = true;
+      bool clone_only = false;
+    } options;
+
+    struct state_t {
+      RWState::State state = RWState::RWNONE;
+      ObjectContextRef obc;
+      bool is_empty() const { return !obc; }
+    };
+    state_t head_state;
+    state_t target_state;
+
+    friend ObjectContextLoader;
+
+    void release_state(state_t &s) {
+      LOG_PREFIX(ObjectContextLoader::release_state);
+      if (s.is_empty()) return;
+
+      switch (s.state) {
+      case RWState::RWREAD:
+	s.obc->lock.unlock_for_read();
+	break;
+      case RWState::RWWRITE:
+	s.obc->lock.unlock_for_write();
+	break;
+      case RWState::RWEXCL:
+	s.obc->lock.unlock_for_excl();
+	break;
+      case RWState::RWNONE:
+	// noop
+	break;
+      default:
+	ceph_assert(0 == "invalid");
+      }
+
+      SUBDEBUGDPP(
+	osd, "released object {}, {}",
+	loader.dpp, s.obc->get_oid(), s.obc->obs);
+      s.obc->remove_from(loader.obc_set_accessing);
+      s = state_t();
+    }
+  public:
+    Manager(Manager &&rhs) : loader(rhs.loader) {
+      std::swap(target, rhs.target);
+      std::swap(options, rhs.options);
+      std::swap(head_state, rhs.head_state);
+      std::swap(target_state, rhs.target_state);
+    }
+
+    Manager &operator=(Manager &&o) {
+      this->~Manager();
+      new(this) Manager(std::move(o));
+      return *this;
+    }
+
+    ObjectContextRef &get_obc() {
+      ceph_assert(!target_state.is_empty());
+      return target_state.obc;
+    }
+
+    void release() {
+      release_state(head_state);
+      release_state(target_state);
+    }
+
+    auto get_releaser() {
+      return seastar::defer([this] {
+	release();
+      });
+    }
+
+    ~Manager() {
+      release();
+    }
+  };
+  Manager get_obc_manager(hobject_t oid, bool resolve_clone = true) {
+    Manager ret(*this, oid);
+    ret.options.resolve_clone = resolve_clone;
+    return ret;
+  }
+
+  using load_and_lock_ertr = load_obc_ertr;
+  using load_and_lock_iertr = interruptible::interruptible_errorator<
+    IOInterruptCondition, load_and_lock_ertr>;
+  using load_and_lock_fut = load_and_lock_iertr::future<>;
+  load_and_lock_fut load_and_lock(Manager &, RWState::State);
 
   using interruptor = ::crimson::interruptible::interruptor<
     ::crimson::osd::IOInterruptCondition>;
@@ -73,7 +175,7 @@ private:
   load_obc_iertr::future<> with_head_obc(const hobject_t& oid,
                                          with_obc_func_t&& func);
 
-  template<RWState::State State, bool track, typename Func>
+  template<RWState::State State, typename Func>
   load_obc_iertr::future<> with_locked_obc(const hobject_t& oid,
 					   Func&& func);
 
@@ -84,4 +186,7 @@ private:
 
   load_obc_iertr::future<> load_obc(ObjectContextRef obc);
 };
+
+using ObjectContextManager = ObjectContextLoader::Manager;
+
 }
