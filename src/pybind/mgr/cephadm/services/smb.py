athanatos/ceph

commit af0ebab0a7f0efd4fa48a5b4c0d1acbe1f8883b1

cephadm: fence old ranks for SMB service
Implemented functionality to fence old SMB service ranks,
ensuring the rank map reflects the correct state of all nodes in the cluster.

Fixes: https://tracker.ceph.com/issues/68512
Signed-off-by: Avan Thakkar <athakkar@redhat.com>

src/pybind/mgr/cephadm/services/smb.py
@@ -33,15 +33,37 @@ class SMBService(CephService):
         smb_spec = cast(SMBSpec, spec)
         return 'clustered' in smb_spec.features
 
+    def fence(self, daemon_id: str) -> None:
+        logger.info(f'Fencing old smb.{daemon_id}')
+        ret, out, err = self.mgr.mon_command({
+            'prefix': 'auth rm',
+            'entity': f'client.smb.fs.cluster.{daemon_id}',
+        })
+
     def fence_old_ranks(
         self,
         spec: ServiceSpec,
         rank_map: Dict[int, Dict[int, Optional[str]]],
         num_ranks: int,
     ) -> None:
-        logger.warning(
-            'fence_old_ranks: Unsupported %r %r', rank_map, num_ranks
-        )
+        smb_spec = cast(SMBSpec, spec)
+        for rank, m in list(rank_map.items()):
+            if rank >= num_ranks:
+                for daemon_id in m.values():
+                    if daemon_id is not None:
+                        logger.info(f'Fencing old smb.{smb_spec.cluster_id}')
+                        self.fence(smb_spec.cluster_id)
+                del rank_map[rank]
+                self.mgr.spec_store.save_rank_map(spec.service_name(), rank_map)
+            else:
+                max_gen = max(m.keys())
+                for gen, daemon_id in list(m.items()):
+                    if gen < max_gen:
+                        if daemon_id is not None:
+                            logger.info(f'Fencing old smb.{smb_spec.cluster_id}')
+                            self.fence(smb_spec.cluster_id)
+                        del rank_map[rank][gen]
+                        self.mgr.spec_store.save_rank_map(spec.service_name(), rank_map)
 
     def prepare_create(
         self, daemon_spec: CephadmDaemonDeploySpec

commit 7dc1ac652a27f023483af60952bc31b085c631e5

mgr/cephadm: do not delete smb fs cephx keys
This change effectively disables fencing for the smb service because
the previous attempt to implement fencing would destroy the only
cephx key. Deleting this key would prevent any smb service part of
the logical cluster from talking to cephfs, even ones that were not
to be fenced.

The whole concept of fencing and ranks needs a bit of a rethink in
regards to smb. For now, we're just going to rely on ctdb and not
cephadm for smb's HA.

Fixes: af0ebab0a7f0efd4fa48a5b4c0d1acbe1f8883b1
Signed-off-by: John Mulligan <jmulligan@redhat.com>

src/pybind/mgr/cephadm/services/smb.py
@@ -34,11 +34,9 @@ class SMBService(CephService):
         return 'clustered' in smb_spec.features
 
     def fence(self, daemon_id: str) -> None:
-        logger.info(f'Fencing old smb.{daemon_id}')
-        ret, out, err = self.mgr.mon_command({
-            'prefix': 'auth rm',
-            'entity': f'client.smb.fs.cluster.{daemon_id}',
-        })
+        # ... but fencing still wont do anything real, because we
+        # do not have per-service keys. but logging is fun
+        logger.debug('Will not fence key for smb cluster %r', daemon_id)
 
     def fence_old_ranks(
         self,
@@ -47,11 +45,11 @@ class SMBService(CephService):
         num_ranks: int,
     ) -> None:
         smb_spec = cast(SMBSpec, spec)
+        logger.info('Fencing called for smb.%s', smb_spec.cluster_id)
         for rank, m in list(rank_map.items()):
             if rank >= num_ranks:
                 for daemon_id in m.values():
                     if daemon_id is not None:
-                        logger.info(f'Fencing old smb.{smb_spec.cluster_id}')
                         self.fence(smb_spec.cluster_id)
                 del rank_map[rank]
                 self.mgr.spec_store.save_rank_map(spec.service_name(), rank_map)
@@ -60,7 +58,6 @@ class SMBService(CephService):
                 for gen, daemon_id in list(m.items()):
                     if gen < max_gen:
                         if daemon_id is not None:
-                            logger.info(f'Fencing old smb.{smb_spec.cluster_id}')
                             self.fence(smb_spec.cluster_id)
                         del rank_map[rank][gen]
                         self.mgr.spec_store.save_rank_map(spec.service_name(), rank_map)