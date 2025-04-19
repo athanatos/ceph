1c57cd5c5775bf1687999593eb218b28d3298588
Samuel Just <sjust@redhat.com>
2025-04-19 01:15:29 +0000

    allow none to be specified for balance_cpu
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -1203,6 +1203,8 @@ do_balance_cpu() {
     local osd=$1
 
     if [ "${balance_cpu}" -eq "none"]; then
+        echo "$CEPH_BIN/ceph -c $conf_fn config set osd.$osd crimson_seastore_num_threads $crimson_smp"
+        $CEPH_BIN/ceph -c $conf_fn config set osd.$osd crimson_seastore_num_threads $crimson_smp
         return
     fi
 
