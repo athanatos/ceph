ddb221468b2424f88e97ef57a44e15baf1567e80
Samuel Just <sjust@redhat.com>
2025-06-05 23:27:14 +0000

    DNM crimson/.../transaction_manager: skip crc check if stored crc is 0

--- src/crimson/os/seastore/transaction_manager.h
+++ src/crimson/os/seastore/transaction_manager.h
@@ -1463,7 +1463,7 @@ private:
 	extent.set_seen_by_users();
       }
     ).si_then([FNAME, &t, pin=pin, this](auto ref) mutable -> ret {
-      if (ref->is_fully_loaded()) {
+      if (ref->is_fully_loaded() && pin.get_checksum() != 0 /* TODOSAM: hacky */) {
         auto crc = ref->calc_crc32c();
         SUBTRACET(
 	  seastore_tm,
