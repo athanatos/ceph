1506c9adbbdf97c27d7bdb5b521875b7a330f860
Samuel Just <sjust@redhat.com>
2025-06-05 22:46:37 +0000

    crimson/.../transaction_manager: convert read_pin to coroutine
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/transaction_manager.h
+++ src/crimson/os/seastore/transaction_manager.h
@@ -18,6 +18,8 @@
 #include "include/ceph_assert.h"
 #include "include/buffer.h"
 
+#include "crimson/common/coroutine.h"
+
 #include "crimson/osd/exceptions.h"
 
 #include "crimson/os/seastore/logging.h"
@@ -288,42 +290,36 @@ public:
     SUBDEBUGT(seastore_tm, "{} {} 0x{:x}~0x{:x} direct_off=0x{:x} ...",
               t, T::TYPE, pin, partial_off, partial_len, direct_partial_off);
 
-    return lba_manager->refresh_lba_mapping(t, std::move(pin)
-    ).si_then([&t, this, direct_partial_off, partial_len,
-	       maybe_init=std::move(maybe_init)](auto npin) mutable {
-      // checking the lba child must be atomic with creating
-      // and linking the absent child
-      auto ret = get_extent_if_linked<T>(t, std::move(npin));
-      if (ret.index() == 1) {
-	return std::get<1>(ret
-        ).si_then([direct_partial_off, partial_len, this, &t](auto extent) {
-          return cache->read_extent_maybe_partial(
-            t, std::move(extent), direct_partial_off, partial_len);
-        }).si_then([maybe_init=std::move(maybe_init)](auto extent) {
-          if (!extent->is_seen_by_users()) {
-            maybe_init(*extent);
-            extent->set_seen_by_users();
-          }
-          return std::move(extent);
-        });
-      } else {
-	auto &r = std::get<0>(ret);
-	return this->pin_to_extent<T>(
-          t, std::move(r.mapping), std::move(r.child_pos),
-	  direct_partial_off, partial_len,
-	  std::move(maybe_init));
+    auto npin = co_await lba_manager->refresh_lba_mapping(t, std::move(pin));
+
+    // checking the lba child must be atomic with creating
+    // and linking the absent child
+    auto ret = get_extent_if_linked<T>(t, std::move(npin));
+    TCachedExtentRef<T> extent;
+    if (ret.index() == 1) {
+      extent = co_await std::move(std::get<1>(ret));
+      extent = co_await cache->read_extent_maybe_partial(
+	t, std::move(extent), direct_partial_off, partial_len);
+      if (!extent->is_seen_by_users()) {
+	maybe_init(*extent);
+	extent->set_seen_by_users();
       }
-    }).si_then([FNAME, maybe_indirect_info, is_clone, &t](TCachedExtentRef<T> ext) {
-      if (maybe_indirect_info.has_value()) {
-        SUBDEBUGT(seastore_tm, "got indirect +0x{:x}~0x{:x} is_clone={} {}",
-                  t, maybe_indirect_info->intermediate_offset,
-                  maybe_indirect_info->length, is_clone, *ext);
-      } else {
-        SUBDEBUGT(seastore_tm, "got direct is_clone={} {}",
-                  t, is_clone, *ext);
-      }
-      return maybe_indirect_extent_t<T>{ext, maybe_indirect_info, is_clone};
-    });
+    } else {
+      auto &r = std::get<0>(ret);
+      extent = co_await this->pin_to_extent<T>(
+	t, std::move(r.mapping), std::move(r.child_pos),
+	direct_partial_off, partial_len,
+	std::move(maybe_init));
+    }
+    if (maybe_indirect_info.has_value()) {
+      SUBDEBUGT(seastore_tm, "got indirect +0x{:x}~0x{:x} is_clone={} {}",
+		t, maybe_indirect_info->intermediate_offset,
+		maybe_indirect_info->length, is_clone, *extent);
+    } else {
+      SUBDEBUGT(seastore_tm, "got direct is_clone={} {}",
+		t, is_clone, *extent);
+    }
+    co_return maybe_indirect_extent_t<T>{extent, maybe_indirect_info, is_clone};
   }
 
   base_iertr::future<LBAMapping> next_mapping(
