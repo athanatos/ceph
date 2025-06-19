3fc33aab082ffcd68ce3d683c17e3ca110fe3a47
shreya subramanian <Shreya.Subramanian@ibm.com>
2025-06-19 22:58:32 +0000

    cherry picked the ceph.py and ceph_manager.py changes from the perf_counters_test branch (now called benchmarking).  because I did the crimsion changes on the same branch and i do not want to run teuthology suite on store-bench yet

--- qa/tasks/ceph.py
+++ qa/tasks/ceph.py
@@ -1600,6 +1600,19 @@ def created_pool(ctx, config):
                 new_pool, 'pg_num')
 
 
+def perf_counter_check(ctx, config):
+    """
+    check threshold for map counters.
+    """
+    ceph_manager=ctx.managers['ceph']
+    for osd_id in ceph_manager.get_osd_status()['live']:
+        full,inc=ceph_manager.do_dump_perf_counter(osd_id)
+        if full==None and inc==None:
+            ceph_manager.log("could not get map data for this osd, look at the log files for do_dump_perf_counter, maybe osd is down")
+            continue 
+        ceph_manager.log(f"shreya output full map {full}")
+        ceph_manager.log(f"shreya output inc map {inc}")
+
 @contextlib.contextmanager
 def suppress_mon_health_to_clog(ctx, config):
     """
--- qa/tasks/ceph_manager.py
+++ qa/tasks/ceph_manager.py
@@ -2651,6 +2651,22 @@ class CephManager:
         :returns: all osds
         """
         return self.get_osd_dump_json()['osds']
+        
+    def do_dump_perf_counter(self, osd_id):
+        """
+        dumps perf counter for full and inc maps for osd with osd id 
+        """
+        try: 
+            proc = self.osd_admin_socket(osd_id, ['perf', 'dump'],stdout=StringIO())
+            py_dic_output=json.loads(proc.stdout.getvalue())
+            full_map_count=py_dic_output['osd']['full_map_received']
+            inc_map_count=py_dic_output['osd']['inc_map_received']
+            res = f"For the osd : {osd_id} full maps = {full_map_count} inc maps = {inc_map_count}"
+            self.log(res)
+            return full_map_count,inc_map_count
+        except Exception as e:
+            output = f"failed to get counters because of {e}"
+            self.log(output)
 
     def get_osd_metadata(self):
         """
